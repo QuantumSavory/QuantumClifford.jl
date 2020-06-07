aa78bfa1927370715f6d91ae042d5dc46153a0d9
Stefan Krastanov <stefan@krastanov.org>
2020-06-07 15:48:57 -0400

    Update travis file based on Example.jl.

--- docs/travis.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-if [ ${TRAVIS_OS_NAME} = "linux" ]; then
-    julia --project=docs/ -e 'println(VERSION); if VERSION.minor!=3 println("not the desired version"); exit() end; println("building docs"); using Pkg; Pkg.instantiate(); Pkg.develop(PackageSpec(path=pwd())); using SimpleClifford; cd(joinpath(dirname(pathof(SimpleClifford)), "..", "docs")); include("make.jl")';
-fi
