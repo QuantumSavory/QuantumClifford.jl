c83f85f69b0c73b73620d036c682925863bd5ff7
Stefan Krastanov <stefan@krastanov.org>
2022-06-02 13:48:45 -0400

    Fix incorrect index permutation in graph generation
    Fixes #57 https://github.com/Krastanov/QuantumClifford.jl/issues/57
    
    This was not caught by tests because the tests were already
    canonicalizing the tableaux.
    Tests are now fixed too.

--- test/test_graphs.jl
+++ test/test_graphs.jl
@@ -4,7 +4,6 @@ function test_graphs()
     @testset "Graph states" begin
         for n in [1,test_sizes...]
             s = random_stabilizer(n)
-            canonicalize_gott!(s)
             g, h_idx, ip_idx, z_idx = graphstate(s)
             gates = graph_gatesequence(h_idx, ip_idx, z_idx)
             gate = graph_gate(h_idx, ip_idx, z_idx, nqubits(s))
@@ -15,6 +14,21 @@ function test_graphs()
             for gate in vcat(gates...) apply!(s1, gate) end
             @test canonicalize!(apply!(copy(s),c0)) == canonicalize!(s1) == canonicalize!(Stabilizer(g))
         end
+        # one more check, done manually, to ensure we are testing for cases
+        # where canonicalize_gott! is causing index permutation
+        # (a few of the random cases already cover that)
+        s = S"- _XZ
+              + _ZX
+              + ZXZ"
+        g, h_idx, ip_idx, z_idx = graphstate(s)
+        gates = graph_gatesequence(h_idx, ip_idx, z_idx)
+        gate = graph_gate(h_idx, ip_idx, z_idx, nqubits(s))
+        c0 = one(CliffordOperator,nqubits(s))
+        for gate in vcat(gates...) apply!(tab(c0), gate) end
+        @test c0==gate
+        s1 = copy(s)
+        for gate in vcat(gates...) apply!(s1, gate) end
+        @test canonicalize!(apply!(copy(s),c0)) == canonicalize!(s1) == canonicalize!(Stabilizer(g))
     end
 end
 
