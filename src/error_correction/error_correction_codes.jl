16d3fb821c92df6600f27208c270d71e71edfca8
Maria Gragera <m.gragera.garces@gmail.com>
2022-06-17 16:24:26 +0100

    changing name functions

--- src/error_correction/error_correction_codes.jl
+++ src/error_correction/error_correction_codes.jl
@@ -3,10 +3,10 @@
 module Codes
 abstract type Code end
 
-struct Shor9 <: Code
+struct Shor_code <: Code
 end
 
-struct Toric <: Code
+struct Toric_code <: Code
     l::Int
 end
 
@@ -14,7 +14,8 @@ struct CSSfromClassical <: Code
     classicalcode::Matrix{Bool}
 end
 
-"""docs"""
+"""documents"""
+
 function rate end
 
 function logicalqubits end
@@ -23,15 +24,15 @@ function physicalqubits end
 
 function codedistance end
 
-function H(code::Shor9) end
+function H(code::Shor_code) end
 
-function QuantumClifford.MixedDestabilizer(code::Shor9)
+function QuantumClifford.MixedDestabilizer(code::Shor_code)
     ...
 end
 
 ...
 
-function rate(code::Shor9) return 1//9 end
+function rate(code::Shor_code) return 1//9 end
 
 ... decoding algorithms and how to structure them
 end # module
