4435335ab94bd32016d2d982a9f98c31c58485a1
Maria Gragera <m.gragera.garces@gmail.com>
2022-06-19 14:41:12 +0100

    shor pauli matrices

--- src/error_correction/error_correction_codes.jl
+++ src/error_correction/error_correction_codes.jl
@@ -19,7 +19,16 @@ function codedistance end
 function H(code::Shor_code) end
 
 function QuantumClifford.MixedDestabilizer(code::Shor_code)
-    ...
+
+    Stabilizer([P"X",P"Y",P"Z"])
+
+    Stabilizer([0x2, 0x0],
+                Bool[0 1;
+                    1 0],
+                Bool[0 -i;
+                    i 1],
+                Bool[1 0;
+                    0 -1])
 end
 
 ...
