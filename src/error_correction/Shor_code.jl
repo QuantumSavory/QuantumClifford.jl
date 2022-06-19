855c0f67b329b390f6517f5650c125a61328a44e
Maria Gragera Garces <m.gragera.garces@gmail.com>
2022-06-19 20:25:19 +0100

    circuit building

--- src/error_correction/Shor_code.jl
+++ src/error_correction/Shor_code.jl
@@ -62,53 +62,59 @@ function rate(code::Shor_code) return 1//9 end
     #Step 0
     # initial_state = one(Stabilizer, N)
 
-    #Step 1
+    #Step 1: 1st set of  CNOT gates
     c1 = sCNOT(1,4)
     c2 = sCNOT(1,7)
 
-    #Step 2
+    #Step 2: 1st set of  Hadamard gates
     h1 = sHadamard(1)
     h2 = sHadamard(4)
     h3 = sHadamard(7)
 
-    #Step 3
+    #Step 3: 2nd set of  CNOT gates
     c3 = sCNOT(4,5)
     c4 = sCNOT(4,6)
     c5 = sCNOT(7,8)
     c6 = sCNOT(7,9)   
     
-    #Step 4
-    #error
+    #Step 4: Error
+    X
+    Z
 
-    #Step 5
+    #Step 5: 4th set of  CNOT GATES
     c7 = sCNOT(1,2)
     c8 = sCNOT(4,5)
     c9 = sCNOT(7,8)
 
-    #Step 6
+    #Step 6: 5th set of  CNOT gates
     c10 = sCNOT(1,2)
     c11 = sCNOT(4,5)
     c12 = sCNOT(7,8)
     
-    #Step 7
+    #Step 7: 1st set of  Toffoli gates
     #are toffoli gates represented by sCCNOT?
-    c10 = sCCNOT(2,3,1)
-    c11 = sCCNOT(5,6,4)
-    c12 = sCCNOT(9,8,7)
+    cc1 = sCCNOT(2,3,1)
+    cc2 = sCCNOT(5,6,4)
+    cc3 = sCCNOT(9,8,7)
 
-    #Step 8
+    #Step 8: 2nd set of  Haramard gates
     h1 = sHadamard(1)
     h2 = sHadamard(4)
     h3 = sHadamard(7)
 
-    #Step 9
+    #Step 9: 6th set of  CNOT gates
     c13 = sCNOT(1,4)
 
-    #Step 10
+    #Step 10: 7th set of CNOT gates
     c14 = sCNOT(1,7)
 
-    #Step 11
-    c13 = sCCNOT(7,4,1)
+    #Step 11: 2nd set of Toffoli gates
+    #Final gates
+    cc4 = sCCNOT(7,4,1)
+
+# This circuit performs a depolarization at rate `epsilon` to all qubits,
+circuit = [c2,c2,h1,h2,h3,c3,c4,c5,c6,"error",c7,c8,c9,c10,c11,c12,cc1,cc2,cc3,h1,h2,h3,,c13,c14,cc4]
 
 end # module
+
 
