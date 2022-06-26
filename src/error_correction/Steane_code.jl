1fa47ce25d7462510f08d0aaecf0a0111818498d
Maria Gragera <m.gragera.garces@gmail.com>
2022-06-26 20:55:23 +0100

    steane code parity matrix

---
+++ src/error_correction/Steane_code.jl
@@ -0,0 +1,9 @@
+#Parity check matrix 
+function QuantumClifford.MixedDestabilizer(code::Steade_code) #!!!UndefVarError
+
+    Stabilizer(Bool[1 0 0 1 0 1 1;
+                    0 1 0 1 1 0 1;
+                    0 0 1 1 1 1 0])
+end
+
+####
