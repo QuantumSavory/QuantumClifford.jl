c20622eec015109fe6233eec68b2a269a199bdde
Stefan Krastanov <stefan@krastanov.org>
2022-06-29 12:41:52 -0400

    Test with JET only in the presence of ENV variable
    Signed-off-by: Stefan Krastanov <stefan@krastanov.org>

--- test/runtests.jl
+++ test/runtests.jl
@@ -46,7 +46,7 @@ doset("graphs")             && include("./test_graphs.jl")
 doset("hash")               && include("./test_hash.jl")
 doset("entanglement")       && include("./test_entanglement.jl")
 doset("enumeration")        && include("./test_enumerate.jl")
-doset("jet")                && VERSION >  v"1.8" && include("./test_jet.jl")
+doset("jet")                && haskey(ENV,"QUANTUMCLIFFORD_JET_TEST") && ENV["QUANTUMCLIFFORD_JET_TEST"]=="true" && include("./test_jet.jl")
 doset("allocations")        && VERSION >= v"1.7" && include("./test_allocations.jl")
 doset("doctests")           && VERSION == v"1.7" && include("./doctests.jl")
 
