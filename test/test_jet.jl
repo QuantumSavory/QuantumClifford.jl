d41c9b793dc7f7b97445a81b5ce9d09f262f90d5
Stefan Krastanov <stefan@krastanov.org>
2022-07-03 11:05:48 -0400

    Try to clean up JET tests and CI

--- test/test_jet.jl
+++ test/test_jet.jl
@@ -1,7 +1,10 @@
 using JET
+using Base.Threads: nthreads
 
 function test_jet()
     @testset "JET checks" begin
+        n = nthreads()
+
         @test_broken isempty(JET.get_reports(@report_call random_destabilizer(10)))
         @test_broken isempty(JET.get_reports(@report_call random_stabilizer(10)))
         @test_broken isempty(JET.get_reports(@report_call random_clifford(10)))
@@ -12,10 +15,14 @@ function test_jet()
         c2 = random_clifford(2)
         @test isempty(JET.get_reports(@report_call project!(s,p)))
         @test isempty(JET.get_reports(@report_call apply!(s,p)))
-        @test_broken isempty(JET.get_reports(@report_call apply!(s,c)))
-        @test_broken isempty(JET.get_reports(@report_call apply!(s,sCNOT(1,2))))
-        @test_broken isempty(JET.get_reports(@report_call apply!(s,sHadamard(1))))
-        @test_broken isempty(JET.get_reports(@report_call apply!(s,c2,[1,2])))
+        if n>1
+            @test_broken isempty(JET.get_reports(@report_call apply!(s,c)))
+        else
+            @test isempty(JET.get_reports(@report_call apply!(s,c)))
+        end
+        @test_broken isempty(JET.get_reports(@report_opt apply!(s,sCNOT(1,2))))
+        @test_broken isempty(JET.get_reports(@report_opt apply!(s,sHadamard(1))))
+        @test_broken isempty(JET.get_reports(@report_opt apply!(s,c2,[1,2])))
         @test isempty(JET.get_reports(@report_call canonicalize!(s)))
         @test isempty(JET.get_reports(@report_call canonicalize_gott!(s)))
         @test isempty(JET.get_reports(@report_call canonicalize_rref!(s)))
@@ -23,8 +30,13 @@ function test_jet()
         @test isempty(JET.get_reports(@report_opt QuantumClifford._project!(s,p)))
         @test isempty(JET.get_reports(@report_opt QuantumClifford._apply!(s,p)))
         @test_broken isempty(JET.get_reports(@report_opt QuantumClifford._apply!(s,c)))
-        @test_broken isempty(JET.get_reports(@report_opt QuantumClifford._apply!(s,sCNOT(1,2))))
-        @test_broken isempty(JET.get_reports(@report_opt QuantumClifford._apply!(s,sHadamard(1))))
+        if n>1
+            @test_broken isempty(JET.get_reports(@report_opt QuantumClifford._apply!(s,sCNOT(1,2))))
+            @test_broken isempty(JET.get_reports(@report_opt QuantumClifford._apply!(s,sHadamard(1))))
+        else
+            @test isempty(JET.get_reports(@report_opt QuantumClifford._apply!(s,sCNOT(1,2))))
+            @test isempty(JET.get_reports(@report_opt QuantumClifford._apply!(s,sHadamard(1))))
+        end
         @test_broken isempty(JET.get_reports(@report_opt QuantumClifford._apply!(s,c2,[1,2])))
         @test isempty(JET.get_reports(@report_opt QuantumClifford._canonicalize!(s)))
         @test isempty(JET.get_reports(@report_opt QuantumClifford._canonicalize_gott!(s)))
