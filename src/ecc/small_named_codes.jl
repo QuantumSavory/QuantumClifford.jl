e0e5767f5798d3479a2d06743d04e3a2ba5a064a
Stefan Krastanov <stefan@krastanov.org>
2022-07-05 15:50:11 -0400

    initial ECC setup

---
+++ src/ecc/small_named_codes.jl
@@ -0,0 +1,14 @@
+struct Shor9 <: AbstractECC end
+
+parity_checks(c::Shor9) = S"ZZ_______
+                            _ZZ______
+                            ___ZZ____
+                            ____ZZ___
+                            ______ZZ_
+                            _______ZZ
+                            XXXXXX___
+                            ___XXXXXX"
+
+code_n(c::Shor9) = 9
+
+#...
