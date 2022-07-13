8d29aad5bab6822bfe2b1e71f4405aa5722a7d4f
Maria Gragera <m.gragera.garces@gmail.com>
2022-07-13 12:23:01 +0100

    Correction syntatic error (def S)

--- src/ecc/shorcode.jl
+++ src/ecc/shorcode.jl
@@ -1,15 +1,15 @@
 struct Shor9 <: AbstractECC end
 
 
-parity_checks(c::Shor9) = Sh9"ZZ_______
-                              _ZZ______
-                              ___ZZ____
-                              ____ZZ___
-                              ______ZZ_
-                              _______ZZ
-                              XXXXXX___
-                              ___XXXXXX"
+parity_checks(c::Shor9) = S"ZZ_______
+                            _ZZ______
+                            ___ZZ____
+                            ____ZZ___
+                            ______ZZ_
+                            _______ZZ
+                            XXXXXX___
+                            ___XXXXXX"
 
 code_n(c::Shor9) = 9
 
-parity_matrix(c::Shor9) = stab_to_gf2(Sh9)
+parity_matrix(c::Shor9) = stab_to_gf2(parity_checks(c::Shor9))
--- src/ecc/steanecode.jl
+++ src/ecc/steanecode.jl
@@ -3,26 +3,26 @@ struct Steane7 <: AbstractECC end
 
 
 #Steane 5 qubit code
-parity_checks(c::Steane5) = St5"XZZX_
-                                _XZZX
-                                X_XZZ
-                                ZX_XZ"
+parity_checks(c::Steane5) = S"XZZX_
+                              _XZZX
+                              X_XZZ
+                              ZX_XZ"
 
 code_n(c::Steane5) = 5
 
-parity_matrix(c::Steane5) = stab_to_gf2(St5)
+parity_matrix(c::Steane5) = stab_to_gf2(parity_checks(c::Steane5))
 
 
 #steane 7 qubit code
-parity_checks(c::Steane7) = St7"___XXXX
-                                _XX__XX
-                                X_X_X_X
-                                ___ZZZZ
-                                _ZZ__ZZ
-                                Z_Z_Z_Z"
+parity_checks(c::Steane7) = S"___XXXX
+                              _XX__XX
+                              X_X_X_X
+                              ___ZZZZ
+                              _ZZ__ZZ
+                              Z_Z_Z_Z"
 
 code_n(c::Steane7) = 7
 
-parity_matrix(c::Steane7) = stab_to_gf2(St7)
+parity_matrix(c::Steane7) = stab_to_gf2(parity_checks(c::Steane7))
 
 
