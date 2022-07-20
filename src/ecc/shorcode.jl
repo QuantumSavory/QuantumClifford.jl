f4440d2acd00e89879e335142f03ea055d0781b4
Maria Gragera <m.gragera.garces@gmail.com>
2022-07-20 10:58:44 +0100

    additional functionalities to the shor and steane codes (missing automation)

--- src/ecc/shorcode.jl
+++ src/ecc/shorcode.jl
@@ -1,6 +1,9 @@
 struct Shor9 <: AbstractECC end
 
+"""The number of physical qubits in a code."""
+code_n(c::Shor9) = 9
 
+"""Parity check tableau of a code."""
 parity_checks(c::Shor9) = S"ZZ_______
                             _ZZ______
                             ___ZZ____
@@ -10,6 +13,60 @@ parity_checks(c::Shor9) = S"ZZ_______
                             XXXXXX___
                             ___XXXXXX"
 
-code_n(c::Shor9) = 9
-
 parity_matrix(c::Shor9) = stab_to_gf2(parity_checks(c::Shor9))
+
+syndrome_circuit(c::Shor9) = #measurement circuit rows / 1 per row: conditional gates
+#simple type: straight forward (naive), fault tolerant (3 types) -Neil, STeane, SHor
+
+#Enconding circuit ----------------------------------
+c1 = sCNOT(1,4)
+c2 = sCNOT(1,7)
+
+h1 = sHadamard(1)
+h2 = sHadamard(4)
+h3 = sHadamard(7)
+
+c3 = sCNOT(4,5)
+c4 = sCNOT(4,6)
+c5 = sCNOT(7,8)
+c6 = sCNOT(7,9) 
+
+ """
+Encoding shor circuit 
+                  ┌───┐                           
+q_1: ──■────■─────┤ H ├──■─────■─────
+       |    |     └───┘┌─┴──┐  |     
+q_2: ──────────────────┤CNOT├────────
+       |    |          └────┘┌─┴──┐  
+q_3: ────────────────────────┤CNOT├──
+     ┌─┴──┐ |     ┌───┐      └────┘   
+q_4: ┤CNOT├───────┤ H ├──■─────■─────
+     └────┘ |     └───┘┌─┴──┐  |      
+q_5: ──────────────────┤CNOT├────────
+            |          └────┘┌─┴──┐  
+q_6: ────────────────────────┤CNOT├──
+          ┌─┴──┐ ┌───┐       └────┘  
+q_7: ─────┤CNOT├─┤ H ├──■─────■──────
+          └────┘ └───┘┌─┴──┐  |      
+q_8: ─────────────────┤CNOT├─────────
+                      └────┘┌─┴──┐                          
+q_9: ───────────────────────┤CNOT├───
+                            └────┘   
+"""
+
+encoding_circuit(c::Shor9) = [c1,c2,h1,h2,h3,c3,c4,c5,c6]
+#----------------------------------------------------------------
+
+code_k(c::Shor9) = 1
+
+code_s(c::Shor9) = 8 
+
+rate(c::Shor9) = 1/8 
+
+distance(c::Shor9) = 3 
+
+logx_ops(c::Shor9) = P"XXXXXXXXX"
+                       
+logz_ops(c::Shor9) = P"ZZZZZZZZZ"
+
+isdegenerate(c::Shor9) = true 
--- src/ecc/steanecode.jl
+++ src/ecc/steanecode.jl
@@ -12,6 +12,33 @@ code_n(c::Steane5) = 5
 
 parity_matrix(c::Steane5) = stab_to_gf2(parity_checks(c::Steane5))
 
+syndrome_circuit(c::Steane5) = #TODO
+
+#Enconding circuit ----------------------------------
+c1 = sCNOT(1,6)
+c2 = sCNOT(2,6)
+C3 = sCNOT(3,6)
+c4 = sCNOT(4,6)
+C5 = sCNOT(5,6)
+#https://cs269q.stanford.edu/projects2019/stabilizer_code_report_Y.pdf
+
+encoding_circuit(c::Steane5) = [] #TODO: unsure of source #can there be circuits with X&Z log_ops??
+#----------------------------------------------------------------
+
+code_k(c::Steane5) = 1
+
+code_s(c::Steane5) = 4 
+
+rate(c::Steane5) = 1/4
+
+distance(c::Steane5) = 3 
+
+logx_ops(c::Steane5) = P"XXXXX" 
+                       
+logz_ops(c::Steane5) = P"ZZZZZ" 
+
+isdegenerate(c::Steane5) = false 
+
 
 #steane 7 qubit code
 parity_checks(c::Steane7) = S"___XXXX
@@ -25,4 +52,39 @@ code_n(c::Steane7) = 7
 
 parity_matrix(c::Steane7) = stab_to_gf2(parity_checks(c::Steane7))
 
+syndrome_circuit(c::Steane7) = #TODO
 
+#Enconding circuit ----------------------------------
+c1 = sCNOT(1,2)
+c2 = sCNOT(1,3)
+
+h1 = sHadamard(5)
+h2 = sHadamard(6)
+h3 = sHadamard(7)
+
+c3 = sCNOT(7,1)
+C4 = sCNOT(7,1)
+c5 = sCNOT(7,4)
+c6 = sCNOT(6,1)
+c7 = sCNOT(6,3)
+c8 = sCNOT(6,4)
+c9 = sCNOT(5,2)
+c10 = sCNOT(5,3)
+c11 = sCNOT(5,2)
+
+encoding_circuit(c::Steane7) = [c1,c2,h1,h2,h3,c3,c4,c5,c6, c7, c8, c9, c10, c11]
+#----------------------------------------------------------------
+
+code_k(c::Steane7) = 1
+
+code_s(c::Steane7) = 6 
+
+rate(c::Steane7) = 1/6 
+
+distance(c::Steane7) = 3 
+
+logx_ops(c::Steane7) = P"XXXXXXX" 
+                       
+logz_ops(c::Steane7) = P"ZZZZZZZ" 
+
+isdegenerate(c::Steane7) = false 
