65dff3ba18efd2bcf46debb4588c437667f6424a
Stefan Krastanov <stefan@krastanov.org>
2022-07-20 20:58:09 -0400

    Additional tests for basic functionality

--- test/test_hash.jl
+++ test/test_hash.jl
@@ -3,6 +3,8 @@ function test_hash()
         @test hash(P"X") == hash(P"X")
         @test hash(S"X") == hash(S"X")
         @test hash(C"X Z") == hash(C"X Z")
+        r = random_destabilizer(2)
+        @test hash(r) == hash(copy(r))
     end
 end
 
