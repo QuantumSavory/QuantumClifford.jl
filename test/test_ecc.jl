a5342af319263bcd07e1030689a043a28ebb3cf7
Maria Gragera <m.gragera.garces@gmail.com>
2022-07-28 23:15:43 +0100

    testing changes - yet to b implementable

--- test/test_ecc.jl
+++ test/test_ecc.jl
@@ -1,4 +1,5 @@
 
+#SEPARATE IMPLMENTATION
 
 #Syndrome circuit testing-------------------------------------
 function test_naive_syndrome_circuit()
@@ -14,23 +15,3 @@ end
 
 
 #--------------------------------------------------------------
-
-#Stabilizer testing -------------------------------------------
-
-function stab_looks_good(s)
-    c = canonicalize!(copy(s))
-    nrows, ncols = size(c)
-    all((c.phases .== 0x0) .| (c.phases .== 0x2)) || return false
-    H = stab_to_gf2(c)
-    good_indices = reduce(|,H,dims=(1,))
-    good_indices = good_indices[1:end÷2] .| good_indices[end÷2+1:end]
-    colsok = ncols>nrows || all(good_indices) # TODO, this can be stricter
-    colsok || return false
-    good_indices = reduce(|,H,dims=(2,))
-    rowsok = nrows>ncols || all(good_indices) # TODO, this can be stricter
-    rowsok || return false
-    check_allrowscommute(c) || return false
-    return true
-end
-
-#----------------------------------------------------------------
