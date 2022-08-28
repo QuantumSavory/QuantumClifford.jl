93f3a5505244813466bfad76f813f0cb5abe10ca
Maria Gragera <m.gragera.garces@gmail.com>
2022-08-28 18:33:09 +0100

    reorg parity check function torix

--- src/ecc/toriccode.jl
+++ src/ecc/toriccode.jl
@@ -7,15 +7,17 @@ struct Toric <: AbstractECC
     n #qubits
 end 
 
-#Toric n qubit code => nx(n-1) parity check matrix
-parity_checks(c::Toric)
-    code_n = c.n
-    #matrix with 1 zero per conection point
-    empty_grid_matrix = zeros(code_n, code_n -1)
+code_n(c::Toric) = c.n
 
-    #HOW TO I CONNECT THEM
+#Parity checks ----------------------------------
 
-end
+#matrix with 1 zero per conection point 
+empty_grid_matrix = zeros(code_n, code_n -1)
+    
+#HOW TO I CONNECT THEM
+
+parity_checks(c::Toric) = 1 #temporary
+#-----------------------------------------------------
 
 parity_matrix(c::Toric) = stab_to_gf2(parity_checks(c))
 
@@ -23,6 +25,5 @@ parity_matrix(c::Toric) = stab_to_gf2(parity_checks(c))
 
 encoding_circuit(c::Toric) = [] #TODO
 #-----------------------------------------------------
-code_n(c::Toric) = c.n
 
 
