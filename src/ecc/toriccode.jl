514930f8e70fceeab7ab638a4ce444f5a03d4435
Maria Gragera <m.gragera.garces@gmail.com>
2022-08-28 17:58:43 +0100

    Toric changes

--- src/ecc/toriccode.jl
+++ src/ecc/toriccode.jl
@@ -1,7 +1,13 @@
-#IN PROGRESS
-struct Toric <: AbstractECC end 
+import .ECC
 
-#Toric x qubit code
+using LinearAlgebra
+using .ECC
+
+struct Toric <: AbstractECC 
+    n #qubits
+end 
+
+#Toric n qubit code => nx(n-1) parity check matrix
 parity_checks(c::Toric) = S"XZZX_
                             _XZZX
                             X_XZZ
@@ -13,18 +19,12 @@ parity_matrix(c::Toric) = stab_to_gf2(parity_checks(c))
 
 encoding_circuit(c::Toric) = [] #TODO
 #-----------------------------------------------------
-#These functions are current in ECC
-#=
-naive_syndrome(c::Toric) #Syndrome circuit
 
-code_n(c::Toric) = #variable input dependent
+code_s(c::Toric) = length(parity_checks(c))
 
-code_k(c::Toric) = #variable input dependent
-
-code_s(c::Toric) = #variable input dependent
+code_k(c::Toric) = c.n - code_s(c)
 
 rate(c::Toric) = code_k(c::Toric)/code_s(c::Toric)
-=#
 
 logx_ops(c::Toric) = #variable input dependent
 
@@ -32,5 +32,4 @@ logz_ops(c::Toric) = #variable input dependent
 
 logy_ops(c::Toric) = #variable input dependent
 
-isdegenerate(c::Toric) = #variable input dependent
 
