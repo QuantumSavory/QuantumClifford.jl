1719d7ce25908cbd74d7afa38953171f1e2016f8
Maria Gragera <m.gragera.garces@gmail.com>
2022-08-31 11:32:15 +0100

    fixed compiling issue

--- src/ecc/ECC.jl
+++ src/ecc/ECC.jl
@@ -4,38 +4,41 @@ module ECC
     abstract type AbstractECC end
     #export AbstractECC
 
-    function sCNOT_gatechange(gate::sCNOT) sCNOT(gate.q2,gate.q1) end
+    function sCNOT_gatechange(gate::sCNOT) 
+        sCNOT(gate.q2,gate.q1) 
+    end
 
     """The encoding circuit of a given code."""
     function encoding_circuit end 
 
     """Parity check tableau of a code."""
-    function parity_checks end
+    function parity_checks  end
 
     """The number of physical qubits in a code."""
     function code_n end
-
+    
     """The number of stabilizer checks in a code."""
     function code_s(c::AbstractECC) 
         s = length(parity_checks(c))
         return s
     end
-
+    
     """The number of logical qubits in a code."""
     function code_k(c::AbstractECC)
         if c == Surface
             k = 1
         else
             k = code_n(c) - code_s(c)
+        end
         return k
     end
-
+    
     """The rate of a code."""
     function rate(c::AbstractECC)
         rate = code_k(c)/code_s(c)
         return rate
     end
-
+    
     """Naive syndrome circuit""" #TODO: add the other 3 types of syndrome circuit
     function naive_syndrome_circuit(c::AbstractECC)
         naive_sc = []
@@ -60,13 +63,13 @@ module ECC
 
     """The distance of a code."""
     function distance end
-
+    
     """Parity matrix of a code."""
     function parity_matrix(c::AbstractECC) 
-        paritym = stab_to_gf2(parity_checks(c)) 
+        paritym = stab_to_gf2(parity_checks(c::AbstractECC)) 
         return paritym
     end
-
+    
     """Logical X operations of a code."""
     function logx_ops end # can be computed from the parity checks
 
@@ -101,13 +104,8 @@ module ECC
     #------TODO--------------------------------
     # Surface codes
     #include("./surface_codes.jl")
-    #include...
     # repetition codes
     # concatenated codes
     # color codes
-    # LDPC expander and hypergraph codes
-    # reed muller and reed solomon codes
 
-    # Add others ------------------------
-
-end
+end #module
