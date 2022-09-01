0b729c786d06bd707136cee5933a5dc17b8f8fbe
Maria Gragera <m.gragera.garces@gmail.com>
2022-09-01 20:10:46 +0100

    x gates

--- src/ecc/toriccode.jl
+++ src/ecc/toriccode.jl
@@ -41,13 +41,12 @@ function checks(c::Toric)
                 end               
             end
             
-            for location in range(1,c.n*c.n)
+            for location in range(1,c.n)
                 #Z gates
                 #within the rox bounds
                 #last location within length bounds
                 #want to make sure w don't take midle row values  
                 if z_n <= (c.n) && (location+2j+1) <= (c.n) && location in evenrows
-                    @show location
                     z_locations[z_n,location] = 1
                     z_locations[z_n,location+j] = 1
                     z_locations[z_n,location+j+1] = 1
@@ -60,26 +59,53 @@ function checks(c::Toric)
 
                 #X gates -not running as expected
                 if (location+1+2j) <= (c.n) && x_n <= (c.n)
-                    x_locations[x_n,location] = 1
-                    x_locations[x_n,location+1] = 1
-                    x_locations[x_n,location+1+j] = 1
-                    x_locations[x_n,location+2j+1] = 1                   
-                elseif (location+1) <= (c.n) && x_n <= (c.n)
-                    x_locations[x_n,location] = 1
-                    x_locations[x_n,location+1] = 1
-                elseif (location+1+j) <= (c.n) && x_n <= (c.n)
-                    x_locations[x_n,location] = 1
-                    x_locations[x_n,location+1+j] = 1
-                #=
-                elseif isdefined(location,x_n) && isdefined(location+1+j,x_n) 
-                    x_locations[location,x_n] = 1
-                    x_locations[location+1+j,x_n] = 1
-                =#
-                end #pretty sure there are more combos
+                    for e in evenrows
+                        for o in oddrows
+                            for l in evenrows
+                                for p in oddrows
+                                    
+                                    if (location == e && location + j == o) || (location == o && location + j == e) # top and left
+                                        
+                                        if ((location == e && location + j +1 != o) || (location == o && location + j +1 != e)) && ((location == e && location + j != o) || (location == o && location + j != e))#making sure we are not at a border
+                                            
+                                            if (location == o && location+j == e && location+1+j == l && x_n,location+2j+1 == p) || (location == e && location+j == o && location+1+j == p && x_n,location+2j+1 == l)
+                                                # 4 sides
+                                                x_locations[x_n,location] = 1
+                                                x_locations[x_n,location+j] = 1
+                                                x_locations[x_n,location+1+j] = 1
+                                                x_locations[x_n,location+2j+1] = 1
+                                            
+                                            elseif (location == o && location+j == e && x_n,location+2j+1 == p) || (location == e && location+j == o && x_n,location+2j+1 == l) #top left
+                                                x_locations[x_n,location] = 1
+                                                x_locations[x_n,location+j] = 1
+                                                x_locations[x_n,location+2j+1] = 1
+                                            
+                                            elseif (location == o && location+j+1 == e && x_n,location+2j+1 == p) || (location == e && location+j+1 == o && x_n,location+2j+1 == l) #top right
+                                                x_locations[x_n,location] = 1
+                                                x_locations[x_n,location+1+j] = 1
+                                                x_locations[x_n,location+2j+1] = 1
+                                            end
+                                        end 
 
+                                    elseif (location+j) <= (c.n) && x_n <= (c.n) #right bottom
+                                        if (location == e && location + j == o) || (location == o && location + j == e)
+                                            x_locations[x_n,location] = 1
+                                            x_locations[x_n,locationj] = 1
+                                        end
+
+                                    elseif (location+j+j/2) <= (c.n) && x_n <= (c.n) #left bottom
+                                        if (location == e && location + j +j/2 == o) || (location == o && location +j +j/2 == e)
+                                            x_locations[x_n,location] = 1
+                                            x_locations[x_n,location+j+j/2] = 1
+                                        end
+                                    end
+                                end
+                            end
+                        end
+                    end
+                end
                 z_n += 1 #next z
                 x_n += 1 #next x
-
             end #for 
 
             #making X & Z into bool
