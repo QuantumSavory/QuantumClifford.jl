9b8f4f204b4a61420f056ba0d3222c06f4abe854
Anthony Micciche <micciche@AnthonynoMacBook-Pro.local>
2023-04-15 17:44:37 -0400

    Pauli frames now work using the Stabilizer data structure from QuantumClifford. Also, the liklihood of an error occurring is now a parameter to the function. Furthermore, a few speedups were implemented

--- src/experimental/PauliFrame.jl
+++ src/experimental/PauliFrame.jl
@@ -1,39 +1,20 @@
 # Runs any number of  pauli frames on a given circuit
-# Sample use: 
-# circuit = [("CNOT", [1,4]), ("PE", [2]),("CNOT", [2,4]),("CNOT", [2,5]),("CNOT", [3,5]), ("MZ", [4]),("MZ", [5])]
-# ref = [0,0];
-# pauli_frames(5,circuit,ref,5)
-# TODO change this input to reflect the Tableau in the rest of Quantum Clifford
-# TODO make the Pauli Error channel take a parameter to determine the probability of non identity error 
-# TODO various speedups
-
-# Inputs: Number of qubits, array of operations (gate/measure/error, bits), array of reference measurements, 
-# number of frames to simulate, Boolean flag for output suppression
-
-# Outputs: An 𝐹×𝑀 matrix, where 𝐹 is the number of frames and 𝑀 was the number of measurements
-
-function pauli_frames(qubits, circuit,ref_m, numframes=1, showFrame=true)
+# p is the probability that a pauli error channel will produce a non identity error
+#      -> Then after this, the three Pauli operators are all equally likely.
+using Random
+using QuantumClifford
+function pauli_frames(qubits, circuit,ref_m, numframes=1, p=0.25, showFrame=true)
     # Copying these because these variables will be modified later
     circuit = deepcopy(circuit)
     ref_m = deepcopy(ref_m)
-    
-    # Need number of measurments for reshaping returnable
+    # Need number of measurements for reshaping returnable
     num_m = length(ref_m)
     
-    # Macros
-    X = 1; Z=2
-    
     # Declaration of the frame data structure
-    # It's size is the number of qubits by 2 by the number of frames
-    frame = zeros(Bool, qubits, 2, numframes)
+    frame = zero(Stabilizer, numframes, qubits)
+    # Randomly apply Z error with prob 1/2 on init
+    frame.tab.xzs[2,:] = rand(0:2^(qubits)-1,qubits,1)
     
-    # randomly apply Z error with prob 1/2 on init
-    # Maybe there's a faster tensor operation to do the same thing?
-    for i in 1:numframes
-        for n in 1:qubits
-                frame[n, Z, i]  =   rand(Bool)
-        end
-    end
     if showFrame
         println("\nInitial Pauli frame\n", frame)
     end
@@ -52,43 +33,46 @@ function pauli_frames(qubits, circuit,ref_m, numframes=1, showFrame=true)
         println(op)
         
         # op was CNOT
-        if op[1]=="CNOT"
+        if op[1]==:"CNOT"
             bit_c = op[2][1]
             bit_t = op[2][2]
-
-            frame[bit_t,X, :] .=  frame[bit_t,X,:]  .⊻ frame[bit_c,X,:] # This  ⊻  is XOR
+            apply!(frame, tCNOT,  [bit_c, bit_t])
             
         # op was Pauli error channel
-        # TODO add parameter that determines the probability of non I error
-        # Currently, {I,X,Y,Z} are all equally likely
-        elseif op[1]=="PE"
-            bit_t = op[2]
-            # There's probably a faster way to do this than a for loop
-            #  Not sure if XOR correctly represents multiplying paulis into the frame? 
-            for i in 1:numframes
-                frame[bit_t, :, i]  =  [rand(Bool) ⊻ frame[bit_t, X, i][1]  , rand(Bool)⊻ frame[bit_t, Z, i][1]]
+        elseif op[1]==:"PE"
+            bit_t = op[2][1]
+            # There's probably a faster way to do this than a for loop?
+            # The difficulty is that I need to randomly choose indices for each frame
+            # Not sure if XOR correctly represents multiplying paulis into the frame? 
+            xyz_error = [(1,1), (1,2), (2,2)]
+            frame_error = zeros(numframes)
+            rand!(frame_error)
+            for f in 1:numframes    
+                if frame_error[f] < p
+                    error = rand(xyz_error)
+                    frame.tab.xzs[error[1]:error[2],f] .= frame.tab.xzs[error[1]:error[2],f] .⊻ (2)^(bit_t-1)
+                end
             end
             
         # op was measurement in Z basis
-        elseif op[1]=="MZ"
+        elseif op[1]==:"MZ"
             ref = popfirst!(ref_m)
-            bit_t = op[2]
+            bit_t = op[2][1]
+            
+            # Vector that represents, for each frame, whether there was an X flip on bit_t
+            x_flips = .!iszero.(frame.tab.xzs[1,:] .& 2^(bit_t-1))
+            
             # add the simulated measurement to the returnable
-            append!(sim_m, frame[bit_t,X,:]  .⊻ ref[1])
+            append!(sim_m, x_flips  .⊻ ref[1])
             
+            # TODO
             # Reset target bit's frame - the Z component is randomly 0 or 1
-            # Potential for speedup here I think
-            # TODO Commented out for now because it makes reading the final frame more confusing for debugging
-            #for i in 1:numframes
-            #    frame[bit_t, :, i]  =  [0  , rand(Bool)]
-            #end
+            # For now, we assume nothing else will happen after a qubit is measured
             
-        #  Hadamard swaps X and Z
-        elseif op[1]== "H"
+        #  Hadamard 
+        elseif op[1]== :"H"
             bit_t = op[2]
-            temp = frame[bit_t,X, :]
-            frame[bit_t,X, :] = frame[bit_t,Z, :]
-            frame[bit_t,Z, :] = temp
+            apply!(frame, tHadamard,  bit_t)
         end
         
     end
@@ -97,12 +81,17 @@ function pauli_frames(qubits, circuit,ref_m, numframes=1, showFrame=true)
        println("\nPauli frame after at end of circuit\n", frame)
     end
  
-    # return matrix: each row is a frame, each column is a measurement
+    #  return matrix: each row is a frame, each column is a measurement. The measurements refer to 
+    # whether the result of the corresponding ith reference measurement flipped
+    #               M1        M2  
+    #   F1[   M1,F1  M2,F1]
+    #  F1[  M1,F2   M2,F2]
     # 
-    # M1F1 M2F1
-    # M1F2 M2F2
-    
-    return reshape(sim_m, (numframes, num_m))
+    return reshape(sim_m, (numframes, num_m)), frame
 end
 
+circuit = [(:"CNOT", [1,4]), (:"PE", [2]),(:"CNOT", [2,4]),(:"CNOT", [2,5]),(:"CNOT", [3,5]), (:"MZ", [4]),(:"MZ", [5])]
+ref = [0,0];
 
+m, f = pauli_frames(5,circuit,ref,5, 0.1)
+print(m)
