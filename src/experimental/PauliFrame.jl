fb9a4e39b76efbd9df0cae7860cc4b780d4851cc
Anthony Micciche <micciche@AnthonynoMacBook-Pro.local>
2023-04-27 23:37:44 -0400

    Added docstring documentation to PauliFrame.jl

--- src/experimental/PauliFrame.jl
+++ src/experimental/PauliFrame.jl
@@ -1,5 +1,5 @@
 # A Pauli Frame simulation can be created by interacting directly with the struct and its methods, or by calling the 
-# pauliFrameCircuitHandler function, which will run an entire circuit for you, calling the appropiate methods
+# pauliFrameCircuitHandler or circuitSim function, which will run an entire circuit for you, calling the appropiate methods
 # and initalizing a PauliFrame struct.
 #
 # Currently, all 1 and 2 qubit gates from QuantumClifford work. However, only Z basis measurement is available.
@@ -17,6 +17,28 @@
 using Random
 using QuantumClifford
 
+"""
+    PauliFrame(numframes, numqubits, reference_measurements)
+
+The frame field holds a tableau. This tableau is not to be viewed as a normal stabilizer tableau, although
+it does conjugate the same under Clifford operations. Each row in the tableau refers to a single frame. Each column a qubit.
+The values stored are X,Y,Z,_ and indicate whether the corresponding pauli operation is waiting to be applied to that qubit.
+
+The provided reference measurements will be used to create the measurements field of this struct. They will also be 
+used when performing measurement. The indices of the provided measurements must match the secondary value of future measurements
+when doing sMZ(measure_this_qubit, index_of_reference_provided). See also [`apply!(frame::PauliFrame, op:sMZ)`](@ref)
+
+# Examples
+```julia-repl
+julia> frame = PauliFrame(numframes, numqubits, [0,1]])
+
+julia> apply!(frame, sMZ(5,1)) 
+julia> apply!(frame, sMZ(1,2)) 
+
+The first ['sMZ']@ref measured qubit 5 and treated 0 as its reference measurement.
+The second ['sMZ']@ref measured qubit 1 and treated 1 as its reference measurement.
+```
+"""
 struct PauliFrame
     numframes::Int
     qubits::Int
@@ -27,44 +49,67 @@ struct PauliFrame
     PauliFrame(numframes, qubits, ref) = new(numframes, qubits, ref, zero(Stabilizer, numframes, qubits), zeros(Bool, numframes, length(ref)))
 end
 
-struct PauliError
+"""
+This type is used to be able to write Pauli Error Channel gates like PauliError(2, 0.75).\n
+In this case, a pauli error is applied on qubit 2 with probability 0.75.\n
+See also [`pauliFrameCircuitHandler`](@ref), [`circuitSim`](@ref) for examples. 
+"""
+struct PauliError <: AbstractSingleQubitOperator
     qubit::Int
-    p::Float16 #probability of non identity error
+    p::Float16 
 end
 
-# Call this after initialization to apply random Z errors with 50% probability on each qubit in each frame.
-# This is essential for simulating non deterministic circuits
+"""
+    initZ(f::PauliFrame)
+
+Inject random Z errors over all frames and qubits for the supplied PauliFrame with probability 0.5.
+
+Calling this after initialization is essential for simulating any non-deterministic circuit.
+
+# Examples
+```julia-repl
+julia> frame = PauliFrame(numframes, qubits, ref_measurements)
+julia> initZ(frame)
+```
+"""
 function initZ(f::PauliFrame)
     f.frame.tab.xzs[2,:] = rand(0:2^(f.qubits)-1,f.numframes,1)
     return f
 end
 
-# applies a symbolic gate from QuantumClifford
+""" Applies a symbolic gate from QuantumClifford to the frames of the provided PauliFrame instance."""
 function apply!(f::PauliFrame, op) 
     QuantumClifford._apply!(f.frame, op; phases=Val(false))
     return f
 end
 
-# Measure in the Z basis. ref is the reference measurement and has value 0 or 1. bit_t is the qubit being measured.
-# Returns a vector of length equal to the number of frames. Each index corresponds to a frame.
-# The value of each index is the new measurement value (0 or 1) on the provided qubit.
 # TODO Currently, this assumes that after measurement, that qubit will not be used. 
 #   - to fix this, we could reset the measured qubit to I or Z, but what about entanglement?
-# takes sMZ data structure
-#   - sMZ.qubit is the qubit to be measured
-#   - sMZ.bit is the refers to the place in the reference array provided at PauliFrame init
+"""
+    apply!(frame::PauliFrame, op::sMZ)
+
+Applies [`sMZ`](@ref) to the PauliFrame instance. 
+
+On initialization of the PauliFrame instance, a set of reference measurements must be given. 
+The sMZ gate here must be of the form sMZ(measure_this_qubit, index_of_reference_provided).
+See also [`pauliFrameCircuitHandler`](@ref), [`circuitSim`](@ref) for examples, and [`PauliFrame`](@ref) for more info.
+"""
 function apply!(frame::PauliFrame, op::sMZ)
     bit_t = op.qubit
     # Vector that represents, for each frame, whether there was an X flip on bit_t
     x_flips = .!iszero.(frame.frame.tab.xzs[1,:] .& 2^(bit_t-1))
 
-    # apply flips to the reference measurement
     ref = frame.ref[op.bit]
     frame.measurements[:,op.bit] = x_flips  .⊻ ref
     return frame
 end
 
-# Inserts a random pauli error into all frames in frame with probabiltiy p, on bit bit_t
+"""
+    apply!(frame::PauliFrame, op::PauliError)
+
+Inserts a random pauli error into all frames in the provided PauliFrame instance with probabiltiy p, on bit bit_t. 
+See also [`PauliError`](@ref)
+"""
 function apply!(frame::PauliFrame, op::PauliError)
     p = op.p; bit_t = op.qubit
     # TODO Not 100% sure if XOR correctly represents multiplying paulis into the frame?
@@ -80,24 +125,30 @@ function apply!(frame::PauliFrame, op::PauliError)
     return frame
 end
 
-# Simulates an entire circuit for the user. Here is sample input:
-#   circuit = [sX(1), sX(1), sCNOT(1,4), QuantumClifford.PauliError(2,0.75), sCNOT(2,4), sCNOT(2,5), sCNOT(3,5), sMZ(4,1), sMZ(5,2)]
-#   ref = [0,0]
-#
-#   m, f = QuantumClifford.pauliFrameCircuitHandler(5,circuit,ref,10)
-#
-# Inputs: Number of qubits, a circuit (refer above), a vector of reference measurements, and the number of frames desired.
-#
-# p is the probability that a pauli error channel will produce a non identity error
-#      -> Then after this, X,Y,Z are all equally likely.
-#
-# Output 1: a matrix where each row is a frame, each column is a measurement.
-#        M1     M2  
-#  F1[ M1,F1  M2,F1]
-#  F1[ M1,F2  M2,F2]
-#
-# Output 2: Another output is the the Stabilizer data structure from QuantumClifford. Assuming all measurements happen
-#          at the end of the circuit, it represents the pauli frame values right before measurement starts.
+"""
+    pauliFrameCircuitHandler(qubits, circuit, ref_m,  numframes=1)
+
+Simulates an entire circuit for the user. 
+
+# Inputs: 
+    - Number of qubits, a circuit (refer above), a vector of reference measurements, and the number of frames desired.
+    
+# Output 1: 
+    A matrix where each row is a frame, each column is a measurement.
+           M1     M2  
+     F1[ M1,F1  M2,F1]
+     F1[ M1,F2  M2,F2]
+    
+# Output 2: 
+    Another output is the the Stabilizer data structure from QuantumClifford. 
+    Assuming all measurements happen at the end of the circuit, it represents the pauli frame values right before measurement starts.
+# Examples
+```julia-repl
+julia> circuit = [sX(1), sX(1), sCNOT(1,4), QuantumClifford.PauliError(2,0.75), sCNOT(2,4), sCNOT(2,5), sCNOT(3,5), sMZ(4,1), sMZ(5,2)]
+julia> ref = [0,0]
+julia> m, f = QuantumClifford.pauliFrameCircuitHandler(5,circuit,ref,10)
+```
+"""
 function pauliFrameCircuitHandler(qubits, circuit, ref_m,  numframes=1)
     frame = PauliFrame(numframes, qubits, ref_m); initZ(frame)
     for op in circuit
@@ -105,7 +156,20 @@ function pauliFrameCircuitHandler(qubits, circuit, ref_m,  numframes=1)
     end
     return  frame.measurements, frame.frame
 end
-# Alternative to the above. This allows greater future flexability with other frameworks
+""" 
+    circuitSim(state::PauliFrame, circuit)
+
+Alternative to  [`pauliFrameCircuitHandler`](@ref). The difference is that this takes a PauliFrame instance, and
+    returns the modified instance.
+
+# Examples
+```julia-repl
+julia> ghz_circuit = [sHadamard(1), sCNOT(1,2), sCNOT(1,3), sMZ(1,1), sMZ(2,2), sMZ(3,3)]
+julia> ref = [0,0,0]
+julia> frame = QuantumClifford.PauliFrame(10^6, 3, ref); QuantumClifford.initZ(frame)
+julia> f = QuantumClifford.circuitSim(frame, ghz_circuit); m = f.measurements; frame = f.frame 
+```
+"""
 function circuitSim(state::PauliFrame, circuit)
     for op in circuit
         apply!(state, op)      
