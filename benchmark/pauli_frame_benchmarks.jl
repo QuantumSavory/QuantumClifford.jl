d14e46cdd46bfd14690d314eae637c779fe3e72a
Anthony Micciche <amicciche@umass.edu>
2023-05-04 23:46:29 -0400

    small change to the benchmark experiment numbers

--- benchmark/pauli_frame_benchmarks.jl
+++ benchmark/pauli_frame_benchmarks.jl
@@ -28,9 +28,10 @@ SUITE["pauliframe"]["pauliframesim"] = BenchmarkGroup(["pauliframesim"])
 SUITE["pauliframe"]["pauliframesim"]["100"] = @benchmarkable QuantumClifford.pauliFrameCircuitHandler(101,circuit,ref_m,NUMFRAMES) setup=(circuit = copy(x_diag_circuit(100)))
 SUITE["pauliframe"]["pauliframesim"]["1000"] = @benchmarkable QuantumClifford.pauliFrameCircuitHandler(1001,circuit,ref_m,NUMFRAMES) setup=(circuit = copy(x_diag_circuit(1000)))
 SUITE["pauliframe"]["pauliframesim"]["1000_1000"] = @benchmarkable QuantumClifford.pauliFrameCircuitHandler(1001,circuit,ref_m,1000) setup=(circuit = copy(x_diag_circuit(1000)))
-SUITE["pauliframe"]["pauliframesim"]["10^6_100"] = @benchmarkable QuantumClifford.pauliFrameCircuitHandler(10^6+1,circuit,ref_m,100) setup=(circuit = copy(x_diag_circuit(1000)))
+SUITE["pauliframe"]["pauliframesim"]["10^6_100"] = @benchmarkable QuantumClifford.pauliFrameCircuitHandler(10^6+1,circuit,ref_m,100) setup=(circuit = copy(x_diag_circuit(10^6)))
 
 
 SUITE["pauliframe"]["quantumclifford"] = BenchmarkGroup(["quantumclifford"])
 SUITE["pauliframe"]["quantumclifford"]["100"] = @benchmarkable  mctrajectory!(state, circuit) setup=(state= Register(one(Stabilizer, 101), [false]); circuit = copy(x_diag_circuit(100)))
 SUITE["pauliframe"]["quantumclifford"]["1000"] = @benchmarkable mctrajectory!(state, circuit)  setup=(state= Register(one(Stabilizer, 1001), [false]); circuit = copy(x_diag_circuit(1000)))
+SUITE["pauliframe"]["quantumclifford"]["10000"] = @benchmarkable mctrajectory!(state, circuit)  setup=(state= Register(one(Stabilizer, 10001), [false]); circuit = copy(x_diag_circuit(10000)))
