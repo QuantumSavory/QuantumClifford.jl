c5cfe44ca93734b54e98a9097a7c508426417e28
Adrian Ariton <adrianariton@192.168.1.5>
2023-05-07 16:47:57 +0300

    Added some more testing

--- test/test_stabtocliff.jl
+++ test/test_stabtocliff.jl
@@ -11,5 +11,26 @@ using Test
         cltab = tab(cl)
         @test Stabilizer(cltab[end÷2+1:end]) == (stab);
     end
+
+
+    for i in 1:10
+
+        gnd4 = S"ZIII
+                 IZII
+                 IIZI
+                 IIIZ"
+
+        stab = random_stabilizer(4)
+        cl = CliffordOperator(stab)
+        cltab = tab(cl)
+        @test apply!(gnd4, cl) == (stab);
+    end
+    ## TODO: More tests??
+end
+
+@testset "Basic Stabs to Clifford" begin
+    stabSWAP = S"IZ
+                 ZI"
+    @test CliffordOperator(stabSWAP) == tSWAP
     ## TODO: More tests??
 end
