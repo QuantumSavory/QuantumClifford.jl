3ddeaa3379cde87b3402edfda9c388a3e9e8333c
Stefan Krastanov <stefan@krastanov.org>
2023-05-22 20:52:35 -0400

    ECC implementation

---
+++ src/ecc/ECC.jl
@@ -0,0 +1,101 @@
+module ECC
+
+using QuantumClifford
+abstract type AbstractECC end
+
+"""The encoding circuit of a given code."""
+function encoding_circuit end
+
+"""Parity check tableau of a code."""
+function parity_checks end
+
+"""The number of physical qubits in a code."""
+function code_n end
+
+"""The number of stabilizer checks in a code."""
+function code_s(c::AbstractECC)
+    s = length(parity_checks(c))
+    return s
+end
+
+"""The number of logical qubits in a code."""
+function code_k(c::AbstractECC)
+    k = code_n(c) - code_s(c)
+    return k
+end
+
+"""The rate of a code."""
+function rate(c::AbstractECC)
+    rate = code_k(c)//code_s(c)
+    return rate
+end
+
+"""Naive syndrome circuit"""
+function naive_syndrome_circuit(c::AbstractECC)
+    naive_sc = []
+
+    ancilla_bit = 1
+    # ancilla_qubit = code_n(c) + ancilla_bit
+    pc = parity_checks(c)
+    for check in pc
+        ancilla_qubit = code_n(c) + ancilla_bit
+        for qubit in 1: code_n(c)
+            if check[qubit] == (1,0) # TODO simplify this branch using sXCX and similar gates
+                h1 = sHadamard(qubit)
+                c1 = sCNOT(qubit, ancilla_qubit)
+                h2 = sHadamard(qubit)
+                push!(naive_sc, h1)
+                push!(naive_sc, c1)
+                push!(naive_sc, h2)
+            elseif check[qubit] == (0,1)
+                c1 = sCNOT(qubit, ancilla_qubit)
+                push!(naive_sc, c1)
+            end # TODO make sure it works if you have a Y check
+        end
+        mz = sMZ(ancilla_qubit, ancilla_bit)
+        push!(naive_sc, mz)
+        ancilla_bit +=1
+    end
+
+    return naive_sc
+end
+
+"""The distance of a code."""
+function distance end
+
+"""Parity matrix of a code, given as a stabilizer tableau."""
+function parity_matrix(c::AbstractECC)
+    paritym = stab_to_gf2(parity_checks(c::AbstractECC))
+    return paritym
+end
+
+"""Logical X operations of a code."""
+function logx_ops(c::AbstractECC)
+    MixedDest = MixedDestabilizer(parity_checks(c))
+    logicalxview(MixedDest)
+end
+
+"""Logical Z operations of a code."""
+function logz_ops(c::AbstractECC)
+    MixedDest = MixedDestabilizer(parity_checks(c))
+    logicalzview(MixedDest)
+end
+
+"""Is the code degenerate"""
+function isdegenerate(c::AbstractECC, errors) # TODO test
+    syndromes = Set()
+    for e in errors
+        s = parity_matrix(c) * e
+        if s in syndromes
+            return true
+        end
+        syndromes.add(s)
+    end
+    return false
+end
+
+include("./bitflipcode.jl")
+include("./shorcode.jl")
+include("./steanecode.jl")
+
+end #module
---
+++ test/test_ecc.jl
@@ -0,0 +1,86 @@
+using Test
+using QuantumClifford
+using QuantumClifford.ECC: AbstractECC, Steane5, Steane7, Shor9, Bitflip3, naive_syndrome_circuit, code_n, parity_checks, encoding_circuit, code_s, code_k, rate, distance,logx_ops, logz_ops, isdegenerate
+
+codes = [
+    Bitflip3(),
+    #Steane5(), # TODO test_naive_syndrome fails
+    Steane7(),
+    Shor9(),
+]
+
+function test_phys_log_op_encoding(c::AbstractECC)
+    # encode k physical qubits into n physical qubits (turning them into k logical qubits)
+    # apply operations in an encoded or unencoded fashion
+    # compare results
+    physicalqubit = random_stabilizer(code_k(c))
+
+    gate = rand((:x,:z))
+    target = rand(1:code_k(c))
+
+    physicalgate, logicalgate =
+    if gate==:x
+        P"X",logx_ops(c)[target]
+    elseif gate == :z
+        P"Z",logz_ops(c)[target]
+    end
+
+    #run 1
+    #start physical state
+    physicalqubit1 = copy(physicalqubit)
+    #apply physical gate
+    apply!(physicalqubit1,physicalgate)
+    #encode into logical state
+    bufferqubits1 = one(Stabilizer,code_s(c))
+    logicalqubit1 = physicalqubit1⊗bufferqubits1 # pad up the k physical qubits into a state of n physical qubits
+    mctrajectory!(logicalqubit1, encoding_circuit(c))
+
+    #run 2
+    #start same physical state
+    physicalqubit2 = copy(physicalqubit)
+    #encode logical state
+    bufferqubits2 = one(Stabilizer,code_s(c))
+    logicalqubit2 = physicalqubit2⊗bufferqubits2
+    mctrajectory!(logicalqubit2, encoding_circuit(c))
+    #apply logical gate
+    apply!(logicalqubit2,logicalgate)
+
+    @test canonicalize!(logicalqubit1) == canonicalize!(logicalqubit2)
+end
+
+@testset "physical vs optical operators - check of encoding circuit" begin
+    for c in codes, _ in 1:2
+        test_phys_log_op_encoding(c)
+    end
+end
+
+
+function test_naive_syndrome(c::AbstractECC)
+    # create a random logical state
+    unencoded_qubits = random_stabilizer(code_k(c))
+    bufferqubits = one(Stabilizer,code_s(c))
+    logicalqubits = unencoded_qubits⊗bufferqubits
+    mctrajectory!(logicalqubits, encoding_circuit(c))
+    # measure using `project!`
+    s1 = copy(logicalqubits)
+    @show s1
+    syndrome1 = [project!(s1, check)[3] for check in parity_checks(c)]
+    @show syndrome1
+    # measure using `naive_syndrome_circuit`
+    naive_circuit = naive_syndrome_circuit(c)
+    ancillaryqubits = one(Stabilizer,code_s(c))
+    s2 = copy(logicalqubits)
+    syndrome2 = Register(s2⊗ancillaryqubits, falses(code_s(c)))
+    mctrajectory!(syndrome2, naive_circuit)
+    @test all(syndrome1 .== 0)
+    @test all(bitview(syndrome2) .== 0)
+    @test bitview(syndrome2) == syndrome1.÷2
+
+    # TODO test when there is potential for errors / non-commuting operators
+end
+
+@testset "naive syndrome circuits - zero syndrome for logical states" begin
+    for c in codes, _ in 1:2
+        test_naive_syndrome(c)
+    end
+end
