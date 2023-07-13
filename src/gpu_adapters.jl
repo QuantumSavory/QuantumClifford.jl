d81ebb6217c1a0945b1d19a1bf0baece14dae5a1
Shayan-P <shayanpardis82@gmail.com>
2023-07-13 16:36:59 -0700

    simplify gpu_adapters

--- src/gpu_adapters.jl
+++ src/gpu_adapters.jl
@@ -1,8 +1,11 @@
 GPU_DEPENDENCIES = ["CUDA"]
 
-# todo this is problematic because the error message is not specific
-# todo make a better error stack trace?
-_throw_error_gpu_dependencies(T::Type) = error("either this function is not supported on type $T or dependencies are not imported.\ndependencies: $GPU_DEPENDENCIES")
+"""
+copies the memory content of the object to GPU
+"""
+function to_cpu end
 
-to_cpu(gpu_object::T) where {T} = _throw_error_gpu_dependencies(T)
-to_gpu(cpu_object::T) where {T} = _throw_error_gpu_dependencies(T)
+"""
+copies the memory content of the object to CPU
+"""
+function to_gpu end
