9220c7af13a208372dadea67ac6d19964ab60e38
Shayan-P <shayanpardis82@gmail.com>
2023-07-13 15:47:06 -0700

    fix error message when CUDA is not imported

--- src/gpu_adapters.jl
+++ src/gpu_adapters.jl
@@ -1,7 +1,8 @@
 GPU_DEPENDENCIES = ["CUDA"]
 
 # todo this is problematic because the error message is not specific
-_throw_error_gpu_dependencies(T::Type) = error("either or this function is not supported on type $T on dependencies are not imported.\ndependencies: $GPU_DEPENDENCIES")
+# todo make a better error stack trace?
+_throw_error_gpu_dependencies(T::Type) = error("either this function is not supported on type $T or dependencies are not imported.\ndependencies: $GPU_DEPENDENCIES")
 
 to_cpu(gpu_object::T) where {T} = _throw_error_gpu_dependencies(T)
 to_gpu(cpu_object::T) where {T} = _throw_error_gpu_dependencies(T)
