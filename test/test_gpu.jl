3f2f0aca09c52be77575c2134674ebbc9e3359f1
Shayan-P <shayanpardis82@gmail.com>
2023-07-21 13:54:50 -0400

    add fastrow tests

--- test/test_gpu.jl
+++ test/test_gpu.jl
@@ -2,43 +2,68 @@ using Test
 using QuantumClifford
 using CUDA
 
+function apply_single_qubit_and_compare(n, s, s_gpu)
+    qbitidx = (((rand(Int) % n) + n) %n) + 1
+    op = random_clifford1(qbitidx)
+    apply!(s_gpu, op)
+    apply!(s, op)
+    if to_cpu(s_gpu) != s
+        throw("result of cpu and gpu differ in single qubit operation. n=$n iteration=$iteration operation=$op")
+    end
+end
+
+function apply_single_or_double_qubit_and_compare(n, s, s_gpu)
+    op = if rand(Int) % 2 == 0
+        qbitidx = (((rand(Int) % n) + n) %n) + 1
+        random_clifford1(qbitidx)
+    else
+        # todo what other gates can we use? how to randomly generate two qubit operation?
+        # can we use random_clifford(2) with this function?
+        qbitidx1 = (((rand(Int) % n) + n) %n) + 1
+        qbitidx2 = (((rand(Int) % n) + n) %n) + 1
+        sCNOT(qbitidx1, qbitidx2)
+    end
+    apply!(s_gpu, op)
+    apply!(s, op)
+    if to_cpu(s_gpu) != s
+        throw("result of cpu and gpu differ in single qubit operation. n=$n iteration=$iteration operation=$op")
+    end
+end
+
 @testset "GPU" begin
+    CUDA.allowscalar(false) # todo how to add this to all tests.
+
     @test begin
-        p = P"_IZXY"
+        p = random_pauli(3)
         p_gpu = to_gpu(p)
         typeof(p_gpu.xz) <: CUDA.CuArray # todo this is a bad test because it depends on representation of data in QuantumClifford. Change later...
     end
     @test begin
-        s = S"-XX
-              +ZZ"
+        s = random_stabilizer(3)
         s_gpu = to_gpu(s)
         typeof(tab(s_gpu).xzs) <: CUDA.CuArray # todo this is a bad test because it depends on representation of data in QuantumClifford. Change later...
     end
 
     @test begin
-        s = to_gpu(S"XX XZ")
-        op = SingleQubitOperator(sHadamard(1))
-        apply!(s, op) # todo rewrite this with normal apply not _apply
-        correct = S"ZX ZZ"
-        to_cpu(s) == correct
+        for n in [2, 4, 8, 100, 500]
+            s = random_stabilizer(n)
+            s_gpu = to_gpu(s)
+            for iteration in 1:100
+                apply_single_qubit_and_compare(n, s, s_gpu)
+            end
+        end
+        true
     end
 
     @test begin
-        s = random_stabilizer(10, 10)
-        s_gpu = to_gpu(s)
-        op = SingleQubitOperator(sHadamard(5))
-        apply!(s_gpu, op) # todo rewrite this with normal apply not _apply
-        apply!(s, op)
-        to_cpu(s_gpu) == s
-    end
-
-    @test begin
-        s = random_stabilizer(10, 10)
-        s_gpu = to_gpu(s)
-        op = sCNOT(2, 3)
-        apply!(s_gpu, op) # todo rewrite this with normal apply not _apply 
-        apply!(s, op)
-        to_cpu(s_gpu) == s
+        for n in [2, 4, 8, 100, 500]
+            s = random_stabilizer(n)
+            s_gpu = to_gpu(s)
+            for iteration in 1:100
+                apply_single_or_double_qubit_and_compare(n, s, s_gpu)
+            end
+        end
+        true
     end
 
     @test begin
@@ -50,13 +75,37 @@ using CUDA
             compactify_circuit(circuit)
         end
 
-        CUDA.allowscalar(false) # todo how to add this to all tests.
-
         frames = QuantumClifford._create_pauliframe(ccircuit; trajectories=10)
         cpu_frames = to_cpu(frames)
         gpu_frames = to_gpu(frames)
         cpu_result = pftrajectories(cpu_frames, ccircuit)
         gpu_result = pftrajectories(gpu_frames, ccircuit)
         (cpu_result.frame == to_cpu(gpu_result.frame)) && (cpu_result.measurements == to_cpu(gpu_result.measurements))
+    end
+
+    @test begin
+        # test fastrow
+        for n in [2, 4, 8, 100, 500]
+            s = fastrow(random_stabilizer(n))
+            s_gpu = fastrow(to_gpu(s))
+            for iteration in 1:100
+                # how to check if after each iteration we are still fastrow
+                apply_single_or_double_qubit_and_compare(n, s, s_gpu)
+            end
+        end
+        true
+    end
+
+    @test begin
+        # test fastcolumn
+        for n in [2, 4, 8, 100, 500]
+            s = fastcolumn(random_stabilizer(n))
+            s_gpu = fastcolumn(to_gpu(s))
+            for iteration in 1:100
+                # how to check if after each iteration we are still fastrow
+                apply_single_or_double_qubit_and_compare(n, s, s_gpu)
+            end
+        end
+        true
     end
 end
