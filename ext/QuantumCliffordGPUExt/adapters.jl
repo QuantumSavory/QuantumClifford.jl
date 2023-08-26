949ada43b5d209526aafc6c3c74a0465554e9eef
Shayan-P <shayanpardis82@gmail.com>
2023-08-26 13:39:36 -0400

    add InnerGPUType=UInt32, InnerCPUType=UInt64

--- ext/QuantumCliffordGPUExt/adapters.jl
+++ ext/QuantumCliffordGPUExt/adapters.jl
@@ -1,24 +1,29 @@
-# const InnerGPUType = UInt64;
+const InnerGPUType = UInt32;
+const InnerCPUType = UInt64;
 # todo. make the conversion types consiting with this...
 # also define a cpu default type?!
 
-to_gpu(array::AbstractArray) = CuArray(array);
+to_gpu(array::AbstractArray) = CuArray(array)
 to_cpu(array::AbstractArray) = Array(array);
+
+# changes the type to InnerGPUType or InnerCPUType as well as copying to cpu/gpu
+to_gpu_reinterpret(array::AbstractArray) = CuArray(reinterpret(InnerGPUType, collect(array)))
+to_cpu_reinterpret(array::AbstractArray) = Array(reinterpret(InnerCPUType, collect(array)))
 
 # maybe change the format of storing the data in gpu array 
 # so that it is more convinient to work with them on gpu?
 # todo later add some type checking to avoid copying (or throw error) if the data is already on gpu/cpu
 to_gpu(tab::QuantumClifford.Tableau) =
-    QuantumClifford.Tableau(to_gpu(tab.phases), tab.nqubits, to_gpu(tab.xzs))
+    QuantumClifford.Tableau(to_gpu(tab.phases), tab.nqubits, to_gpu_reinterpret(tab.xzs))
 
 to_cpu(tab::QuantumClifford.Tableau) =
-    QuantumClifford.Tableau(to_cpu(tab.phases), tab.nqubits, to_cpu(tab.xzs))
+    QuantumClifford.Tableau(to_cpu(tab.phases), tab.nqubits, to_cpu_reinterpret(tab.xzs))
 
 to_gpu(pauli::QuantumClifford.PauliOperator) =
-    QuantumClifford.PauliOperator(to_gpu(pauli.phase), pauli.nqubits, to_gpu(pauli.xz))
+    QuantumClifford.PauliOperator(to_gpu(pauli.phase), pauli.nqubits, to_gpu_reinterpret(pauli.xz))
 
 to_cpu(pauli::QuantumClifford.PauliOperator) =
-    QuantumClifford.PauliOperator(to_cpu(pauli.phase), pauli.nqubits, to_cpu(pauli.xz))
+    QuantumClifford.PauliOperator(to_cpu(pauli.phase), pauli.nqubits, to_cpu_reinterpret(pauli.xz))
 
 to_gpu(stabilizer::QuantumClifford.Stabilizer) =
     Stabilizer(to_gpu(tab(stabilizer)))
