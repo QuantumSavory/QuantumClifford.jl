fc6a8be9cfa85398dc286e1889b0cdbf3665ba1e
Stefan Krastanov <stefan@krastanov.org>
2023-12-31 15:55:49 -0500

    benchmark cleanup and selfhosting

--- benchmark/plotttfxbenchmarks.jl
+++ benchmark/plotttfxbenchmarks.jl
@@ -8,7 +8,8 @@ using Statistics
 using Glob
 
 ##
-
+cd("QuantumCliffordBenchmarksLog/ttfxlogs")
+##
 files = glob("using_*.ttfxresults")
 records = []
 for f in files
@@ -18,26 +19,38 @@ for f in files
         continue
     end
     tag = m.captures[1]
+    version = VersionNumber(tag[2:end])
     threads = m.captures[2]
-    julia = m.captures[3]
-    pkgimport = minimum(parse.(Float64, readlines(f)))
-    task = minimum(parse.(Float64, readlines("task_tag=$(tag)-nthreads=$(threads)-julia=$(julia).ttfxresults")))
-    push!(records, (;tag, threads, julia, pkgimport, task))
+    juliastr = m.captures[3]
+    julia = juliastr=="nightly" ? v"1.999" : VersionNumber(m.captures[3])
+    pkgimportsamples = parse.(Float64, readlines(f))
+    pkgimport = isempty(pkgimportsamples) ? missing : minimum(parse.(Float64, readlines(f)))
+    tasksamples = parse.(Float64, readlines("task_tag=$(tag)-nthreads=$(threads)-julia=$(juliastr).ttfxresults"))
+    task = isempty(tasksamples) ? missing : minimum(tasksamples)
+    push!(records, (;version, threads, julia, pkgimport, task))
 end
 
 ##
 
 df = DataFrame(records)
+maxjversion = maximum(df[df.julia .!= v"1.999",:julia])
+df[df.julia .== v"1.999", :julia] .= VersionNumber(maxjversion.major, maxjversion.minor+1, 0)
 df[!,:total] .= df[:,:task] .+ df[:,:pkgimport]
 df = DataFrames.stack(df, [:total, :task, :pkgimport], variable_name=:x, value_name=:time)
+sort!(df) # by first column (version)
+
+df = df[df.time .!== missing,:] # otherwise the ticks are messed up, probably a bug
 
 layers = visual(Stairs, step=:center)
-axis = (;width = 500, height = 400, xticklabelrotation=-pi/3)
-facet = (;linkyaxes=:minimal)
+maxy = ceil(maximum(skipmissing(df.time)))
+axis = (;width = 800, height = 400, xticklabelrotation=-pi/2, yticks=(0:0.5:maxy))
+facet = (;
+    linkyaxes=:minimal,
+    )
 
 ##
 
 benchmark_data = data(df)
-mappings = mapping(:tag, :time, color=:julia, row=:x, col=:threads)
+mappings = mapping(:version, :time, color=:julia, row=:x, col=:threads)
 f = draw(benchmark_data * layers * mappings; facet, axis)
 save("../benchmarks_ttfx.png",f)
