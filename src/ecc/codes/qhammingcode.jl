a1901f5e75e8605b5dd6e96156726e8aabd78326
Feroz <93876775+Fe-r-oz@users.noreply.github.com>
2024-03-03 22:38:49 +0500

    Update qhammingcode.jl

--- src/ecc/codes/qhammingcode.jl
+++ src/ecc/codes/qhammingcode.jl
@@ -2,11 +2,8 @@ struct QHamming <:AbstractECC
     r::Int
 end
 
-struct InvalidRError <: Exception end  # Define a custom exception type
-
 function parity_checks(c::QHamming)
-    if !(3 <= c.r < 15)
-        throw(InvalidRError())  # Throw the exception if r is out of bounds
+    if (3 <= c.r <= 15) || error("The r parameter in QHamming has to be between 3 and 15, in order to create a valid code of tractable size")
     end
     n = 2^c.r
     k = 2^c.r - c.r - 2
