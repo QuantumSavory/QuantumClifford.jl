c8bc9f4e0ef8878b54a2d52e4e5bcef9146939ed
Feroz <93876775+Fe-r-oz@users.noreply.github.com>
2024-03-04 20:13:15 +0500

    Update ECC.jl

--- src/ecc/ECC.jl
+++ src/ecc/ECC.jl
@@ -19,6 +19,7 @@ export parity_checks, parity_checks_x, parity_checks_z,
     RepCode,
     CSS,
     Shor9, Steane7, Cleve8, Perfect5, Bitflip3,
+    QHamming,
     Toric,
     evaluate_decoder,
     CommutationCheckECCSetup, NaiveSyndromeECCSetup, ShorSyndromeECCSetup,
@@ -333,4 +334,5 @@ include("codes/steanecode.jl")
 include("codes/shorcode.jl")
 include("codes/clevecode.jl")
 include("codes/toric.jl")
+include("codes/qhammingcode.jl")
 end #module
