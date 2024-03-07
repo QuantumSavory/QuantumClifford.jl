733a7ad7bd0372d7a0fe2329366996e60c8f1cca
Fe-r-oz <ferozahmad7cf@gmail.com>
2024-03-07 05:52:14 +0500

    minor modifications

--- src/ecc/codes/n16_k10_d3.jl
+++ src/ecc/codes/n16_k10_d3.jl
@@ -2,7 +2,7 @@ struct N16_K10_D3 <: AbstractECC end
 
 code_n(c::N16_K10_D3) = 16
 
-parity_checks(c: N16_K10_D3) =   S"XXXXXXXXXXXXXXXX
+parity_checks(c::N16_K10_D3) =   S"XXXXXXXXXXXXXXXX
                                    ZZZZZZZZZZZZZZZZ
                                    _X_X_X_XZYZYZYZY 
                                    _X_XZYZYX_X_YZYZ
