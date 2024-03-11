1a7e9267cb1b78eab0b375fa43ef4b80137ffffe
Fe-r-oz <ferozahmad7cf@gmail.com>
2024-03-12 02:24:08 +0500

    Adding documentation for Gottesman codes aka quantum Hamming codes

--- src/ecc/codes/qhamming.jl
+++ src/ecc/codes/qhamming.jl
@@ -1,4 +1,14 @@
-"""This class implements the Gottesman codes [[2^j, 2^j-j-2,3]], also known as quantum Hamming codes, as described in Gottesman's 1997 thesis on stabilizer codes [gottesman1997stabilizer](@cite)."""
+"""This class implements the family of Gottesman codes [[2^j, 2^j - j - 2, 3]], also known as quantum Hamming codes, as described in Gottesman's 1997 thesis on stabilizer codes [Gottesman1997](@cite).
+
+Key details for the [[8, 3, 3]] code when j = 3 using Stabilizer Formalism:
+- [6506105](@cite) utilizes an explicit construction method for the stabilizer generators. The remaining m generators, apart from Mx and Mz, are constructed using the check matrix [Hm|AmHm], where Hm = [c0, c1, ..., c^2m − 1]. Each column ck (for k = 0, 1, ..., 2^m − 1) represents a binary vector corresponding to the integer k. Also, Am refers to any invertible m × m matrix devoid of fixed points such that Am​.s ≠ 0 and Am.s ≠ s for all s ∈ F₂ᵐ.
+- [Chao2017QuantumEC](@cite) introduces alternative stabilizer generators for the [[8, 3, 3]] code. It identifies permutations effective for all stabilizer generators except for X^⊗8 and Z^⊗8. However, these can be replaced with XXYZIYZI and ZZIXYIXY, respectively. The stabilizer generators are permuted to achieve desired properties, reflecting an alternative approach to constructing the stabilizer code.
+
+Notes:
+- This implementation adopts the Gottesman Notation for the [[8, 3, 3]] stabilizer as depicted in Table 3.3 on Page 22 on the Gottesman's thesis. Additionally, for j = 4, our [[16, 10, 3]] stabilizer stemming from QHamming(4) can be cross-verified from Table 8.1 on Page 91 as well [Gottesman1997](@cite).
+- The differences between [6506105](@cite), [Chao2017QuantumEC](@cite), and [Gottesman1997](@cite) primarily lie in the choice of stabilizer generators and their permutations.
+- The discrepancy in stabilizer generator representations underscores the flexibility within the stabilizer formalism, allowing for various valid choices and permutations, albeit with different notations and implementation details.
+"""
 
 struct QHamming <: AbstractECC
     j::Int
