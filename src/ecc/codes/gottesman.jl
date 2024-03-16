9a48cb270061c9f5129612bb487558a746c5e0d0
Stefan Krastanov <stefan@krastanov.org>
2024-03-16 15:55:41 -0400

    cleanup

--- src/ecc/codes/gottesman.jl
+++ src/ecc/codes/gottesman.jl
@@ -1,18 +1,13 @@
-"""This class implements the family of [[2^j, 2^j - j - 2, 3]] Gottesman codes, also known as [[2^j, 2^j - j - 2, 3]] quantum Hamming codes, as described in Gottesman's 1997 PhD thesis on 'Stabilizer Codes and Quantum Error Correction' [Gottesman1997](@cite).
+"""The family of `[[2ʲ, 2ʲ - j - 2, 3]]` Gottesman codes, also known as quantum Hamming codes, as described in [Gottesman's 1997 PhD thesis](@cite Gottesman1997).
 
-Key details for the [[8, 3, 3]] code when j = 3 using Stabilizer Formalism:
-- [6506105](@cite) utilizes an explicit construction method for the stabilizer generators. The remaining m generators, apart from Mx and Mz, are constructed using the check matrix [Hm|AmHm], where Hm = [c0, c1, ..., c^2m − 1]. Each column ck (for k = 0, 1, ..., 2^m − 1) represents a binary vector corresponding to the integer k. Also, Am refers to any invertible m × m matrix devoid of fixed points such that Am​.s ≠ 0 and Am.s ≠ s for all s ∈ F₂ᵐ.
-- [Chao2017QuantumEC](@cite) introduces alternative stabilizer generators for the [[8, 3, 3]] code. It identifies permutations effective for all stabilizer generators except for X^⊗8 and Z^⊗8. However, these can be replaced with XXYZIYZI and ZZIXYIXY, respectively. The stabilizer generators are permuted to achieve desired properties, reflecting an alternative approach to constructing the stabilizer code.
+You might be interested in consulting [yu2013all](@cite) and [chao2018quantum](@cite) as well.
 
-Notes:
-- This implementation adopts the Gottesman Notation for the [[8, 3, 3]] stabilizer code as depicted in Table 3.3 on Page 22 on the Gottesman's PhD thesis. Additionally, for j = 4, our [[16, 10, 3]] stabilizer code stemming from QHamming(4) can be cross-verified from Table 8.1 on Page 91 as well [Gottesman1997](@cite).
-- The differences between [6506105](@cite), [Chao2017QuantumEC](@cite), and [Gottesman1997](@cite) primarily lie in the choice of stabilizer generators and their permutations for the [[8, 3, 3]] stabilizer code.
+The ECC Zoo has an [entry for this family](https://errorcorrectionzoo.org/c/quantum_hamming)
 """
-
 struct Gottesman <: AbstractECC
     j::Int
     function Gottesman(j)
-        (j >= 3 && j < 21) || error("In `Gottesman(j)`, `j` must be ≥  3 in order to obtain a valid code and `j` must be < 21 to remain tractable")
+        (j >= 3 && j < 21) || error("In `Gottesman(j)`, `j` must be ≥  3 in order to obtain a valid code and < 21 to remain tractable")
         new(j)
     end
 end
