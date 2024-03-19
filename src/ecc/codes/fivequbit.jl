4411b85622688ce8e46a7b4029af20c4c286aeb8
Fe-r-oz <ferozahmad7cf@gmail.com>
2024-03-19 09:01:42 +0500

    adding iscss function

--- src/ecc/codes/fivequbit.jl
+++ src/ecc/codes/fivequbit.jl
@@ -1,5 +1,9 @@
 struct Perfect5 <: AbstractECC end
 
+function iscss(::Type{Perfect5})
+    return false
+end
+
 parity_checks(c::Perfect5) = S"XZZX_
                                _XZZX
                                X_XZZ
