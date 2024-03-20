870b234adedb91b8719c2148a26615bbad28cb62
Fe-r-oz <ferozahmad7cf@gmail.com>
2024-03-20 17:31:39 +0500

    making it concise

--- src/ecc/codes/reedmullercode.jl
+++ src/ecc/codes/reedmullercode.jl
@@ -24,8 +24,7 @@ function parity_checks(c::ReedMuller)
     r=c.r
     m=c.m
     rx = [construct(m, i) for i in 0:m - 1]
-    row_matrices = [reduce(vmult, [rx[i + 1] for i in S], init = ones(Int, 2^m)) 
-                     for s in 0:r for S in combinations(0:m - 1, s)]
+    row_matrices = [reduce(vmult, [rx[i + 1] for i in S], init = ones(Int, 2^m)) for s in 0:r for S in combinations(0:m - 1, s)]
     rows = length(row_matrices)
     cols = length(row_matrices[1])
     H = reshape(vcat(row_matrices...), cols, rows)'
