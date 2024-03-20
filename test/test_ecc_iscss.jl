063b417fc741a25d2c7c6f9d76f3b0e450f75e99
Feroz <93876775+Fe-r-oz@users.noreply.github.com>
2024-03-20 08:31:01 +0500

    Update test_ecc_iscss.jl

--- test/test_ecc_iscss.jl
+++ test/test_ecc_iscss.jl
@@ -9,7 +9,6 @@ function is_css_matrix(H)
         has_x = false
         has_z = false
         for j in 1:ncols
-            e = H[i, j]
             has_x |= H[i,j][1]
             has_z |= H[i,j][2]
             has_x && has_z && return false
@@ -18,18 +17,11 @@ function is_css_matrix(H)
     return true
 end
 
-function test_code(code)
-    H = parity_checks(code)
-    @test iscss(code) == is_css_matrix(H)
-    if iscss(code)
-        @test is_css_matrix(H) == true
-    end
-end
-
 known_all_codes = [Shor9(), Steane7(), Gottesman(3), Cleve8(), Perfect5(), Toric(8,8), CSS([0 1 1 0; 1 1 0 0], [1 1 1 1]), Bitflip3()]
 
 @testset "is CSS" begin
     for code in known_all_codes
-        test_code(code)
+        H = parity_checks(code)
+        @test iscss(code) == is_css_matrix(H)
     end
 end
