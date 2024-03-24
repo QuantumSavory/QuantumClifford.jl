56b5903a2b783591c6e55a05196b2a3e3ad73e64
Fe-r-oz <ferozahmad7cf@gmail.com>
2024-03-24 17:02:54 +0500

    Adding further checks and satisfying constraints

--- src/ecc/codes/quantumreedmuller.jl
+++ src/ecc/codes/quantumreedmuller.jl
@@ -5,6 +5,14 @@ The ECC Zoo has an [entry for this family](https://errorcorrectionzoo.org/c/quan
 struct QuantumReedMuller <: AbstractECC
     t::Int  
     r::Int  
+    function QuantumReedMuller(t, r)
+        if t == 1 && r == 2 
+            throw(ArgumentError("Invalid parameters: t = 1, r = 2 is not quantum code as it does not satisfy Hx.Hz' + Hz.Hx' = 0 [steane1999quantum](@cite)."))
+        elseif t < 0 || t > 10 || r < 1 || r > 10
+            throw(ArgumentError("Invalid parameters: r must be positive and < 10 and t >= 0 and < 10 in order to obtain a valid code and to remain tractable"))
+        end
+        new(t, r)
+    end
 end
 
 function iscss(::Type{QuantumReedMuller})
@@ -33,7 +41,11 @@ function generate_Dx(dx_rows, row_len, hdist)
 end
 
 function trows(t, r)
-    return 2^r + 2^r - sum(binomial.(r, 0:t)) -  sum(binomial.(r, 0:t - 1)) 
+    return (2^r + 2^r) - sum(binomial.(r, 0:t)) -  sum(binomial.(r, 0:t - 1)) 
+end 
+
+function k0(t, r)
+    return 2^r - binomial(r, t) -  sum(binomial.(r, 0:t - 1)) 
 end 
 
 function parity_checks(c::QuantumReedMuller)
@@ -42,24 +54,25 @@ function parity_checks(c::QuantumReedMuller)
     
     G1 = parity_checks(ReedMuller(t, r))
     pad_zeros = zeros(Int64, size(G1, 1), size(G1, 2))
-     
-    total_rows = trows(t, r)
-    if t == 2 && r == 1
-        Dx_rows = 0     
+   
+    if (trows(t, r) - 2^r) == 0
+        Dx_rows = r + t
+        k = k0(t, r)
+        G1 = G1[1:k, :]
+        pad_zeros = zeros(Int64, size(G1, 1), size(G1, 2))     
     else
-        Dx_rows = abs(total_rows - 2^r)
+        Dx_rows = abs(trows(t, r) - 2^r)
     end
-
+   
     Dx_cols = size(G1, 2)
-    Dx = generate_Dx(Dx_rows, Dx_cols, t)
-    
+    Dx = generate_Dx(Dx_rows, Dx_cols, 2^t)
+    Dx_cols = size(G1, 2)
     m = sum(binomial.(r, 0:t)) - sum(binomial.(r, 0:t - 1))
-    Dz = Dx
+    Dz = similar(Dx)
     for i in 1:m - 1
-        Dz[i, :] .= Dx[i + 1, :]
+        Dz[i, :] = Dx[i + 1, :]
     end
-    Dz[m, :] .= reverse(circshift(reverse(Dx[1, :]), 1))
-
+    Dz[m, :] = circshift(Dx[1, :], -m) 
     Hx = vcat(G1, pad_zeros, Dx)    
     Hz = vcat(pad_zeros, G1, Dz)    
 
