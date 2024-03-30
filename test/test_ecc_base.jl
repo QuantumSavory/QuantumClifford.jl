4ac40c31daf419438a927f8eaaf4f16271d66799
Fe-r-oz <ferozahmad7cf@gmail.com>
2024-03-31 04:35:44 +0500

    Adding extra tests

--- test/test_ecc_base.jl
+++ test/test_ecc_base.jl
@@ -9,7 +9,8 @@ const code_instance_args = Dict(
     Toric => [(3,3), (4,4), (3,6), (4,3), (5,5)],
     Surface => [(3,3), (4,4), (3,6), (4,3), (5,5)],
     Gottesman => [3, 4, 5],
-    CSS => (c -> (parity_checks_x(c), parity_checks_z(c))).([Shor9(), Steane7(), Toric(4,4)])
+    CSS => (c -> (parity_checks_x(c), parity_checks_z(c))).([Shor9(), Steane7(), Toric(4,4)]),
+    SteaneReedMuller => [(1,3), (1,5), (1,7), (2,4), (2,5)]
 )
 
 function all_testablable_code_instances(;maxn=nothing)
