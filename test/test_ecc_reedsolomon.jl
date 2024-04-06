258fc306b65f4e21a9deff8e47ee82f22eaafd5a
Fe-r-oz <ferozahmad7cf@gmail.com>
2024-04-06 06:20:51 +0500

    using Nemo Finite Fields

--- test/test_ecc_reedsolomon.jl
+++ test/test_ecc_reedsolomon.jl
@@ -1,14 +1,41 @@
 using Test
-using LinearAlgebra
+using Nemo
 using QuantumClifford
 using QuantumClifford.ECC
 using QuantumClifford.ECC: AbstractECC, ReedSolomon
 
-@testset "Test ReedSolomon(m, e) Matrix Rank" begin
-    for m in 5:20
-        for e in 2:10
-            G = generator_matrix(ReedSolomon(m, e))
-            @test rank(G) == m
+@testset "Test ReedSolomon(n, k, j) Matrix Degree" begin
+    #RS (7, 3) code
+    for n in 7:7
+        for k in 3:3
+            for j in 2:500
+                deg = ceil(Int, log2(n + 1))
+                t = div(n - k, 2)
+                GF2ͬ, a = finite_field(2, deg, "a")
+                P, x = GF2ͬ[:x]
+                poly = x - a^j
+                for i in 1:(2*t - 1)
+                   poly *= (x - a^(j + i))
+                end
+                @test degree(poly) - div(deg, 2) == n - k - 1
+            end
+        end
+    end
+
+    # RS(255, 223) code
+    for n in 255:255
+        for k in 223:223
+            for j in 2:500
+                deg = ceil(Int, log2(n + 1))
+                t = div(n - k, 2)
+                GF2ͬ, a = finite_field(2, deg, "a")
+                P, x = GF2ͬ[:x]
+                poly = x - a^j
+                for i in 1:(2*t - 1)
+                   poly *= (x - a^(j + i))
+                end
+                @test degree(poly) == n - k
+            end
         end
     end
 end
