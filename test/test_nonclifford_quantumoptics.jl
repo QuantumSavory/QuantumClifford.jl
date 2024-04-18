9ecb7bf49472a91fcdecb9ae8a1ad94ae7e66574
Stefan Krastanov <stefan@krastanov.org>
2024-04-18 14:46:11 -0400

    add smaller tests for `expect` on nonstabilizer states

--- test/test_nonclifford_quantumoptics.jl
+++ test/test_nonclifford_quantumoptics.jl
@@ -7,41 +7,54 @@ using Test
 using InteractiveUtils
 using Random
 
-#using QuantumCliffordQOpticsExt: _l0, _l1, _s₊, _s₋, _i₊, _i₋
-const qo = Base.get_extension(QuantumClifford, :QuantumCliffordQOpticsExt)
-const _l0 = qo._l0
-const _l1 = qo._l1
-const _s₊ = qo._s₊
-const _s₋ = qo._s₋
-const _i₊ = qo._i₊
-const _i₋ = qo._i₋
-
 qo_basis = SpinBasis(1//2)
 qo_tgate = sparse(identityoperator(qo_basis))
 qo_tgate.data[2,2] = exp(im*pi/4)
 
 ##
 
-for n in 1:5
-    i = rand(1:n)
-    stab = random_stabilizer(n)
-    genstab = GeneralizedStabilizer(stab)
-    ket = Ket(stab)
-    @test dm(ket) ≈ Operator(stab)
-    @test dm(ket) ≈ Operator(genstab)
+for s in [S"X", S"Y", S"Z", S"-X", S"-Y", S"-Z"]
+    for p in [P"X", P"Y", P"Z", P"-X", P"-Y", P"-Z"]
+        gs = GeneralizedStabilizer(s)
+        apply!(gs, pcT)
+        ρ = dm(qo_tgate*Ket(s))
+        @test Operator(gs) ≈ ρ
+        if isapprox(expect(p, gs), expect(Operator(p),ρ); atol=1e-5)
+        else
+            println("failure of expectation calculation for state = T*(", s, ") and observable = ", p)
+        end
+    end
+end
 
-    pauli = random_pauli(n; nophase=false, realphase=true)
-    qo_pauli = Operator(pauli)
+##
 
-    qo_bigtgate = n==1 ? qo_tgate : embed(qo_basis^n, i, qo_tgate)
-    bigtgate = embed(n,i, pcT)
-    @test qo_bigtgate ≈ Operator(bigtgate)
-
-    for step in 1:10
-        # apply!(ket, qo_bigtgate) TODO implement this API
-        ket = qo_bigtgate*ket
-        apply!(genstab, bigtgate)
+for _ in 1:10
+    for n in 1:1
+        i = rand(1:n)
+        stab = random_stabilizer(n)
+        genstab = GeneralizedStabilizer(stab)
+        ket = Ket(stab)
+        @test dm(ket) ≈ Operator(stab)
         @test dm(ket) ≈ Operator(genstab)
-        @test isapprox(expect(qo_pauli, ket), expect(pauli, genstab); atol=1e-5)
+
+        pauli = random_pauli(n; nophase=false, realphase=true)
+        qo_pauli = Operator(pauli)
+
+        qo_bigtgate = n==1 ? qo_tgate : embed(qo_basis^n, i, qo_tgate)
+        bigtgate = embed(n,i, pcT)
+        @test qo_bigtgate ≈ Operator(bigtgate)
+
+        for step in 1:10
+            # apply!(ket, qo_bigtgate) TODO implement this API
+            ket = qo_bigtgate*ket
+            apply!(genstab, bigtgate)
+            @test dm(ket) ≈ Operator(genstab)
+            if isapprox(expect(qo_pauli, ket), expect(pauli, genstab); atol=1e-5)
+            else
+                @show step
+                ref[] = (stab,pauli)
+                break
+            end
+        end
     end
 end
