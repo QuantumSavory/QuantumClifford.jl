a0420db77c9075d2847a0cc256102fb68eb903c0
Fe-r-oz <ferozahmad7cf@gmail.com>
2024-04-26 19:56:38 +0500

    adding Goppa Coefficient Matrix X

--- src/ecc/codes/classical/goppa.jl
+++ src/ecc/codes/classical/goppa.jl
@@ -6,14 +6,13 @@ An irreducible binary Goppa code is characterized by two key elements:
 
 - Support List `(L)`: A list containing `n` distinct elements from the finite field `GF(2^m)`. These elements must not be roots of the Goppa polynomial g(x).
 
-
 The sequence L plays a crucial role in defining the structure of an irreducible binary Goppa code. The steps involved in forming L are as follows:
 
 1. Field Selection: A finite field, `GF(2^m)`, is chosen based on the extension degree `m`.
 2. Goppa Polynomial: The input polynomial g(x) is used, which should be irreducible and have no repeated roots within `GF(2^m)`.
 3. Elements Selection for `L`: The sequence `L` consists of `n` distinct elements chosen from `GF(2^m)`. Importantly, no element in L can be a root of g(x).
 
-The parity-check matrix `(H)` of an irreducible binary Goppa code can be expressed as the product `(H = VD)` of two simpler matrices:
+The parity-check matrix `(H)` of an irreducible binary Goppa code can be expressed as the product `(H = XVD)` of three simpler matrices:
 
 - Vandermonde Matrix `(V)`:
   - Each row of `V` is constructed using the elements from the support list `(L)` raised to different powers.
@@ -27,7 +26,7 @@ The parity-check matrix `(H)` of an irreducible binary Goppa code can be express
 (L_1) ^ (t - 1)      (L_2) ^ (t - 1)         (L_3) ^ (t - 1)      ...        (L_n) ^ (t - 1)         
    
 - Diagonal Matrix `(D)`:
-  - Each diagonal element in D is related to the roots of the Goppa polynomial `(g(x))`.
+  - Each diagonal element in `D` is related to the roots of the Goppa polynomial `(g(x))`.
       
    1/g(L_1)                0                       0              ...            0
       0                 1/g(L_2)                   0              ...            0
@@ -37,6 +36,15 @@ The parity-check matrix `(H)` of an irreducible binary Goppa code can be express
       .                    .                       .                             .
       0                    0                       0              ...         1/g(L_n)
 
+- X Matrix `(X)`:
+  - Each element in `X` is related to the coefficients of the Goppa polynomial `(g(x))`
+
+     g_t                   0                       0              ...            0
+  g_(t - 1)               g_t                      0              ...            0 
+      .                    .                       .              ...            .
+      .                    .                       .              ...            .
+      .                    .                       .              ...            .
+     g_1                  g_2                     g_3             ...           g_t
 
 You might be interested in consulting [berlekamp1973goppa](@cite), [mceliece1978public](@cite), [patterson1975algebraic](@cite), [sugiyama1975method](@cite), [van1988classical](@cite), [bernstein2008attacking](@cite), [wirtz1988parameters](@cite) and [singh2019code](@cite) an as well.
 
@@ -62,9 +70,10 @@ function parity_checks(ga::Goppa)
     k = GF(2, r)
     po, b = polynomial_ring(k)
     gx = FqPolyRingElem
-    for i in 1:20
-        if is_irreducible(rand(po, 1:ga.t) + b^ga.t) == true
-            gx = rand(po, 1:ga.t) + b^ga.t
+    for i in 1:100
+        gx = rand(po, 1:ga.t) + b^ga.t
+        if is_irreducible(gx) == true
+            break
         end
     end
     L = FqFieldElem[]
@@ -75,35 +84,44 @@ function parity_checks(ga::Goppa)
         end
         i += 1
     end
-    V = Matrix{FqFieldElem}(undef, ga.t - 1, ga.n)
+    V = Matrix{FqFieldElem}(undef, ga.t, ga.n)
     for j in 1:ga.n
         V[1, j] = o ^ 0
     end
-    for i in 2:ga.t - 1
+    for i in 2:ga.t
         for j in 1:ga.n
             V[i, j] = L[j] ^ (i - 1)
         end
     end
     M = identity_matrix(GF2ͬ, ga.n)
-    for i in 1:ga.t - 1
+    for i in 1:ga.n
         for j in 1:ga.n
-            M[i, j] = getindex(M, i, j)*1/evaluate(gx, L[i])
+            M[i, j] = getindex(M, i, j)*1/evaluate(gx, L[j])
         end
     end
-    HField = Matrix{FqFieldElem}(undef, ga.t - 1, ga.n)
-    for i in 1:ga.t - 1
+    HField = Matrix{FqFieldElem}(undef, ga.t, ga.n)
+    for i in 1:ga.t
         for j in 1:ga.n
-            HField[i, j] = V[i, j]*M[i, j]
+            HField[i, j] = V[i, j] * M[i, j]
         end
     end
-    H = Matrix{Bool}(undef, r*(ga.t - 1), ga.n)
-    for i in 1:ga.t - 1
+    X = Matrix{FqFieldElem}(undef, ga.t, ga.t)
+    fill!(X, o^0 - 1)
+    j = 1
+    for col in eachcol(X)
+        col[j:ga.t] .= (coeff(gx, i) for i in ga.t - j + 1:-1:1)
+        j += 1   
+    end  
+    HF = Matrix{FqFieldElem}(undef, ga.t, ga.n)
+    HF = X * HField
+    H = Matrix{Bool}(undef, r*ga.t, ga.n)
+    for i in 1:ga.t 
         row_start = (i - 1) * r + 1
         row_end = row_start + r - 1
         for j in 1:ga.n
             t_tuple = Bool[]
             for k in 0:r - 1
-                t_tuple = [t_tuple; !is_zero(coeff(HField[i, j], k))]
+                t_tuple = [t_tuple; !is_zero(coeff(HF[i, j], k))]
             end 
             H[row_start:row_end, j] .=  vec(t_tuple')
         end
