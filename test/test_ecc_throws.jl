451793588a704806732885d979e0af46d56b118d
Fe-r-oz <ferozahmad7cf@gmail.com>
2024-04-28 23:31:42 +0500

    Adding testthrows as well

--- test/test_ecc_throws.jl
+++ test/test_ecc_throws.jl
@@ -1,7 +1,10 @@
 using Test
 using QuantumClifford
 using QuantumClifford.ECC
-using QuantumClifford.ECC: ReedMuller
+using QuantumClifford.ECC: ReedMuller, Goppa
 
 @test_throws ArgumentError ReedMuller(-1, 3)
 @test_throws ArgumentError ReedMuller(1, 0)
+
+@test_throws ArgumentError Goppa(6, 1)
+@test_throws ArgumentError Goppa(7, 8)
