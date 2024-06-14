3cf3c88691baae3b88cb750c5b719305766686e7
Yuxuan Yan <yyan.tsinghua@gmail.com>
2024-06-15 00:13:18 +0800

    add test cases

--- test/test_ecc_base.jl
+++ test/test_ecc_base.jl
@@ -11,6 +11,11 @@ const code_instance_args = Dict(
     Gottesman => [3, 4, 5],
     CSS => (c -> (parity_checks_x(c), parity_checks_z(c))).([Shor9(), Steane7(), Toric(4,4)]),
     Concat => [(Perfect5(), Perfect5()), (Perfect5(), Steane7()), (Steane7(), Cleve8()), (Toric(2,2), Shor9())],
+    RandomCircuitCode => repeat([
+            (10, Val(:alltoall), 30, 8), (10, Val(:alltoall), 30, 1:2:7),
+            ((7,), Val(:brickwork), 3, [1, 7]), ((20,), Val(:brickwork), 4, 1:4:20),
+            ((4, 6), Val(:brickwork), 4, 1:8)
+        ], 5), # repeat for more randomness
 )
 
 function all_testablable_code_instances(;maxn=nothing)
