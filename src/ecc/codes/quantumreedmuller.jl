c35472f1193a9907dc4f19b9b8513fc7bb20df2e
Fe-r-oz <ferozahmad7cf@gmail.com>
2024-06-30 16:08:54 +0500

    Adding a separate test file for quantum reed-muller codes

--- src/ecc/codes/quantumreedmuller.jl
+++ src/ecc/codes/quantumreedmuller.jl
@@ -1,11 +1,9 @@
 """
-The family of [[2ᵐ - 1, 1, 3]] CSS Quantum-Reed-Muller codes, as discovered by Steane in his 1999 paper [steane1999quantum](@cite).
+The family of `[[2ᵐ - 1, 1, 3]]` CSS Quantum-Reed-Muller codes, as discovered by Steane in his 1999 paper [steane1999quantum](@cite).
 
-Quantum codes are constructed from shortened Reed-Muller codes RM(1, m), by removing the first row and column of the generator matrix Gₘ. Similarly, we can define truncated dual codes RM(m - 2, m) using the generator matrix Hₘ. The quantum Reed-Muller codes QRM(m) derived from RM(1, m) are CSS codes. This means their stabilizer generators split into two sets, Axₘ and Azₘ. 
+Quantum codes are constructed from shortened Reed-Muller codes `RM(1, m)`, by removing the first row and column of the generator matrix `Gₘ`. Similarly, we can define truncated dual codes `RM(m - 2, m)` using the generator matrix `Hₘ`. The quantum Reed-Muller codes `QRM(m)` derived from `RM(1, m)` are CSS codes. 
 
-The Axₘ elements are formed by converting the 1s in the rows of Gm to Xs and the 0s to Is. The Azₘ elements are similarly formed but using the generator matrix of the truncated dual code RM(m - 2, m). Because RM(1, m) is a subset of RM(m - 2, m), Azₘ includes the same operators as Axₘ with Xs replaced by Zs, along with additional operators from the dual code.
-
-In CSS codes, Ax detects z-type errors and Az detects x-type errors. Given that the stabilizers of the quantum code are defined through the generator matrix of the classical code, the minimum distance of the quantum code corresponds to the minimum distance of the dual classical code, which is d = 3, thus it can correct any single qubit error. Since one stabilizer from the original and one from the dual code are removed in the truncation process, the code parameters are [[2ᵐ - 1, 1, 3]].
+Given that the stabilizers of the quantum code are defined through the generator matrix of the classical code, the minimum distance of the quantum code corresponds to the minimum distance of the dual classical code, which is `d = 3`, thus it can correct any single qubit error. Since one stabilizer from the original and one from the dual code are removed in the truncation process, the code parameters are `[[2ᵐ - 1, 1, 3]]`.
 
 The ECC Zoo has an [entry for this family](https://errorcorrectionzoo.org/c/quantum_reed_muller).
 """
@@ -40,5 +38,4 @@ distance(c::QuantumReedMuller) = 3
 parity_checks_x(c::QuantumReedMuller) = stab_to_gf2(parity_checks(QuantumReedMuller(c.m)))[1:c.m, 1:end÷2]
 
 parity_checks_z(c::QuantumReedMuller) = stab_to_gf2(parity_checks(QuantumReedMuller(c.m)))[end-(code_n(c::QuantumReedMuller) - 2 - c.m):end, end÷2+1:end]
-
 
