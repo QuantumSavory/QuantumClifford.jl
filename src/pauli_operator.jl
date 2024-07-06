d2bf12a111d396fcec4194ea0bca9d0330ca1235
isaac patterson <isaacthepatterson@gmail.com>
2024-07-06 16:05:10 -0400

    converted delete to deleteat! and moved to paulioperator.jl

--- src/pauli_operator.jl
+++ src/pauli_operator.jl
@@ -122,7 +122,10 @@ Base.hash(p::PauliOperator, h::UInt) = hash(p.phase,hash(p.nqubits,hash(p.xz, h)
 
 Base.copy(p::PauliOperator) = PauliOperator(copy(p.phase),p.nqubits,copy(p.xz))
 
-Base.deleteat!(p::PauliOperator) = p[setdiff(1:length(p), subset)]
+function Base.deleteat!(p::PauliOperator, subset) 
+    p =p[setdiff(1:length(p), subset)]
+    return p
+end
 
 _nchunks(i::Int,T::Type{<:Unsigned}) = 2*( (i-1) ÷ (8*sizeof(T)) + 1 )
 Base.zero(::Type{PauliOperator{Tz, Tv}}, q) where {Tz,T<:Unsigned,Tv<:AbstractVector{T}} = PauliOperator(zeros(UInt8), q, zeros(T, _nchunks(q,T)))
