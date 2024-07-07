0798fe1a0cfc4ec45d23ac75a203d39e0f3174a1
Yuxuan Yan <yyan.tsinghua@gmail.com>
2024-07-07 22:45:29 +0800

    remove MatrixElem type in lifted code

--- src/ecc/codes/classical/lifted.jl
+++ src/ecc/codes/classical/lifted.jl
@@ -1,24 +1,31 @@
-struct LiftedCode{T} <: ClassicalCode
-    A::Union{MatrixElem{T}, Matrix{T}} where T<:NCRingElement
+struct LiftedCode{T} <: ClassicalCode where T<:NCRingElement
+    A::Matrix{T}
     repr::Function
 
-    function LiftedCode(A::Union{MatrixElem{T}, Matrix{T}}, repr::Function) where T<:NCRingElement
+    function LiftedCode(A::Matrix{T}, repr::Function) where T<:NCRingElement
         new{T}(A, repr)
     end
 end
 
-function LiftedCode(A::Union{MatrixElem{PermGroupRingElem{FqFieldElem}}, Matrix{PermGroupRingElem{FqFieldElem}}})
-    !(characteristic(A.base_ring.base_ring) == 2) && error("The default permutation representation applies only to GF(2) group algebra")
+function LiftedCode(A::Matrix{PermGroupRingElem{FqFieldElem}})
+    !(characteristic(base_ring(A[1,1])) == 2) && error("The default permutation representation applies only to GF(2) group algebra")
+    # TODO also handle the case where the base ring is not GF(2)
     LiftedCode(A, permutation_repr)
 end
 
+LiftedCode(A::Matrix{Bool}) = LiftedCode(A, x->x)
+
 function permutation_repr(x::PermGroupRingElem{FqFieldElem})
-    return sum([Int(lift(ZZ, x.coeffs[k])) .* Array(matrix_repr(k)) for k in keys(x.coeffs)], init=zeros(Bool, parent(x).l, parent(x).l))
+    return sum([Int(Nemo.lift(ZZ, x.coeffs[k])) .* Array(matrix_repr(k)) for k in keys(x.coeffs)], init=zeros(Bool, parent(x).l, parent(x).l))
 end
 
-function parity_checks(c::LiftedCode)
-    vcat([hcat([c.repr(c.A[i, j]) for j in 1:size(c.A, 2)]...) for i in 1:size(c.A, 1)]...)
+function lift(repr::Function, mat::Matrix{T}) where T<:NCRingElement
+    vcat([hcat([repr(mat[i, j]) for j in axes(mat, 2)]...) for i in axes(mat, 1)]...)
 end
+
+lift(A::Matrix{Bool}) = A
+
+parity_checks(c::LiftedCode) = lift(c.repr, c.A)
 
 code_n(c::LiftedCode) = size(parity_checks(c), 2)
 
