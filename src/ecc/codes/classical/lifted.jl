8a905d4113f3a8ae47e5be12b63773345e029b29
Yuxuan Yan <yyan.tsinghua@gmail.com>
2024-07-09 23:04:11 +0800

    updated support type and related information

--- src/ecc/codes/classical/lifted.jl
+++ src/ecc/codes/classical/lifted.jl
@@ -10,12 +10,11 @@ struct LiftedCode{T} <: ClassicalCode where T<:NCRingElement
 end
 
 function LiftedCode(A::Matrix{PermGroupRingElem{FqFieldElem}})
+    # TODO we may also want to handle the case where the base ring is not GF(2),
+    # such as residue_ring(ZZ, 2), which is mathematically equivalent, and residue_ring(ZZ, n) for n > 2
     !(characteristic(base_ring(A[1,1])) == 2) && error("The default permutation representation applies only to GF(2) group algebra")
-    # TODO also handle the case where the base ring is not GF(2)
     LiftedCode(A, permutation_repr)
 end
-
-LiftedCode(A::Matrix{Bool}) = LiftedCode(A, x->x)
 
 function permutation_repr(x::PermGroupRingElem{FqFieldElem})
     return sum([Int(lift(ZZ, x.coeffs[k])) .* Array(matrix_repr(k)) for k in keys(x.coeffs)], init=zeros(Bool, parent(x).l, parent(x).l))
@@ -24,8 +23,6 @@ end
 function lift(repr::Function, mat::Matrix{T}) where T<:NCRingElement
     vcat([hcat([repr(mat[i, j]) for j in axes(mat, 2)]...) for i in axes(mat, 1)]...)
 end
-
-lift(A::Matrix{Bool}) = A
 
 parity_checks(c::LiftedCode) = lift(c.repr, c.A)
 
