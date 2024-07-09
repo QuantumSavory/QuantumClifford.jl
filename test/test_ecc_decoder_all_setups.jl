1a3ca374de332bc8082c984340102fcac340d946
Yuxuan Yan <yyan.tsinghua@gmail.com>
2024-07-10 00:19:09 +0800

    add BP decoder tests for LP codes

--- test/test_ecc_decoder_all_setups.jl
+++ test/test_ecc_decoder_all_setups.jl
@@ -5,6 +5,9 @@ using QuantumClifford.ECC
 import PyQDecoders
 import LDPCDecoders
 
+import Nemo: GF
+import LinearAlgebra
+
 include("test_ecc_base.jl")
 
 @testset "table decoder, good for small codes" begin
@@ -33,28 +36,106 @@ include("test_ecc_base.jl")
     end
 end
 
-##
+B04 = Dict()
+
+B04[7] = [
+    0 0 0 0;
+    0 1 2 5;
+    0 6 3 1
+]
+
+B04[9] = [
+    0 0 0 0;
+    0 1 6 7;
+    0 4 5 2
+]
+
+B04[17] = [
+    0 0 0 0;
+    0 1 2 11;
+    0 8 12 13
+]
+
+B04[19] = [
+    0 0 0 0;
+    0 2 6 9;
+    0 16 7 11
+]
+
+B118 = Dict()
+
+B118[16] = [
+    0 0 0 0 0;
+    0 2 4 7 11;
+    0 3 10 14 15
+]
+
+B118[21] = [
+    0 0 0 0 0;
+    0 4 5 7 17;
+    0 14 18 12 11
+]
+
+B118[30] = [
+    0 0 0 0 0;
+    0 2 14 24 25;
+    0 16 11 14 13
+]
+
+LP04 = []
+for l in keys(B04)
+    A = map(B04[l]) do x
+        (PermutationGroupRing(GF(2), l))(cyclic_permutation(x, l))
+    end
+    push!(LP04, LPCode(LiftedCode(A), LiftedCode(A)))
+end
+
+LP118 = []
+for l in keys(B118)
+    A = map(B118[l]) do x
+        (PermutationGroupRing(GF(2), l))(cyclic_permutation(x, l))
+    end
+    push!(LP118, LPCode(LiftedCode(A), LiftedCode(A)))
+end
+
+other_lifted_product_codes = []
+
+l = 63
+R = PermutationGroupRing(GF(2), l)
+A = zeros(R, 7,7)
+A[LinearAlgebra.diagind(A)] .= R(cyclic_permutation(27, l))
+A[LinearAlgebra.diagind(A, -1)] .= R(cyclic_permutation(54, l))
+A[LinearAlgebra.diagind(A, 6)] .= R(cyclic_permutation(54, l))
+A[LinearAlgebra.diagind(A, -2)] .= R(1)
+A[LinearAlgebra.diagind(A, 5)] .= R(1)
+
+B = zeros(R, 1, 1)
+B[1,1] = (R(1) + R(cyclic_permutation(1, l)) + R(cyclic_permutation(6, l)))'
+
+push!(other_lifted_product_codes, LPCode(LiftedCode(A), LiftedCode(B)))
+
 
 @testset "belief prop decoders, good for sparse codes" begin
-    codes = [
-        # TODO
-    ]
+    codes = vcat(LP04, LP118, other_lifted_product_codes)
 
     noise = 0.001
 
     setups = [
         CommutationCheckECCSetup(noise),
-        NaiveSyndromeECCSetup(noise, 0),
-        ShorSyndromeECCSetup(noise, 0),
+        # NaiveSyndromeECCSetup(noise, 0),
+        # ShorSyndromeECCSetup(noise, 0),
     ]
+    # lifted product codes currently trigger errors in syndrome circuits
 
     for c in codes
         for s in setups
             for d in [c->PyBeliefPropOSDecoder(c, maxiter=10)]
-                e = evaluate_decoder(d(c), s, 100000)
-                @show c
-                @show s
-                @show e
+                nsamples = code_n(c)>400 ? 1000 : 10000
+                # take fewer samples for larger codes to save time
+                e = evaluate_decoder(d(c), s, nsamples)
+                # @show c
+                # @show s
+                # @show e
                 @assert max(e...) < noise/4
             end
         end
