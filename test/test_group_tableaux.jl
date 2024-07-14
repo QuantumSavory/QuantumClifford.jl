e0ad281c62a8ede69d8d0386080947f372635ebe
isaac patterson <isaacthepatterson@gmail.com>
2024-07-14 19:40:42 -0400

    fixed tests

--- test/test_group_tableaux.jl
+++ test/test_group_tableaux.jl
@@ -1,12 +1,14 @@
+using Test
+
 using Random
 using QuantumClifford
 
 # Including sizes that would test off-by-one errors in the bit encoding.
-test_sizes = [1, 2, 3, 4, 5, 7, 8, 9, 15, 16, 17] # Zero function(in groupify) slows down around 2^30(n=30),eventually breaks
+test_sizes = [1, 2, 3, 4, 5, 7, 8, 9, 15, 16, 17] 
 # Zero function(in groupify) slows down around 2^30(n=30),eventually breaks
-small_test_sizes = [1, 2, 3, 4, 5, 7, 8] # Pauligroup slows around n =9
+small_test_sizes = [1, 2, 3, 4, 5, 7] # Pauligroup slows around n = 8
 
-@test_set "Group Tableaux" begin
+@testset "group_tableaux" begin
     #Test groupify
     for n in [1, test_sizes...]
         s = random_stabilizer(n)
@@ -26,69 +28,75 @@ small_test_sizes = [1, 2, 3, 4, 5, 7, 8] # Pauligroup slows around n =9
     for n in [1, small_test_sizes...]
         s = random_stabilizer(n)
         group = groupify(s)
-        gen_set = get_generating_set(group)
+        gen_set = minimal_generating_set(Stabilizer(group))
         new_group = groupify(gen_set)
-        canonicalize!(group)
-        canonicalize!(new_group)
+        canonicalize!(Stabilizer(group))
+        canonicalize!(Stabilizer(new_group))
         @test group == new_group
         s = zero(Stabilizer, rand(1:(2*n)), n)
         for i in 1:length(s)
             s[i] = random_pauli(n)
         end
-        gen_set = get_generating_set(s)
+        gen_set = minimal_generating_set(s)
         new_group = groupify(s)
         for operator in s
             @test operator in new_group
         end
     end
-
+    #Test pauligroup
+    for n in [1, small_test_sizes...] 
+        @test length(QuantumClifford.pauligroup(n, phases=false)) == 4^n
+        @test length(QuantumClifford.pauligroup(n, phases=true)) == 4^(n+1)
+    end
     #Test normalizer
     for n in [1, small_test_sizes...] # pauligroup is very slow at n=14
-        s = random_stabilizer(n)
-        normalized = normalizer(s)
-        stabilizers = pauligroup(n, true)
-        for n_stabilizer in normalized
-            for stabilizer in s
-                @test comm(n_stabilizer, stabilizer) == 0x0
+        t = zero(QuantumClifford.Tableau, rand(1:(2*n)), n)
+        for i in eachindex(t) t[i] = random_pauli(n) end
+        normalized = normalizer(t)
+        paulis = QuantumClifford.pauligroup(n, phases=true)
+        for n_pauli in normalized
+            for t_pauli in t
+                @test comm(n_pauli, t_pauli) == 0x0
             end
         end
-    end
-    for stabilizer in stabilizers
-        commutes = true
-        for n_stabilizer in normalized
-            if !(comm(n_stabilizer, stabilizer) == 0x0)
-                commutes = false
+        for pauli in paulis
+            commutes = true
+            for t_pauli in t
+                if comm(t_pauli, pauli) == 0x01
+                    commutes = false
+                end
             end
+           @test (!commutes) || (pauli in normalized)
         end
-        @test !commutes || stabilizer in normalized
     end
     #Test centralizer
     for n in [1, test_sizes...]
-        s = random_stabilizer(n)
-        c = centeralizer(s)
-        for c_stabilizer in c
-            for stabilizer in s
-                @test comm(c_stabilizer, stabilizer) == 0x0
+        t = zero(QuantumClifford.Tableau, rand(1:(2*n)), n)
+        for i in eachindex(t) t[i] = random_pauli(n) end
+        c = centralizer(t)
+        for c_pauli in c
+            for t_pauli in t
+                @test comm(c_pauli, t_pauli) == 0x0
             end
         end
-        for stabilizer in s
+        for pauli in t
             commutes = true
-            for stab in s
-                @test comm(stab, stabilizer) == 0x0
+            for t_pauli in t
+                if comm(t_pauli, pauli)==0x01 commutes = false end
             end
-            @test !commutes || st in c
+            @test !commutes || pauli in c
         end
 
     end
     #Test contract
     for n in [1, test_sizes...]
         s = random_stabilizer(n)
-        g = QuantumClifford.groupify(s)
+        g = groupify(s)
         subset = []
         for i in 1:nqubits(s) #create a random subset
             if rand(1:2) == 1 push!(subset, i) end
         end
-        c = QuantumClifford.contract(s, subset)
+        c = contracter(s, subset)
         count = 0
         for stabilizer in g 
             contractable = true
@@ -97,7 +105,7 @@ small_test_sizes = [1, 2, 3, 4, 5, 7, 8] # Pauligroup slows around n =9
             end
             if contractable count+=1 end
         end
-        @test count = size(c)
+        @test count == length(c)
         for contracted in c
             p = zero(PauliOperator, nqubits(s))
             index = 0
@@ -107,7 +115,7 @@ small_test_sizes = [1, 2, 3, 4, 5, 7, 8] # Pauligroup slows around n =9
                     p[i] = contracted[index] 
                 end
             end
-            @test p in g 
+            @test p in g || -1* p in g || 1im * p in g || -1im * p in g
         end
     end
 end
