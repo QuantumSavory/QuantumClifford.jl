056442e124cfd526d045bac974252c49d855624a
Stefan Krastanov <stefan@krastanov.org>
2024-08-03 12:40:19 -0400

    vscode testrunner module shenanigans

--- test/test_doctests.jl
+++ test/test_doctests.jl
@@ -5,7 +5,10 @@
     ENV["LINES"] = 80    # for forcing `displaysize(io)` to be big enough
     ENV["COLUMNS"] = 80
     DocMeta.setdocmeta!(QuantumClifford, :DocTestSetup, :(using QuantumClifford; using QuantumClifford.ECC); recursive=true)
-    doctest(QuantumClifford,
+    doctestfilters = [r"(QuantumClifford\.|)"]
+    doctest(QuantumClifford;
+            doctestfilters
             #fix=true
            )
+    # TODO failures in VSCode related to https://github.com/julia-vscode/TestItemRunner.jl/issues/49
 end
