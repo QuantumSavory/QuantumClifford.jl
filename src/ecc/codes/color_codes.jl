223020f392cadb51dc77628bd12f00be5aebd7a5
Anthony Micciche <amicciche@umass.edu>
2024-09-06 23:11:14 -0400

    started working on triangular 6.6.6 codes. currently can generate some of the stabilizers for this code. A more complete commit will be coming in a few days

--- src/ecc/codes/color_codes.jl
+++ src/ecc/codes/color_codes.jl
@@ -11,7 +11,18 @@ struct Triangular4_8_8 <: TriangularCode
     end
 end
 
+struct Triangular6_6_6 <: TriangularCode
+    d::Int
+    function Triangular6_6_6(d)
+        if d%2!=1
+            throw(DomainError("only odd distance trianglular color codes are allowed.\nRefer to https://arxiv.org/abs/1108.5738"))
+        end
+        return new(d)
+    end
+end
+
 Triangular4_8_8() = Triangular4_8_8(3) # smallest d
+Triangular6_6_6() = Triangular6_6_6(3) # smalled d
 
 function parity_checks(c::Triangular4_8_8)
     matrix = get_check_matrix(c)
@@ -20,7 +31,38 @@ function parity_checks(c::Triangular4_8_8)
     return Stabilizer(vcat(matrix,zeros(Bool,num_checks,qubits)), vcat(zeros(Bool,num_checks,qubits), matrix))
 end
 
-"""Returns the binary matrix defining the x stabilizers for the Triangular4_8_8 code. The Z stabilizers are the same."""
+function get_check_matrix(c::Triangular6_6_6)
+    n = code_n(c)
+    num_checks = (n-1)/2 |> Int 
+    num_layers = (c.d-1)/2 |> Int
+    checks = zeros(Bool, num_checks, n)
+
+    i = 1
+    checks_written = 0
+    for layer in 1:num_layers
+        # red trapezoid
+        checks[checks_written+1,i] = 1
+        checks[checks_written+1,i+(layer-1)*2+1] = 1
+        checks[checks_written+1,i+2+4*(layer-1)] = 1
+        checks[checks_written+1,i+2+4*(layer-1)+1] = 1
+        checks_written += 1
+
+        # blue trapezoid
+        checks[checks_written+1,i+1+4*(layer-1)] = 1
+        checks[checks_written+1,i+1+4*(layer-1)+layer*2] = 1
+        checks[checks_written+1,i+5+8*(layer-1)] = 1
+        checks[checks_written+1,i+5+8*(layer-1)+1] = 1
+        checks_written += 1
+
+        # red hexagons
+
+        i += 4+6*(layer-1)
+    end
+
+    return checks
+end
+
+"""Returns the binary matrix defining the X stabilizers for the Triangular4_8_8 code. The Z stabilizers are the same."""
 function get_check_matrix(c::Triangular4_8_8)
     n = code_n(c)
     num_checks = (n-1)/2 |> Int 
@@ -113,4 +155,5 @@ end
 
 """From https://arxiv.org/abs/1108.5738 Fig. 2's caption:"""
 code_n(c::Triangular4_8_8) = 0.5*c.d^2+c.d-0.5 |> Int
+code_n(c::Triangular6_6_6) = 0.75*c.d^2+.25 |> Int
 code_k(c::TriangularCode) = 1
