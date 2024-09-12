da60debf9a86df4095de8de358a6ed40279ef6b5
Stefan Krastanov <stefan@krastanov.org>
2024-09-12 09:47:54 -0400

    simplify JET (#358)

--- test/test_jet.jl
+++ test/test_jet.jl
@@ -6,34 +6,17 @@ using Graphs
 using StridedViews
 using LinearAlgebra
 
-using JET: ReportPass, BasicPass, InferenceErrorReport, UncaughtExceptionReport
-
-# Custom report pass that ignores `UncaughtExceptionReport`
-# Too coarse currently, but it serves to ignore the various
-# "may throw" messages for runtime errors we raise on purpose
-# (mostly on malformed user input)
-struct MayThrowIsOk <: ReportPass end
-
-# ignores `UncaughtExceptionReport` analyzed by `JETAnalyzer`
-(::MayThrowIsOk)(::Type{UncaughtExceptionReport}, @nospecialize(_...)) = return
-
-# forward to `BasicPass` for everything else
-function (::MayThrowIsOk)(report_type::Type{<:InferenceErrorReport}, @nospecialize(args...))
-    BasicPass()(report_type, args...)
-end
-
-    rep = report_package("QuantumClifford";
-        report_pass=MayThrowIsOk(),
-        ignored_modules=(
-            AnyFrameModule(Graphs.LinAlg),
-            AnyFrameModule(Graphs.SimpleGraphs),
-            AnyFrameModule(ArrayInterface),
-            AnyFrameModule(Static),
-            AnyFrameModule(StridedViews),
-            AnyFrameModule(LinearAlgebra),
-        )
+rep = report_package("QuantumClifford";
+    ignored_modules=(
+        AnyFrameModule(Graphs.LinAlg),
+        AnyFrameModule(Graphs.SimpleGraphs),
+        AnyFrameModule(ArrayInterface),
+        AnyFrameModule(Static),
+        AnyFrameModule(StridedViews),
+        AnyFrameModule(LinearAlgebra),
     )
-    @show rep
-    @test_broken length(JET.get_reports(rep)) == 0
-    @test length(JET.get_reports(rep)) <= 28
+)
+@show rep
+@test_broken length(JET.get_reports(rep)) == 0
+@test length(JET.get_reports(rep)) <= 23
 end
