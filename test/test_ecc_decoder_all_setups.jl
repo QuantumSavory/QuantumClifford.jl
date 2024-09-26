776aeac31d3c574759ba0fee57c20b96cf4d104a
Stefan Krastanov <stefan@krastanov.org>
2024-09-26 15:49:10 -0400

    remove repeated tests

--- test/test_ecc_decoder_all_setups.jl
+++ test/test_ecc_decoder_all_setups.jl
@@ -123,39 +123,3 @@ end
         end
     end
 end
-
-##
-
-using Test
-using QuantumClifford
-using QuantumClifford.ECC
-
-import PyQDecoders
-import LDPCDecoders
-
-@testset "matching decoder, good as long as column weight of the code is limited" begin
-    codes = [
-        Toric(8,8),
-        Toric(9,9),
-        Surface(8,8),
-        Surface(9,9)
-    ]
-
-    noise = 0.01
-
-    setups = [
-        CommutationCheckECCSetup(noise),
-        NaiveSyndromeECCSetup(noise, 0),
-        ShorSyndromeECCSetup(noise, 0),
-    ]
-
-    for c in codes
-        for s in setups
-            e = evaluate_decoder(PyMatchingDecoder(c), s, 10000)
-            #@show c
-            #@show s
-            #@show e
-            @assert max(e...) < noise/5
-        end
-    end
-end
