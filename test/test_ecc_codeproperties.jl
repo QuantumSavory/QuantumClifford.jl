014a77abb59ce951f31ced53f7d3e04db9edc872
Stefan Krastanov <stefan@krastanov.org>
2024-09-26 14:25:32 -0400

    reword one of the tests for rank/code_s consistency

--- test/test_ecc_codeproperties.jl
+++ test/test_ecc_codeproperties.jl
@@ -31,7 +31,8 @@
             @test nqubits(code) == size(H, 2) == code_n(code)
             @test size(H, 1) == code_s(code)
             @test code_s(code) + code_k(code) >= code_n(code) # possibly exist redundant checks
-            @test size(H, 1) <= size(H, 2) # equal in some cases with redundancy
+            _, _, rank = canonicalize!(copy(H), ranks=true)
+            @test rank <= size(H, 1)
             @test QuantumClifford.stab_looks_good(copy(H))
         end
     end
