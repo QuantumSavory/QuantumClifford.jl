86e622f4bbeafa7f9f259896f1f0e5fdc2d1ffce
Fe-r-oz <feroz.ahmad.email@gmail.com>
2024-10-11 01:31:07 +0500

    Haah's cubic code

--- docs/src/references.md
+++ docs/src/references.md
@@ -40,6 +40,7 @@ For quantum code construction routines:
 - [steane1999quantum](@cite)
 - [campbell2012magic](@cite)
 - [anderson2014fault](@cite)
+- [haah2011local](@cite)
 
 For classical code construction routines:
 - [muller1954application](@cite)
--- ext/QuantumCliffordHeckeExt/QuantumCliffordHeckeExt.jl
+++ ext/QuantumCliffordHeckeExt/QuantumCliffordHeckeExt.jl
@@ -11,7 +11,7 @@ import Nemo: characteristic, matrix_repr, GF, ZZ, lift
 
 import QuantumClifford.ECC: AbstractECC, CSS, ClassicalCode,
     hgp, code_k, code_n, code_s, iscss, parity_checks, parity_checks_x, parity_checks_z, parity_checks_xz,
-    two_block_group_algebra_codes, generalized_bicycle_codes, bicycle_codes
+    two_block_group_algebra_codes, generalized_bicycle_codes, bicycle_codes, Haah_cubic_codes
 
 include("types.jl")
 include("lifted.jl")
