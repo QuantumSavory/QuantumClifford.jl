4e6807cee5eb3cb87092d187f838469e3920142b
Fe-r-oz <feroz.ahmad.email@gmail.com>
2024-11-05 16:10:46 +0500

    formatting cleanup

--- test/test_ecc_multivariate_bicycle.jl
+++ test/test_ecc_multivariate_bicycle.jl
@@ -13,7 +13,7 @@
         A = z^2 + z^6
         B = x + x^6
         c = two_block_group_algebra_codes(A, B)
-        @test code_n(c) == 112 &&  code_k(c) == 8
+        @test code_n(c) == 112 && code_k(c) == 8
 
         # [[64, 2, 8]]
         l=8; m=4
@@ -23,7 +23,7 @@
         A = x + x^2
         B = x^3 + y
         c = two_block_group_algebra_codes(A, B)
-        @test code_n(c) == 64 &&  code_k(c) == 2
+        @test code_n(c) == 64 && code_k(c) == 2
 
         # [[72, 2, 8]]
         l=4; m=9
@@ -33,7 +33,7 @@
         A = x + y^2
         B = x^2 + y^2
         c = two_block_group_algebra_codes(A, B)
-        @test code_n(c) == 72 &&  code_k(c) == 2
+        @test code_n(c) == 72 && code_k(c) == 2
 
         # [[96, 2, 8]]
         l=6; m=8
@@ -43,7 +43,7 @@
         A = x^5 + y^6
         B = z + z^4
         c = two_block_group_algebra_codes(A, B)
-        @test code_n(c) == 96 &&  code_k(c) == 2
+        @test code_n(c) == 96 && code_k(c) == 2
 
         # [[112, 2, 10]]
         l=7; m=8
@@ -53,7 +53,7 @@
         A = z^6 + x^5
         B = z^2 + y^5
         c = two_block_group_algebra_codes(A, B)
-        @test code_n(c) == 112 &&  code_k(c) == 2
+        @test code_n(c) == 112 && code_k(c) == 2
 
         # [[144, 2, 12]]
         l=8; m=9
@@ -63,7 +63,7 @@
         A = x^3 + y^7
         B = x + y^5
         c = two_block_group_algebra_codes(A, B)
-        @test code_n(c) == 144 &&  code_k(c) == 2
+        @test code_n(c) == 144 && code_k(c) == 2
     end
 
     @testset "Weight-5 QLDPC Codes" begin
@@ -75,7 +75,7 @@
         A = x + z^4
         B = x + y^2 + z^2 
         c = two_block_group_algebra_codes(A, B)
-        @test code_n(c) == 30 &&  code_k(c) == 4
+        @test code_n(c) == 30 && code_k(c) == 4
 
         # [[72, 4, 8]]
         l=4; m=9
@@ -85,7 +85,7 @@
         A = x + y^3
         B = x^2 + y + y^2 
         c = two_block_group_algebra_codes(A, B)
-        @test code_n(c) == 72 &&  code_k(c) == 4
+        @test code_n(c) == 72 && code_k(c) == 4
 
         # [96, 4, 8]]
         l=8; m=6
@@ -95,7 +95,7 @@
         A = x^6 + x^3
         B = z^5 + x^5 + y 
         c = two_block_group_algebra_codes(A, B)
-        @test code_n(c) == 96 &&  code_k(c) == 4
+        @test code_n(c) == 96 && code_k(c) == 4
     end
 
     @testset "Weight-6 QLDPC codes" begin
@@ -107,7 +107,7 @@
         A = x^4 + z^3
         B = x^4 + x + z^4 + y
         c = two_block_group_algebra_codes(A, B)
-        @test code_n(c) == 30 &&  code_k(c) == 6
+        @test code_n(c) == 30 && code_k(c) == 6
 
         # [[48, 6, 6]]
         l=4; m=6
@@ -117,7 +117,7 @@
         A = x^2 + y^4
         B = x^3 + z^3 + y^2 + y
         c = two_block_group_algebra_codes(A, B)
-        @test code_n(c) == 48 &&  code_k(c) == 6
+        @test code_n(c) == 48 && code_k(c) == 6
 
         # [[40, 4, 6]]
         l=4; m=5
@@ -127,7 +127,7 @@
         A = x^2 + y
         B = y^4 + y^2 + x^3 + x
         c = two_block_group_algebra_codes(A, B)
-        @test code_n(c) == 40 &&  code_k(c) == 4
+        @test code_n(c) == 40 && code_k(c) == 4
 
         # [[48, 4, 6]]
         l=4; m=6
@@ -137,7 +137,7 @@
         A = x^3 + y^5
         B = x + z^5 + y^5 + y^2
         c = two_block_group_algebra_codes(A, B)
-        @test code_n(c) == 48 &&  code_k(c) == 4
+        @test code_n(c) == 48 && code_k(c) == 4
     end
 
     @testset "Weight-7 QLDPC codes" begin
@@ -149,6 +149,6 @@
         A = x^4 + x^2
         B = x + x^2 + y + z^2 + z^3
         c = two_block_group_algebra_codes(A, B)
-        @test code_n(c) == 30 &&  code_k(c) == 4
+        @test code_n(c) == 30 && code_k(c) == 4
     end
 end
