d851e621be12023a04b999eaa34036222a3554d9
Stefan Krastanov <stefan@krastanov.org>
2024-12-20 12:56:28 -0500

    make sure doctests actually run

--- test/test_doctests.jl
+++ test/test_doctests.jl
@@ -1,12 +1,18 @@
 @testitem "Doctests" tags=[:doctests] begin
     using Documenter
     using QuantumClifford
+    using QuantumClifford.Experimental.NoisyCircuits
+    using QuantumInterface
+
+    ENV["HECKE_PRINT_BANNER"] = "false"
+    import Hecke
+    const QuantumCliffordHeckeExt = Base.get_extension(QuantumClifford, :QuantumCliffordHeckeExt)
 
     ENV["LINES"] = 80    # for forcing `displaysize(io)` to be big enough
     ENV["COLUMNS"] = 80
-    DocMeta.setdocmeta!(QuantumClifford, :DocTestSetup, :(using QuantumClifford; using QuantumClifford.ECC); recursive=true)
+    modules = [QuantumClifford, QuantumClifford.Experimental.NoisyCircuits, QuantumClifford.ECC, QuantumInterface, QuantumCliffordHeckeExt]
     doctestfilters = [r"(QuantumClifford\.|)"]
-    doctest(QuantumClifford;
+    doctest(nothing, modules;
             doctestfilters
             #fix=true
            )
