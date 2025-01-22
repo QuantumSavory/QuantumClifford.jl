46bd8b2be5a7fd6099b8af0046b54286ebaede9a
Fe-r-oz <feroz.ahmad.email@gmail.com>
2025-01-22 23:08:31 +0500

    polish test suite

--- test/test_ecc_delfosse_generalized_reedmuller.jl
+++ test/test_ecc_delfosse_generalized_reedmuller.jl
@@ -8,38 +8,35 @@
 
     @testset "Testing [[8rp, (8r − 2)p − 2m, 4]] DelfosseRepCode properties" begin
         @testset "test [16p, 14p − 8, 4]] code family that uses RM(2,4)" begin
+            r = 2
+            m = 4
             for i in 2:100
-                for m in [4]
-                    for r in [2]
-                        p = i
-                        n = 8*r*p
-                        k = (8*r − 2)*p − 2*m
-                        stab = parity_checks(DelfosseGeneralizedReedMuller(p,r,m))
-                        H = stab_to_gf2(stab)
-                        mat = matrix(GF(2), H)
-                        computed_rank = rank(mat)
-                        @test computed_rank == n - k
-                    end
-                end
+                p = i
+                n = 8 *r*p
+                k = (8*r-2)*p-2*m
+                stab = parity_checks(DelfosseGeneralizedReedMuller(p,r,m))
+                H = stab_to_gf2(stab)
+                mat = matrix(GF(2), H)
+                computed_rank = rank(mat)
+                @test computed_rank == n - k
             end
         end
 
         @testset "test [[8p, 6(p−1), 4]] code family that uses RM(1,3)" begin
+            r = 1
+            m = 3
             for i in 2:100
-                for m in [3]
-                    for r in [1]
-                        p = i
-                        n = 8*r*p
-                        k = (8*r − 2)*p − 2*m
-                        stab = parity_checks(DelfosseGeneralizedReedMuller(p,r,m))
-                        H = stab_to_gf2(stab)
-                        mat = matrix(GF(2), H)
-                        computed_rank = rank(mat)
-                        @test computed_rank == n - k
-                    end
-                end
+                p = i
+                n = 8 *r*p
+                k = (8*r-2)*p-2*m
+                stab = parity_checks(DelfosseGeneralizedReedMuller(p,r,m))
+                H = stab_to_gf2(stab)
+                mat = matrix(GF(2), H)
+                computed_rank = rank(mat)
+                @test computed_rank == n - k
             end
         end
+
         # crosscheck parity check matrices from VIII. 2 Generalizing the [8, 4, 4]
         # and [16, 11, 4] classical codes, pg. 11 of https://arxiv.org/pdf/2008.05051
         r = 1
