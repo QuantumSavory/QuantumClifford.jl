10321de5870435fabdf9267a3d628af7be0f8db8
alilavas <h.lavasani@gmail.com>
2025-02-13 12:55:58 -0800

    fix a typo in the fastrow signature docstring

--- src/fastmemlayout.jl
+++ src/fastmemlayout.jl
@@ -2,7 +2,7 @@
 
 In this layout a Pauli string (a row of the tableau) is stored contiguously in memory.
 
-See also: [`fastrow`](@ref)"""
+See also: [`fastcolumn`](@ref)"""
 function fastrow end
 
 """Convert a tableau to a memory layout that is fast for column operations.
