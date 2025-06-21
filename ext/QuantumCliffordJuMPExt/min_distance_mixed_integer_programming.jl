8b29f0abc3d80b5e38eceb7ce96f8aa6a356f094
Fe-r-oz <feroz.ahmad.email@gmail.com>
2025-06-22 01:35:34 +0500

    🛠️ undo the formatting changes to jump ext as they are being worked on in earlier pr to prevent merge conflicts

--- ext/QuantumCliffordJuMPExt/min_distance_mixed_integer_programming.jl
+++ ext/QuantumCliffordJuMPExt/min_distance_mixed_integer_programming.jl
@@ -1,5 +1,5 @@
 @doc raw"""
-$(TYPEDSIGNATURES)
+$TYPEDSIGNATURES
 
 Compute the distance of a code using mixed integer programming.
 See [`QuantumClifford.ECC.DistanceMIPAlgorithm`](@ref) for configuration options.
@@ -146,15 +146,26 @@ Mixed-integer programming (MIP) is applied in quantum error correction,
 notably for decoding and minimum distance computation. Some applications
 are as follows:
 
-- The first usecase of the MIP approach was the code capacity Most Likely Error (MLE) decoder for color codes introduced in [landahl2011color](@cite).
+- The first usecase of the MIP approach was the code capacity Most Likely
+Error (MLE) decoder for color codes introduced in [landahl2011color](@cite).
 
-- For all quantum LDPC codes presented in [panteleev2021degenerate](@cite),the lower and upper bounds on the minimum distance was obtained by reduction to a mixed integer linear program and using the GNU Linear Programming Kit ([makhorin2008glpk](@cite)).
+- For all quantum LDPC codes presented in [panteleev2021degenerate](@cite),
+the lower and upper bounds on the minimum distance was obtained by reduction
+to a mixed integer linear program and using the GNU Linear Programming
+Kit ([makhorin2008glpk](@cite)).
 
-- For all the Bivariate Bicycle (BB) codes presented in [bravyi2024high](@cite), the code distance was calculated using the mixed integer programming approach.
+- For all the Bivariate Bicycle (BB) codes presented in [bravyi2024high](@cite),
+the code distance was calculated using the mixed integer programming approach.
 
-- [lacroix2024scaling](@cite) developed a MLE decoder that finds the most likely chain of Pauli errors given the observed error syndrome by solving a mixed-integer program using `HiGHS` package ([huangfu2018parallelizing](@cite)).
+- [lacroix2024scaling](@cite) developed a MLE decoder that finds the most
+likely chain of Pauli errors given the observed error syndrome by solving
+a mixed-integer program using `HiGHS` package ([huangfu2018parallelizing](@cite)).
 
-- [cain2025correlateddecodinglogicalalgorithms](@cite) formulate maximum-likelihood decoding as a mixed-integer program maximizing ``\prod_{j=1}^M p_j^{E_j}(1-p_j)^{1-E_j}`` (where binary variables ``E_j \in {0,1}`` indicate error occurrence) subject to syndrome constraints, solved optimally via MIP solvers despite its NP-hard complexity.
+- [cain2025correlateddecodinglogicalalgorithms](@cite) formulate maximum-likelihood
+decoding as a mixed-integer program maximizing ``\prod_{j=1}^M p_j^{E_j}(1-p_j)^{1-E_j}``
+(where binary variables ``E_j \in {0,1}`` indicate error occurrence) subject
+to syndrome constraints, solved optimally via MIP solvers despite its
+NP-hard complexity.
 """
 function distance(code::AbstractECC, alg::DistanceMIPAlgorithm)
     logical_qubits = isnothing(alg.logical_qubit) ? (1:code_k(code)) : (alg.logical_qubit:alg.logical_qubit)
