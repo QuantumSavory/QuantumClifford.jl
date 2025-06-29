ceaac4fd0b991c65ece3a3d10ecfd5e8f66459bd
Fe-r-oz <feroz.ahmad.email@gmail.com>
2025-06-29 10:34:48 +0500

    🚂 minor formatting attempt 7

--- test/test_ecc_generalized_bicycle_codes_slow.jl
+++ test/test_ecc_generalized_bicycle_codes_slow.jl
@@ -53,37 +53,30 @@
         c = generalized_bicycle_codes([0, 1, 2, 3], [0, 1],  5)
         @test code_n(c) == 10
         @test distance(c, DistanceMIPAlgorithm(solver=HiGHS)) == 3
-        @test distance(c, DistanceMIPAlgorithm(solver=HiGHS, upper_bound=true)) == 3
         c = generalized_bicycle_codes([0, 1, 2, 3], [0, 1],  7)
         @test code_n(c) == 14
         @test distance(c, DistanceMIPAlgorithm(solver=HiGHS)) == 3
-        @test distance(c, DistanceMIPAlgorithm(solver=HiGHS, upper_bound=true)) == 4
         c = generalized_bicycle_codes([0, 1, 2, 5], [0, 1], 11)
         @test code_n(c) == 22
         @test distance(c, DistanceMIPAlgorithm(solver=HiGHS)) == 5
-        @test distance(c, DistanceMIPAlgorithm(solver=HiGHS, upper_bound=true)) == 5
         c = generalized_bicycle_codes([0, 1, 2, 5], [0, 1], 13)
         @test code_n(c) == 26
         @test distance(c, DistanceMIPAlgorithm(solver=HiGHS)) == 5
         c = generalized_bicycle_codes([0, 1, 3, 9], [0, 1], 17)
         @test code_n(c) == 34
         @test distance(c, DistanceMIPAlgorithm(solver=HiGHS)) == 7
-        @test distance(c, DistanceMIPAlgorithm(solver=HiGHS, upper_bound=true)) == 8
         c = generalized_bicycle_codes([0, 1, 2, 8], [0, 1], 19)
         @test code_n(c) == 38
         @test distance(c, DistanceMIPAlgorithm(solver=HiGHS)) == 7
-        @test distance(c, DistanceMIPAlgorithm(solver=HiGHS, upper_bound=true)) == 8
         c = generalized_bicycle_codes([0, 1, 3, 9], [0, 1], 23)
         @test code_n(c) == 46
         @test distance(c, DistanceMIPAlgorithm(solver=HiGHS)) == 8
         c = generalized_bicycle_codes([0, 1, 3, 10], [0, 1], 29)
         @test code_n(c) == 58
         @test distance(c, DistanceMIPAlgorithm(solver=HiGHS)) == 9
-        @test distance(c, DistanceMIPAlgorithm(solver=HiGHS, upper_bound=true)) == 10
         c = generalized_bicycle_codes([0, 5, 11,17], [0, 1], 31)
         @test code_n(c) == 62
         @test distance(c, DistanceMIPAlgorithm(solver=HiGHS)) == 10
-        @test distance(c, DistanceMIPAlgorithm(solver=HiGHS, upper_bound=true)) == 11
         c = generalized_bicycle_codes([0, 1, 4, 14], [0, 1], 37)
         @test code_n(c) == 74
         @test distance(c, DistanceMIPAlgorithm(solver=HiGHS)) == 11
@@ -91,14 +84,11 @@
         c = generalized_bicycle_codes([0, 1, 2, 4, 5, 8], [0, 1], 11)
         @test code_n(c) == 22
         @test distance(c, DistanceMIPAlgorithm(solver=HiGHS)) == 6
-        @test distance(c, DistanceMIPAlgorithm(solver=HiGHS, upper_bound=true)) == 7
         c = generalized_bicycle_codes([0, 2, 3, 6, 7, 9], [0, 1], 13)
         @test code_n(c) == 26
         @test distance(c, DistanceMIPAlgorithm(solver=HiGHS)) == 6
-        @test distance(c, DistanceMIPAlgorithm(solver=HiGHS, upper_bound=true)) == 7
         c = generalized_bicycle_codes([0, 1, 2, 3, 4, 8], [0, 1], 19)
         @test code_n(c) == 38
         @test distance(c, DistanceMIPAlgorithm(solver=HiGHS)) == 7
-        @test distance(c, DistanceMIPAlgorithm(solver=HiGHS, upper_bound=true)) == 7
     end
 end
