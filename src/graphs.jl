9adf626b3639c8670f45385506a20e438388bef4
Harry Ying <lexugeyky@outlook.com>
2025-06-29 22:01:56 +0100

    add: `GraphState` type
    - cleaner API on graph states through `GraphState` type
    - updated tests and docs accordingly

--- src/graphs.jl
+++ src/graphs.jl
@@ -1,43 +1,89 @@
-import Graphs
+import Graphs: Graphs, AbstractGraph, nv
 
-"""An in-place version of [`graphstate`](@ref)."""
-function graphstate!(stab::Stabilizer)
+"""
+[Graph states](https://en.wikipedia.org/wiki/Graph_state) are a special type
+of entangled stabilizer states that can be represented by a graph.
+For a graph ``G=(V,E)`` the corresponding stabilizers are ``S_v = X_v \\prod_{u ∈ neighbors(v)} Z_u``.
+Notice that such tableau are symmetric and contain only X on diagonal and Z off-diagonal.
+There is a set of single qubit gates that converts any stabilizer state to a graph state.
+"""
+# TODO: implement a graph backend interface so this constructor is generic over graph backend
+mutable struct GraphState
+    graph::AbstractGraph # underlying graph
+    # To transform from stabilizer state to graph state, we apply VOP in the orders of H, InvPhase, Z
+    # NOTE: Hadamard gate sequence may be swapped with InvPhase sequences. But Z gate sequence shall not be swapped with the previous two.
+    h_idx::Vector{Int} # indices for Hadamard gate to transform a given stabilizer state into graph state
+    ip_idx::Vector{Int} # indices for InvPhase gate
+    z_idx::Vector{Int} # indices for Z gate
+end
+
+nqubits(g::GraphState) = nv(g)
+
+"""An in-place version of [`GraphState`](@ref)."""
+function GraphState!(stab::Stabilizer)
     n = nqubits(stab)
-    stab, r, s, permx, permz = canonicalize_gott!(stab)
+    # canonicalization is in-place
+    _, r, _, permx, permz = canonicalize_gott!(stab)
     perm = permx[permz]
-    h_idx = [perm[i] for i in (r+1):n] # Qubits in which X ↔ Z is needed
-    ip_idx = [perm[i] for i in 1:n if stab[i,i]==(true,true)] # Qubits for which Y → X is needed
-    phase_flips = [perm[i] for i in 1:n if phases(stab)[i]!=0x0]
+
+    # Swap X ↔ Z in the (r+1):n columns so we have only X,Y,I diagonal and only Z off-diagonal
+    h_idx = [perm[i] for i in (r+1):n]
+    # Swap Y → X on the diagonal so we have only X, I on the diagonal
+    ip_idx = [perm[i] for i in 1:n if stab[i,i]==(true,true)]
+    # Since ZXZ = -X, apply Z on rows with negative phase to make all phases positive.
+    z_idx = [perm[i] for i in 1:n if phases(stab)[i]!=0x0]
+
+    # canonicalized stabilizer tableau serves as an adjacency matrix
     graph = Graphs.SimpleGraphFromIterator((
         Graphs.SimpleEdge(perm[i],perm[j])
         for i in 1:n, j in 1:n
         if i!=j && stab[i,j]!=(false,false)
     ))
     Graphs.add_vertices!(graph, n-Graphs.nv(graph))
-    graph, h_idx, ip_idx, phase_flips
+    return GraphState(graph, h_idx, ip_idx, z_idx)
+end
+
+"""A helper function converting the gate indices from [`GraphState`](@ref) into a sequence of gates.
+
+```jldoctest
+julia> s = S" XXX
+              YZ_
+             -_ZZ";
+
+
+julia> g = GraphState(s);
+
+
+julia> for gate in graph_gate_sequence(g) apply!(s, gate) end
+
+
+julia> s # This is now a graph state (notice you need to multiply row 1 by row 2)
++ YYZ
++ XZ_
++ _ZX
+
+julia> canonicalize!(s) == canonicalize!(Stabilizer(g.graph))
+true
+```
+
+See also: [`graph_gate_sequence`](@ref)
+"""
+function graph_gate_sequence(self::GraphState)
+    vcat([sHadamard(i)::AbstractSingleQubitOperator for i in self.h_idx],
+         [sInvPhase(i)::AbstractSingleQubitOperator for i in self.ip_idx],
+         [sZ(i)::AbstractSingleQubitOperator for i in self.z_idx])
+end
+
+# Return the stabilizer state corresponding to the graph state
+function Stabilizer(self::GraphState)
+    s_raw = Stabilizer(self.graph)
+    for gate in reverse(graph_gate_sequence(self)) apply!(s_raw, inv(gate)) end
+    return s_raw
 end
 
 """ Convert any stabilizer state to a graph state
 
-[Graph states](https://en.wikipedia.org/wiki/Graph_state) are a special type
-of entangled stabilizer states that can be represented by a graph.
-For a graph ``G=(V,E)`` the corresponding stabilizers are ``S_v = X_v \\prod_{u ∈ N(v)} Z_u``.
-Notice that such tableau rows contain only a single X operator.
-There is a set of single qubit gates that converts any stabilizer state to a graph state.
-
-This function returns the graph state corresponding to a stabilizer and the gates that
-might be necessary to convert the stabilizer into a state representable as a graph.
-
-For a tableau `stab` you can convert it with:
-```julia
-graph, hadamard_idx, iphase_idx, flips_idx = graphstate()
-```
-where `graph` is the graph representation of `stab`,
-and the rest specifies the single-qubit gates converting `stab` to `graph`:
-`hadamard_idx` are the qubits that require a Hadamard gate (mapping X ↔ Z),
-`iphase_idx` are (different) qubits that require an inverse Phase gate (Y → X),
-and `flips_idx` are the qubits that require a phase flip (Pauli Z gate),
-after the previous two sets of gates.
+This function returns the graph state corresponding to a stabilizer.
 
 ```jldoctest graph
 julia> using Graphs
@@ -47,23 +93,23 @@ julia> s = S" XXX
              -_ZZ";
 
 
-julia> g, h_idx, ip_idx, z_idx = graphstate(s);
+julia> g = GraphState(s);
 
 
-julia> collect(edges(g))
+julia> collect(edges(g.graph))
 2-element Vector{Graphs.SimpleGraphs.SimpleEdge{Int64}}:
  Edge 1 => 2
  Edge 1 => 3
 
-julia> h_idx
+julia> g.h_idx
 2-element Vector{Int64}:
  2
  3
 
-julia> ip_idx
+julia> g.ip_idx
 Int64[]
 
-julia> z_idx
+julia> g.z_idx
 1-element Vector{Int64}:
  3
 ```
@@ -83,18 +129,31 @@ julia> collect(edges( Graph(bell()) ))
 ```
 
 For a version that does not copy the stabilizer, but rather
-performs transformations in-place, use `graphstate!`. It would
+performs transformations in-place, use `GraphState!`. It would
 perform `canonicalize_gott!` on its argument as it finds a way
 to convert it to a graph state.
 """
-graphstate(s::AbstractStabilizer) = graphstate!(copy(stabilizerview(s)))
+GraphState(stab::AbstractStabilizer) = GraphState!(copy(stabilizerview(stab)))
 
-Graphs.Graph(s::AbstractStabilizer) = graphstate(s)[1]
+function Graphs.Graph(s::AbstractStabilizer)
+    stab = copy(stabilizerview(s))
+    n = nqubits(stab)
+    _, _, _, permx, permz = canonicalize_gott!(stab)
+    perm = permx[permz]
+
+    graph = Graphs.SimpleGraphFromIterator((
+        Graphs.SimpleEdge(perm[i],perm[j])
+        for i in 1:n, j in 1:n
+        if i!=j && stab[i,j]!=(false,false)
+    ))
+    Graphs.add_vertices!(graph, n-Graphs.nv(graph))
+    return graph
+end
 
 """Convert a graph representing a stabilizer state to an explicit Stabilizer.
 
-See also: [`graphstate`](@ref)"""
-function Stabilizer(g::Graphs.Graph)
+See also: [`GraphState`](@ref)"""
+function Stabilizer(g::AbstractGraph)
     s = zero(Stabilizer, Graphs.nv(g)) # number of vertices
     for v in Graphs.vertices(g)
         s[v,v]=(true,false)
@@ -105,7 +164,8 @@ function Stabilizer(g::Graphs.Graph)
     return s
 end
 
-"""A helper function converting the gate indices from [`graphstate`](@ref) into a sequence of gates.
+
+"""A helper function converting the gate sequences from [`GraphState`](@ref) into a Clifford operator.
 
 ```jldoctest
 julia> s = S" XXX
@@ -113,42 +173,10 @@ julia> s = S" XXX
              -_ZZ";
 
 
-julia> graph, h_idx, ip_idx, z_idx = graphstate(s);
+julia> g = GraphState(s);
 
 
-julia> gates = graph_gatesequence(h_idx, ip_idx, z_idx);
-
-
-julia> for gate in vcat(gates...) apply!(s, gate) end
-
-
-julia> s # This is now a graph state (notice you need to multiply row 1 by row 2)
-+ YYZ
-+ XZ_
-+ _ZX
-
-julia> canonicalize!(s) == canonicalize!(Stabilizer(graph))
-true
-```
-
-See also: [`graph_gatesequence`](@ref)
-"""
-function graph_gatesequence(h_idx::Vector{Int}, ip_idx::Vector{Int}, z_idx::Vector{Int})
-    ([sHadamard(i) for i in h_idx], [sInvPhase(i) for i in ip_idx], [sZ(i) for i in z_idx])
-end
-
-"""A helper function converting the gate indices from [`graphstate`](@ref) into a Clifford operator.
-
-```jldoctest
-julia> s = S" XXX
-              YZ_
-             -_ZZ";
-
-
-julia> graph, h_idx, ip_idx, z_idx = graphstate(s);
-
-
-julia> gate = graph_gate(h_idx, ip_idx, z_idx, nqubits(s));
+julia> gate = graph_gate(g);
 
 
 julia> apply!(s, gate) # This is now a graph state (notice you need to multiply row 1 by row 2)
@@ -156,13 +184,16 @@ julia> apply!(s, gate) # This is now a graph state (notice you need to multiply 
 + XZ_
 + _ZX
 
-julia> canonicalize!(s) == canonicalize!(Stabilizer(graph))
+julia> canonicalize!(s) == canonicalize!(Stabilizer(g.graph))
 true
 ```
 
-See also: [`graph_gatesequence`](@ref)
+See also: [`graph_gate_sequence`](@ref)
 """
-function graph_gate(h_idx, ip_idx, z_idx, n)
+function graph_gate(g::GraphState)
+    h_idx, ip_idx, z_idx = g.h_idx, g.ip_idx, g.z_idx
+    n = nv(g.graph)
+
     c = one(CliffordOperator,n)
     t = tab(c)
     x = @view t[1:n]
