ac1dd492285731f2a69ddccd6cfcec60cdb3cbb8
Fe-r-oz <feroz.ahmad.email@gmail.com>
2025-06-29 14:18:03 +0500

    🧹 add basic tests test_nowarn that just check there is no method errors now

--- test/test_ecc_throws.jl
+++ test/test_ecc_throws.jl
@@ -1,6 +1,8 @@
 @testitem "ECC throws" begin
 
-    using QuantumClifford.ECC: ReedMuller, BCH, RecursiveReedMuller, Golay, Triangular488, Triangular666
+    using Hecke
+    using Hecke: group_algebra, GF, abelian_group, gens, one, representation_matrix
+    using QuantumClifford.ECC: ReedMuller, BCH, RecursiveReedMuller, Golay, Triangular488, Triangular666, LiftedCode, code_k, code_n, code_s, parity_matrix
 
     @test_throws ArgumentError ReedMuller(-1, 3)
     @test_throws ArgumentError ReedMuller(1, 0)
@@ -19,4 +21,18 @@
     @test_throws ArgumentError Triangular488(1)
     @test_throws ArgumentError Triangular666(8)
     @test_throws ArgumentError Triangular666(1)
+
+    l = 12; GA = group_algebra(GF(2), abelian_group(l)); x = gens(GA)[]
+    B = reshape([1 + x + x^3 + x^6], (1, 1))
+    c = LiftedCode(B, repr = representation_matrix)
+    @test_nowarn parity_matrix(c)
+    @test_nowarn code_n(c)
+    @test_nowarn code_k(c)
+    @test_nowarn code_s(c)
+    base_matrix = [0 0 0 0; 0 1 2 5; 0 6 3 1]; l = 3;
+    @test_nowarn LiftedCode(base_matrix, l);
+    @test_nowarn parity_matrix(c)
+    @test_nowarn code_n(c)
+    @test_nowarn code_k(c)
+    @test_nowarn code_s(c)
 end
