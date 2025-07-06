1fcddb9fb028f7f5bfe9e4716adeb8832f90da78
sagnikpal2004 <sagnikpal@umass.edu>
2025-07-07 04:09:37 +0530

    better tests

--- test/test_inv.jl
+++ test/test_inv.jl
@@ -2,7 +2,8 @@
     using InteractiveUtils
 
     @testset "Apply Inv" begin
-        stabilizers = [S"I", S"X", S"Y", S"Z"]
+        stabilizers = [S" I", S" X", S" Y", S" Z",
+                       S"-I", S"-X", S"-Y", S"-Z",]
         for gate in subtypes(AbstractSingleQubitOperator)
             gate == SingleQubitOperator && continue
             for stab in stabilizers
@@ -12,10 +13,14 @@
     end
 
     @testset "Apply Inv two-qubit" begin
-        stabilizers = [S"II", S"IX", S"IY", S"IZ",
-                       S"XI", S"XX", S"XY", S"XZ",
-                       S"YI", S"YX", S"YY", S"YZ",
-                       S"ZI", S"ZX", S"ZY", S"ZZ"]
+        stabilizers = [S" II", S" IX", S" IY", S" IZ",
+                       S"-II", S"-IX", S"-IY", S"-IZ",
+                       S" XI", S" XX", S" XY", S" XZ",
+                       S"-XI", S"-XX", S"-XY", S"-XZ",
+                       S" YI", S" YX", S" YY", S" YZ",
+                       S"-YI", S"-YX", S"-YY", S"-YZ",
+                       S" ZI", S" ZX", S" ZY", S" ZZ",
+                       S"-ZI", S"-ZX", S"-ZY", S"-ZZ",]                        
         for gate in subtypes(AbstractTwoQubitOperator)
             for stab in stabilizers
                 @test apply_inv!(stab, gate(1, 2)) == apply!(stab, inv(CliffordOperator(gate(1, 2), 2)))
