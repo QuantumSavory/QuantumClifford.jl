1518bdd6f2ce52529e76833b5641cf4c75a42b5e
Feroz Ahmed Mian <feroz.ahmed.mian@gmail.com>
2025-07-08 05:24:10 +0500

    export honeycomb

--- ext/QuantumCliffordHeckeExt/QuantumCliffordHeckeExt.jl
+++ ext/QuantumCliffordHeckeExt/QuantumCliffordHeckeExt.jl
@@ -20,7 +20,7 @@ import QECCore: AbstractECC, CSS,
 
 # exported from extension so that Documenter.jl sees them when autogenerating API lists
 export hgp, two_block_group_algebra_codes, generalized_bicycle_codes, bicycle_codes, haah_cubic_codes,
-    LPCode, LiftedCode
+    LPCode, LiftedCode, honeycomb_color_codes
 
 include("util.jl")
 include("types.jl")
