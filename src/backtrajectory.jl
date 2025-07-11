f25f4f0560eac29daf6d261dd94e8d8ece21b59f
sagnikpal2004 <sagnikpal@umass.edu>
2025-07-12 00:36:55 +0530

    more symbolic prepends

--- src/backtrajectory.jl
+++ src/backtrajectory.jl
@@ -19,7 +19,7 @@ function expand_circuit(circuit::Vector{<:AbstractOperation})
 end
 
 
-"""the `prepend!` function is used to prepend any quantum operation to unitary Clifford operations"""
+"""the `prepend!` function is used to prepend any quantum operation to unitary Clifford and Pauli operations"""
 function prepend! end
 """the prepend_inv! function is used to prepend the inverse of a quantum operation to unitary Clifford operations"""
 function prepend_inv! end
@@ -35,12 +35,12 @@ function prepend_inv! end
 
 # FAST versions of prepend! and prepend_inv! for CliffordOperator - TODO
 function prepend!(l::CliffordOperator, r; phases)
-    @warn "Slow prepend! operation"
+    @warn "Slow prepend! operation: $r"
     prepend!(l, CliffordOperator(r, nqubits(l)); phases=phases)
 end
 
 function prepend!(l::CliffordOperator, r::CliffordOperator; phases=false)
-    nqubits(l)==nqubits(r) || throw(DimensionMismatch("The tableau and the Clifford operator need to act on the same number of qubits. Consider specifying an array of indices as a third argument to the `apply!` function to avoid this error."))
+    nqubits(l)==nqubits(r) || throw(DimensionMismatch("The two Clifford operators need to act on the same number of qubits."))
     l_tab = tab(l)
     r_tab = tab(r)
     threadlocal = l.buffer
@@ -76,7 +76,7 @@ end
 
 
 function prepend_inv!(l::CliffordOperator, r; phases=false)
-    @warn "Slow prepend_inv! operation"
+    @warn "Slow prepend_inv! operation: $r"
     prepend!(l, inv(CliffordOperator(r, nqubits(l))); phases=phases)
 end
 function prepend_inv!(l::CliffordOperator, r::CliffordOperator; phases=false)
@@ -84,6 +84,38 @@ function prepend_inv!(l::CliffordOperator, r::CliffordOperator; phases=false)
 end
 
 # Symbolic
+function prepend!(l::CliffordOperator, r::PauliOperator; phases=false)
+    nqubits(l)==nqubits(r) || throw(DimensionMismatch("The Clifford and Pauli operators need to act on the same number of qubits."))
+    tab(l).phases[nqubits(l)+1:end] .⊻= r[1:nqubits(l)]
+    tab(l).phases[1:nqubits(l)] .⊻= r[nqubits(l)+1:end]
+end
+# struct IgnoreAntiCommute
+#     rhs::PauliOperator
+# end
+# Base.(*)(l::PauliOpearator, r::IgnoreAntiCommute)
+# end
+function prepend!(l::CliffordOperator, r::sHadamard; phases=false)
+    l[r.q], l[nqubits(l)+r.q] = l[nqubits(l)+r.q], l[r.q]
+    return l
+end
+function prepend!(l::CliffordOperator, r::sHadamardXY; phases=false)
+    error("Not implemented: prepend!(l, r::sHadamardXY)")
+    l[r.q] = 
+    prepend!(l, sY(r.q); phases=phases)
+    return l
+end
+function prepend!(l::CliffordOperator, r::sHadamardYZ; phases=false)
+    error("Not implemented: prepend!(l, r::sHadamardYZ)")
+    l[nqubits(l)+r.q] = 
+    prepend!(l, sZ(r.q); phases=phases)
+    return l
+end
+function prepend!(l::CliffordOperator, r::sPhase; phases=false)
+    error("Not implemented: prepend!(l, r::sPhase)")
+end
+function prepend!(l::CliffordOperator, r::sInvPhase; phases=false)
+    error("Not implemented: prepend!(l, r::sInvPhase)")
+end
 function prepend!(l::CliffordOperator, r::sX; phases=false)
     tab(l).phases[nqubits(l)+r.q] ⊻= 0x02
     return l
@@ -97,6 +129,148 @@ function prepend!(l::CliffordOperator, r::sZ; phases=false)
     tab(l).phases[r.q] ⊻= 0x02
     return l
 end
+function prepend!(l::CliffordOperator, r::sSQRTX; phases=false)
+    error("Not implemented: prepend!(l, r::$(typeof(r)))")
+    return l
+end
+function prepend!(l::CliffordOperator, r::sInvSQRTX; phases=false)
+    error("Not implemented: prepend!(l, r::$(typeof(r)))")
+    return l
+end
+function prepend!(l::CliffordOperator, r::sSQRTY; phases=false)
+    error("Not implemented: prepend!(l, r::$(typeof(r)))")
+    return l
+end
+function prepend!(l::CliffordOperator, r::sInvSQRTY; phases=false)
+    error("Not implemented: prepend!(l, r::$(typeof(r)))")
+    return l
+end
+function prepend!(l::CliffordOperator, r::sCXYZ; phases=false)
+    error("Not implemented: prepend!(l, r::$(typeof(r)))")
+    return l
+end
+function prepend!(l::CliffordOperator, r::sCZYX; phases=false)
+    error("Not implemented: prepend!(l, r::$(typeof(r)))")
+    return l
+end
+function prepend!(l::CliffordOperator, r::sId1; phases=false)
+    error("Not implemented: prepend!(l, r::sId1)")
+    return l
+end
+function prepend!(l::CliffordOperator, r::SingleQubitOperator; phases=false)
+    error("Not implemented: prepend!(l. r::SingleQubitOperator)")
+    return l
+end
+
+function prepend!(l::CliffordOperator, r::sSWAP; phases=false)
+    l[nqubits(l)+r.q1], l[nqubits(l)+r.q2] = l[nqubits(l)+r.q2], l[nqubits(l)+r.q1]
+    l[r.q1], l[r.q2] = l[r.q2], l[r.q1]
+    return l
+end
+function prepend!(l::CliffordOperator, r::sSWAPCX; phases=false)
+    error("Not implemented: prepend!(l, r::$(typeof(r)))")
+    return l
+end
+function prepend!(l::CliffordOperator, r::sInvSWAPCX; phases=false)
+    error("Not implemented: prepend!(l, r::$(typeof(r)))")
+    return l
+end
+function prepend!(l::CliffordOperator, r::sISWAP; phases=false)
+    prepend!(l, sSWAP(r.q1, r.q2); phases=phases)
+    prepend!(l, sZCZ(r.q1, r.q2); phases=phases)
+    prepend!(l, sSQRTZ(r.q1); phases=phases)
+    prepend!(l, sSQRTZ(r.q2); phases=phases)
+    return l
+end
+function prepend!(l::CliffordOperator, r::sInvISWAP; phases=false)
+    error("Not implemented: prepend!(l, r::$(typeof(r)))")
+    return l
+end
+function prepend!(l::CliffordOperator, r::sCZSWAP; phases=false)
+    error("Not implemented: prepend!(l, r::$(typeof(r)))")
+    return l
+end
+function prepend!(l::CliffordOperator, r::sCXSWAP; phases=false)
+    error("Not implemented: prepend!(l, r::$(typeof(r)))")
+    return l
+end
+function prepend!(l::CliffordOperator, r::sCNOT; phases=false)
+    l[nqubits(l)+r.q2] *= l[nqubits(l)+r.q1]
+    l[r.q1] *= l[r.q2]
+    return l
+end
+function prepend!(l::CliffordOperator, r::sCPHASE; phases=false)
+    error("Not implemented: prepend!(l, r::$(typeof(r)))")
+    return l
+end
+function prepend!(l::CliffordOperator, r::sZCX; phases=false)
+    error("Not implemented: prepend!(l, r::$(typeof(r)))")
+    return l
+end
+function prepend!(l::CliffordOperator, r::sZCY; phases=false)
+    error("Not implemented: prepend!(l, r::$(typeof(r)))")
+    return l
+end
+function prepend!(l::CliffordOperator, r::sZCZ; phases=false)
+    error("Not implemented: prepend!(l, r::$(typeof(r)))")
+    return l
+end
+function prepend!(l::CliffordOperator, r::sXCX; phases=false)
+    error("Not implemented: prepend!(l, r::$(typeof(r)))")
+    return l
+end
+function prepend!(l::CliffordOperator, r::sXCY; phases=false)
+    error("Not implemented: prepend!(l, r::$(typeof(r)))")
+    return l
+end
+function prepend!(l::CliffordOperator, r::sXCZ; phases=false)
+    error("Not implemented: prepend!(l, r::$(typeof(r)))")
+    return l
+end
+function prepend!(l::CliffordOperator, r::sYCX; phases=false)
+    error("Not implemented: prepend!(l, r::$(typeof(r)))")
+    return l
+end
+function prepend!(l::CliffordOperator, r::sYCY; phases=false)
+    error("Not implemented: prepend!(l, r::$(typeof(r)))")
+    return l
+end
+function prepend!(l::CliffordOperator, r::sYCZ; phases=false)
+    error("Not implemented: prepend!(l, r::$(typeof(r)))")
+    return l
+end
+function prepend!(l::CliffordOperator, r::sZCrY; phases=false)
+    error("Not implemented: prepend!(l, r::$(typeof(r)))")
+    return l
+end
+function prepend!(l::CliffordOperator, r::sInvZCrY; phases=false)
+    error("Not implemented: prepend!(l, r::$(typeof(r)))")
+    return l
+end
+function prepend!(l::CliffordOperator, r::sSQRTZZ; phases=false)
+    error("Not implemented: prepend!(l, r::$(typeof(r)))")
+    return l
+end
+function prepend!(l::CliffordOperator, r::sInvSQRTZZ; phases=false)
+    error("Not implemented: prepend!(l, r::$(typeof(r)))")
+    return l
+end
+function prepend!(l::CliffordOperator, r::sSQRTXX; phases=false)
+    error("Not implemented: prepend!(l, r::$(typeof(r)))")
+    return l
+end
+function prepend!(l::CliffordOperator, r::sInvSQRTXX; phases=false)
+    error("Not implemented: prepend!(l, r::$(typeof(r)))")
+    return l
+end
+function prepend!(l::CliffordOperator, r::sSQRTYY; phases=false)
+    error("Not implemented: prepend!(l, r::$(typeof(r)))")
+    return l
+end
+function prepend!(l::CliffordOperator, r::sInvSQRTYY; phases=false)
+    error("Not implemented: prepend!(l, r::$(typeof(r)))")
+    return l
+end
 
 function prepend_inv!(l::CliffordOperator, r::sX; phases=false)
     tab(l).phases[nqubits(l)+r.q] ⊻= 0x02
@@ -108,6 +282,28 @@ function prepend_inv!(l::CliffordOperator, r::sY; phases=false)
 end
 function prepend_inv!(l::CliffordOperator, r::sZ; phases=false)
     tab(l).phases[r.q] ⊻= 0x02
+    return l
+end
+function prepend_inv!(l::CliffordOperator, r::sHadamard; phases=false)
+    l[r.q], l[nqubits(l)+r.q] = l[nqubits(l)+r.q], l[r.q]
+    return l
+end
+function prepend_inv!(l::CliffordOperator, r::sHadamardYZ; phases=false)
+    error("Not implemented: prepend_inv!(l, r::sHadamardYZ)")
+    return l
+end
+function prepend_inv!(l::CliffordOperator, r::sHadamardXY; phases=false)
+    error("Not implemented: prepend_inv!(l, r::sHadamardXY)")
+    return l
+end
+function prepend_inv!(l::CliffordOperator, r::sCNOT; phases=false)
+    l[nqubits(l)+r.q2] *= l[nqubits(l)+r.q1]
+    l[r.q1] *= l[r.q2]
+    return l
+end
+function prepend_inv!(l::CliffordOperator, r::sSWAP; phases=false)
+    l[nqubits(l)+r.q1], l[nqubits(l)+r.q2] = l[nqubits(l)+r.q2], l[nqubits(l)+r.q1]
+    l[r.q1], l[r.q2] = l[r.q2], l[r.q1]
     return l
 end
 
@@ -134,7 +330,7 @@ function backtrajectory(circuit0::Vector{<:AbstractOperation}, n::Int)
     
     for op in circuit
         if op isa AbstractCliffordOperator
-            T = prepend_inv!(T, op; phases=true)
+            prepend_inv!(T, op; phases=true)
         elseif op isa sMZ
             pivot = 0   
             for c in 1:n
