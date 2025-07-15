280cd5ee7a3a7d0c3551831805f196ca1b4e6d7a
Fe-r-oz <feroz.ahmed.mian@gmail.com>
2025-07-15 21:50:22 +0500

    remove twobga_codes from the codes list as it contains non-abelian codes that throw error with the older implementation

--- test/test_ecc_lpcode_simpler_repr.jl
+++ test/test_ecc_lpcode_simpler_repr.jl
@@ -8,7 +8,7 @@
     const QuantumCliffordHeckeExt = Base.get_extension(QuantumClifford, :QuantumCliffordHeckeExt)
 
     include("test_ecc_base.jl")
-    codes = vcat(LP04, LP118, test_gb_codes, test_bb_codes, test_mbb_codes, test_coprimeBB_codes, test_hcubic_codes, test_twobga_codes, test_honeycomb_color_codes);
+    codes = vcat(LP04, LP118, test_gb_codes, test_bb_codes, test_mbb_codes, test_coprimeBB_codes, test_hcubic_codes, test_honeycomb_color_codes);
 
     for c in codes
         @test QECCore.parity_matrix_xz(c) == QuantumCliffordHeckeExt._parity_matrix_xz_if_comm_algebra(c)
