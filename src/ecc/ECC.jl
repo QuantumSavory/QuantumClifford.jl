ac1de03858400b2c24e902f101b711ca7ae55f5d
Feroz Ahmed Mian <feroz.ahmed.mian@gmail.com>
2025-07-22 22:41:52 +0500

    D-dimensional surface and toric codes via Oscar's chain complexes and GF2 homology (#534)

--- src/ecc/ECC.jl
+++ src/ecc/ECC.jl
@@ -33,8 +33,9 @@ export parity_checks, parity_matrix_x, parity_matrix_z, iscss,
     haah_cubic_codes, twobga_from_fp_group, twobga_from_direct_product,
     random_brickwork_circuit_code, random_all_to_all_circuit_code,
     Triangular488, Triangular666, honeycomb_color_codes, DelfosseReichardt,
-    DelfosseReichardtRepCode, DelfosseReichardt823, QuantumTannerGraphProduct,
-    CyclicQuantumTannerGraphProduct,
+    DelfosseReichardtRepCode, DelfosseReichardt823,
+    QuantumTannerGraphProduct, CyclicQuantumTannerGraphProduct,
+    DDimensionalSurfaceCode, DDimensionalToricCode, boundary_maps,
     evaluate_decoder,
     CommutationCheckECCSetup, NaiveSyndromeECCSetup, ShorSyndromeECCSetup,
     TableDecoder,
@@ -400,5 +401,8 @@ include("codes/classical/bch.jl")
 # qLDPC
 include("codes/classical/lifted.jl")
 include("codes/lifted_product.jl")
+
+# higher dimensional codes
+include("codes/d_dimensional_codes.jl")
 
 end #module
