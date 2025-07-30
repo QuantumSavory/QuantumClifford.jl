170b0039151d6fcabbd69c0edab37d195c9738bb
Fe-r-oz <feroz.ahmed.mian@gmail.com>
2025-07-30 18:08:47 +0500

    rm Oscar from test-project.toml and add Stefan comments

--- lib/QECCore/test/runtests.jl
+++ lib/QECCore/test/runtests.jl
@@ -1,4 +1,15 @@
 using QECCore
 using TestItemRunner
 
+Oscar_flag = false
+
+if Sys.iswindows() || Sys.ARCH != :x86_64
+    @info "Skipping Oscar tests -- only supported x86_64 *NIX platforms."
+else
+    Oscar_flag = VERSION >= v"1.11"
+    !Oscar_flag && @info "Skipping Oscar tests -- not tested on Julia < 1.11"
+end
+
+Oscar_flag && Pkg.add("Oscar")
+
 @run_package_tests
