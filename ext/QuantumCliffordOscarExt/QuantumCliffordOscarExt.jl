4c80ff017fa6398a5e0c0335c3dbc0a775d69ebc
Fe-r-oz <feroz.ahmed.mian@gmail.com>
2025-08-12 13:08:12 +0500

    prove the Topological Order condition for Generalized Toric codes using Oscar

--- ext/QuantumCliffordOscarExt/QuantumCliffordOscarExt.jl
+++ ext/QuantumCliffordOscarExt/QuantumCliffordOscarExt.jl
@@ -9,12 +9,13 @@ import QuantumClifford: Stabilizer
 import Nemo
 import Nemo: FqFieldElem
 import Hecke: group_algebra, GF, abelian_group, gens, quo, one, GroupAlgebra,
-   GroupAlgebraElem, direct_product, sub, ZZ, lift
+   GroupAlgebraElem, direct_product, sub, ZZ, lift, polynomial_ring
 import Oscar
 import Oscar: free_group, small_group_identification, describe, order, FPGroupElem, FPGroup,
     BasicGAPGroupElem, DirectProductGroup, cyclic_group, free_module, hom, transpose, tensor_product,
     chain_complex, total_complex, map, summands, MatElem, matrix, nrows, ncols, kernel, dim, range, image,
-    base_ring, ComplexOfMorphisms, coefficients, laurent_polynomial_ring, hnf_with_transform
+    base_ring, ComplexOfMorphisms, coefficients, laurent_polynomial_ring, hnf_with_transform, ideal, intersect,
+    ==, is_coprime, quo
 import Oscar.Generic.DirectSumModule
 import Oscar.Generic.LaurentMPolyWrap
 import Oscar.Generic.exponent_vectors
