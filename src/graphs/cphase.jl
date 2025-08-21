80fac14c886ef575cce454ceeda52da25019b77c
Harry Ying <lexugeyky@outlook.com>
2025-08-21 03:19:38 +0100

    fix: always use non-operand neighbors as swapping partner - always use non-operand neighbors as swapping partner in all cases - add more comment to explain the algorithm - add a relevant test case

--- src/graphs/cphase.jl
+++ src/graphs/cphase.jl
@@ -102,6 +102,29 @@ function apply!(gs::GraphState, gate::sCPHASE)
     q1, q2 = affectedqubits(gate)
     g = graph(gs)
 
+    # The algorithm wants to end up in either of the two cases
+    # 1. Fast path: both U₁, U₂ ∈ Z_COMMUTATION_SUBGROUP. In that case we can just toggle the edge between q1, q2.
+    # 2. Fallback: for each of the U₁, U₂, it is either
+    #    - ∈ Z_COMMUTATION_SUBGROUP
+    #    - or, its neighbor is ∈ {q1, q2}
+    #    then we can apply the ISOLATED_CPHASE_TABLE directly (see the paper for details on why)
+
+
+    # In theory we can always apply the lookup table as case 2 includes case 1.
+    # However, case 1 is retianed for performance consideration (1.5x performance penalty)
+    # To always use fallback route, one can simply write:
+    #
+    # for q in [q1, q2, q1]
+    #     targets = filter(x-> x ∉ [q1, q2], inneighbors(g, q))
+    #     if !isempty(targets)
+    #         remove_vop!(gs, q, targets[1])
+    #     end
+    # end
+    #
+    # apply_isolated_cphase_lookup_table!(gs, q1, q2)
+    #
+    # We can investigate the performance difference in the future
+
     if has_edge(g, q1, q2)
         # If q1 and q2 are connected, use q2 as a swapping partner of q1 to transfer VOPs of q1 to q2
         remove_vop!(gs, q1, q2)
@@ -120,24 +143,29 @@ function apply!(gs::GraphState, gate::sCPHASE)
             remove_vop!(gs, q2, targets[1])
 
             # in which case we are sure both U₁, U₂ ∈ Z_COMMUTATION_SUBGROUP,
-            # and we could just toggle the edge between them
+            # and we could just toggle the edge between them (fast path)
             toggle_edge!(g, q1, q2)
         else
             # If we don't have a non-operand swapping partner for q2, the VOP of q1 is identity, and q2 is not connected to other qubits,
-            # so we could use the lookup table (see the paper for why even q1's potential non-operand neighbor doesn't matter)
+            # so we could use the lookup table (fallback path)
             apply_isolated_cphase_lookup_table!(gs, q1, q2)
         end
    else
         # try reduce both U₁, U₂ to identity
         for q in [q1,q2]
-            nghbr = inneighbors(g, q)
-            if !isempty(nghbr)
-                remove_vop!(gs, q, nghbr[1])
+            # non-operand neighbors
+            targets = filter(x-> (x != q1) && (x != q2), inneighbors(g, q))
+            # since q1 and q2 are disconnected, when non-operand neighbor is empty, it means the qubit is truly isolated
+            # and any later local complementation won't change this. Therefore, we are safe to do a one-pass [q1, q2] removal above
+            # instead of a general [q1, q2, q1] if we are dealing with the general case.
+            if !isempty(targets)
+                remove_vop!(gs, q, targets[1])
             end
         end
 
-        # now both U₁, U₂ ∈ Z_COMMUTATION_SUBGROUP, we can apply the table
+        # in the fallback path, apply the lookup table
         apply_isolated_cphase_lookup_table!(gs, q1, q2)
    end
+
    return gs
 end
