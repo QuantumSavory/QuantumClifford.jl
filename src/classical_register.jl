c8a35c4d5b53ec3f4e152dfbfe7c229c50af2dfe
Sai Rohan Reddy Kondlapudi <rohan@vl965-172-31-127-152.wireless.umass.edu>
2025-09-18 14:58:17 -0400

    fixed some more errors

--- src/classical_register.jl
+++ src/classical_register.jl
@@ -133,14 +133,14 @@ function applybranches(s::Register, op::PauliMeasurement; max_order=1) # TODO th
     if isnothing(r)
         s1 = s
         phases(stabilizerview(s1.stab))[anticom] = 0x00
-        setBit(s1.bits, op.bit, false)
+        s1.bits[op.bit] = false
         s2 = copy(s)
         phases(stabilizerview(s2.stab))[anticom] = 0x02
-        setBit(s2.bits, op.bit, true)
+        s2.bits[op.bit] = true
         push!(new_branches, (s1,continue_stat,1/2,0))
         push!(new_branches, (s2,continue_stat,1/2,0))
     else
-        setBit(s.bits, op.bit, r==0x02)
+        s.bits[op.bit] = r==0x02
         push!(new_branches, (s,continue_stat,1,0))
     end
     new_branches
@@ -153,14 +153,14 @@ function applybranches(s::Register, op::AbstractMeasurement; max_order=1)
     if isnothing(r)
         s1 = s
         phases(stabilizerview(s1.stab))[anticom] = 0x00
-        setBit(s1.bits, op.bit, false)
+        s1.bits[op.bit] = false
         s2 = copy(s)
         phases(stabilizerview(s2.stab))[anticom] = 0x02
-        setBit(s2.bits, op.bit, true)
+        s2.bits[op.bit] = true
         push!(new_branches, (s1,continue_stat,1/2,0))
         push!(new_branches, (s2,continue_stat,1/2,0))
     else
-        setBit(s.bits, op.bit, r==0x02)
+        s.bits[op.bit] = r==0x02
         push!(new_branches, (s,continue_stat,1,0))
     end
     new_branches
