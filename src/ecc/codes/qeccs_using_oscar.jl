3d88013f80f4341ee85ea7b503b9270cda051c94
Fe-r-oz <feroz.ahmed.mian@gmail.com>
2025-09-20 21:28:59 -0400

    bivariate bicycle code using polynomial quotient ring

--- src/ecc/codes/qeccs_using_oscar.jl
+++ src/ecc/codes/qeccs_using_oscar.jl
@@ -74,3 +74,13 @@ function TrivariateTricycleCode(args...; kwargs...)
     end
     return ext.TrivariateTricycleCode(args...; kwargs...)
 end
+
+"""Bivariate Bicycle codes ([jacob2025singleshotdecodingfaulttolerantgates](@cite))
+Implemented as a package extension with `Oscar`. Check the [QuantumClifford documentation](http://qc.quantumsavory.org/stable/ECC_API/) for more details on that extension."""
+function BivariateBicycleCode(args...; kwargs...)
+    ext = Base.get_extension(QuantumClifford, :QuantumCliffordOscarExt)
+    if isnothing(ext)
+        throw("The `BivariateBicycleCode` depends on the package `Oscar` but you have not installed or imported it yet. Immediately after you import `Oscar`, the `BivariateBicycleCode` will be available.")
+    end
+    return ext.BivariateBicycleCode(args...; kwargs...)
+end
