43bb8e85cc9005feb815b8c8b5efc99ec4dec908
Sai Rohan Reddy Kondlapudi <kondlapudisairohanreddy@gmail.com>
2025-09-26 21:12:18 -0400

    refined decodercorrectiongate

--- src/ecc/correction_gate.jl
+++ src/ecc/correction_gate.jl
@@ -2,25 +2,33 @@ using QuantumClifford:Register, AbstractOperation, applywstatus!, PauliOperator
 """A type of Decision Gate that uses the decoder's lookup table to apply a correction gate based on the syndrome of the input register.
  Does not require an explicit decision function."""
 struct DecoderCorrectionGate <: AbstractOperation
-    decoderx::AbstractSyndromeDecoder# just a function that maps inputbits to an operation on affectedqubits
-    affectedqubits::Vector{Int}
-    inputbits::Vector{Int}   
-    function DecoderCorrectionGate(dec::TableDecoder, qs, bs)
-        return new(dec, collect(Int, qs),collect(Int, bs))
+    decoder::AbstractSyndromeDecoder# just a function that maps inputbits to an operation on affectedqubits
+    affected_qubits::Vector{Int}
+    input_bits::Vector{Int}   
+    
+    function DecoderCorrectionGate(dec::AbstractSyndromeDecoder, affected_qubits, input_bits)
+        qs = collect(Int, affected_qubits)
+        bs = collect(Int, input_bits)
+        if length(qs)!=dec.n
+            throw(ArgumentError(lazy"Decoder expects $dec.n qubits, got $(length(affected_qubits))"))
+        end
+        if length(bs)!=dec.s
+            throw(ArgumentError(lazy"Decoder expects $dec.s qubits, got $(length(input_bits))"))
+        end
+        return new(dec, qs,bs)
     end
 end
 function QuantumClifford.apply!(state::Register, op::DecoderCorrectionGate)
-  lookup = op.decoder.lookup_table # use the decode function
-  key = Vector{bool}(state.bits[op.inputbits])
-  if !haskey(lookup, key)
-        return state
-  end
-  n2bit_array = lookup[key]
-  pauli_operator = PauliOperator(n2bit_array)
-  apply!(state, pauli_operator, op.affectedqubits) 
-  state
+  targets = op.affected_qubits
+  key = Vector{Bool}(state.bits[op.input_bits])
+  correction = decode(op.decoder, key)  
+  correction === nothing && return state
+  pauli_operator = PauliOperator(correction)
+  apply!(state, pauli_operator, targets) 
+  return state
 end
 applybranches(s::Register, op::DecoderCorrectionGate; max_order=1) = [(applywstatus!(copy(s),op)...,1,0)]
 
 affectedqubits(op::DecoderCorrectionGate) = op.affected_qubits
 affectedbits(op::DecoderCorrectionGate)   = op.input_bits
+
