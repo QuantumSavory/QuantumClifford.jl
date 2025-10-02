7dca74f74137e54e86c4675f8a77daa72ae7cb95
Sai Rohan Reddy Kondlapudi <kondlapudisairohanreddy@gmail.com>
2025-10-01 22:59:06 -0400

    made changes to decodercorrectiongate

--- src/ecc/decoder_correction_gate.jl
+++ src/ecc/decoder_correction_gate.jl
@@ -1,27 +1,27 @@
 using QuantumClifford:Register, AbstractOperation, applywstatus!, PauliOperator
 import QuantumClifford:affectedqubits,affectedbits,applybranches
-"""A type of Decision Gate that uses the decoder's lookup table to apply a correction gate based on the syndrome of the input register.
- Does not require an explicit decision function."""
+"""A gate that uses a lookup table to apply a correction gate based on the syndrome of the input register."""
 struct DecoderCorrectionGate <: AbstractOperation
     decoder::AbstractSyndromeDecoder# just a function that maps inputbits to an operation on affectedqubits
-    affected_qubits::Vector{Int}
-    input_bits::Vector{Int}   
+    data_qubits::Vector{Int}
+    syndrome_bits::Vector{Int}   
     
-    function DecoderCorrectionGate(dec::AbstractSyndromeDecoder, affected_qubits, input_bits)
-        qs = collect(Int, affected_qubits)
-        bs = collect(Int, input_bits)
-        if length(qs)!=dec.n
-            throw(ArgumentError(lazy"Decoder expects $dec.n qubits, got $(length(affected_qubits))"))
+    function DecoderCorrectionGate(dec::AbstractSyndromeDecoder, data_qubits, syndrome_bits)
+        qs = collect(Int, data_qubits)
+        bs = collect(Int, syndrome_bits)
+        bits, qubits = size(parity_checks(decoder))
+        if length(qs)!=qubits
+            throw(ArgumentError(lazy"Decoder expects $qubits qubits, got $(length(data_qubits))"))
         end
-        if length(bs)!=dec.s
-            throw(ArgumentError(lazy"Decoder expects $dec.s qubits, got $(length(input_bits))"))
+        if length(bs)!=bits
+            throw(ArgumentError(lazy"Decoder expects $bits bits, got $(length(syndrome_bits))"))
         end
         return new(dec, qs,bs)
     end
 end
 function QuantumClifford.apply!(state::Register, op::DecoderCorrectionGate)
-  targets = op.affected_qubits
-  key = Vector{Bool}(state.bits[op.input_bits])
+  targets = op.data_qubits
+  key = Vector{Bool}(state.bits[op.syndrome_bits])
   correction = decode(op.decoder, key)  
   correction === nothing && return state
   pauli_operator = PauliOperator(correction)
@@ -30,6 +30,6 @@ function QuantumClifford.apply!(state::Register, op::DecoderCorrectionGate)
 end
 applybranches(s::Register, op::DecoderCorrectionGate; max_order=1) = [(applywstatus!(copy(s),op)...,1,0)]
 
-affectedqubits(op::DecoderCorrectionGate) = op.affected_qubits
-affectedbits(op::DecoderCorrectionGate)   = op.input_bits
+affectedqubits(op::DecoderCorrectionGate) = op.data_qubits
+affectedbits(op::DecoderCorrectionGate)   = op.syndrome_bits
 
