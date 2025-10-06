2472ce8d67da61a168e2921c921b35019bbb98f6
Fe-r-oz <feroz.ahmed.mian@gmail.com>
2025-10-06 00:05:20 -0400

    fix typos in unrelated PR

--- ext/QuantumCliffordGPUExt/fastmemlayout.jl
+++ ext/QuantumCliffordGPUExt/fastmemlayout.jl
@@ -8,7 +8,7 @@ TableauAdj is the type of Tableau with its data stored in an adjoint of a CUDA a
 rest of the fastrow, fastcolumn functions are implemented in QuantumClifford.jl
 """
 
-# todo when we use to_gpu, to_cpu the efffect of fastrow, fastcolumn disappears
+# todo when we use to_gpu, to_cpu the effect of fastrow, fastcolumn disappears
 fastrow(t::TableauCUDA) = t
 fastrow(t::TableauAdj) = Tableau(t.phases, t.nqubits, CuArray(t.xzs))
 fastcolumn(t::TableauCUDA) = Tableau(t.phases, t.nqubits, CuArray(t.xzs')')
