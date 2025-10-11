d617a69724960d65cc7c135a20d33b61d3e80e26
Sai Rohan Reddy Kondlapudi <kondlapudisairohanreddy@gmail.com>
2025-10-10 20:59:29 -0400

    fixed docstring

--- src/ecc/decoder_correction_gate.jl
+++ src/ecc/decoder_correction_gate.jl
@@ -1,6 +1,6 @@
 using QuantumClifford:Register, AbstractOperation, applywstatus!, PauliOperator
 import QuantumClifford:affectedqubits,affectedbits,applybranches
-"""A gate that uses a lookup table to apply a correction gate based on the syndrome of the input register."""
+"""Apply a Pauli correction determined by a decoder, using syndrome bits from the classical register to correct the specified data qubits."""
 struct DecoderCorrectionGate <: AbstractOperation
     decoder::AbstractSyndromeDecoder# just a function that maps inputbits to an operation on affectedqubits
     data_qubits::Vector{Int}
