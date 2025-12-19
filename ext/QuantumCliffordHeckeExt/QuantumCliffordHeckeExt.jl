630b2ca5082dfeac1b3582196bd7d4a8f5958282
Feroz Ahmed Mian <feroz.ahmed.mian@gmail.com>
2025-12-19 10:22:19 -0500

    fix  `parity_matrix` generation and parameter calculations in classical `LiftedCode` (#525)
    Co-authored-by: Stefan Krastanov <stefan@krastanov.org>
    Co-authored-by: Stefan Krastanov <github.acc@krastanov.org>

--- ext/QuantumCliffordHeckeExt/QuantumCliffordHeckeExt.jl
+++ ext/QuantumCliffordHeckeExt/QuantumCliffordHeckeExt.jl
@@ -4,25 +4,24 @@ using QECCore
 import QECCore: code_n, code_s, code_k, rate, distance
 using DocStringExtensions
 
-import QuantumClifford
-import QuantumClifford: gf2_row_echelon_with_pivots!
-import LinearAlgebra
+import QuantumClifford, LinearAlgebra
+
 import Hecke: Group, GroupElem, AdditiveGroupElem,
     GroupAlgebra, GroupAlgebraElem, FqFieldElem, representation_matrix, dim, base_ring,
     multiplication_table, coefficients, abelian_group, group_algebra, rand, gens, order,
     is_commutative, FqPolyRingElem, residue_ring, coeff, zero_matrix, mod1, lift, ZZ, gen,
-    matrix, ncols, nrows, degree, EuclideanRingResidueRingElem, quo, parent, zero, gcd,
-    polynomial_ring, characteristic, isone, mod, factor, zeros
+    polynomial_ring, characteristic, isone, mod, factor, zeros, group, EuclideanRingResidueRingElem,
+    degree, gcd, nrows, ncols
 import Hecke.Generic.MatSpaceElem
 import Nemo
-import Nemo: characteristic, matrix_repr, GF, ZZ, lift, rank
+import Nemo: characteristic, matrix_repr, GF, ZZ, lift, matrix, rank
 
-import QuantumClifford.ECC: iscss, parity_checks,
+import QuantumClifford.ECC: iscss, parity_checks, parity_matrix,
     two_block_group_algebra_codes, generalized_bicycle_codes, bicycle_codes, check_repr_commutation_relation,
     haah_cubic_codes, honeycomb_color_codes, check_repr_regular_linear, random_qc_ghp_code_matrix_A
 
 import QECCore: AbstractQECC, CSS, AbstractCSSCode,
-    hgp, code_k, code_n, code_s, parity_matrix_x, parity_matrix_z, parity_matrix_xz
+    hgp, code_k, code_n, code_s, parity_matrix_x, parity_matrix_z, parity_matrix_xz, parity_matrix
 
 import Random
 import Random: AbstractRNG, default_rng, randperm
