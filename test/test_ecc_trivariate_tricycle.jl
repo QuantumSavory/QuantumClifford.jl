137e0481edfb72c093fb8d5c9ea57a38c93b5e77
Fe-r-oz <feroz.ahmed.mian@gmail.com>
2025-12-22 17:52:34 -0500

    more tests

--- test/test_ecc_trivariate_tricycle.jl
+++ test/test_ecc_trivariate_tricycle.jl
@@ -77,6 +77,16 @@
             @test code_k(c) == k == code_k(stab)
             @test stab_looks_good(stab, remove_redundant_rows=true) == true
             @test iszero(mod.(metacheck_matrix_z(c)*parity_matrix_z(c), 2))
+            c = MultivariateMulticycleCode([l,m, p], [A, B, C])
+            stab = parity_checks(c)
+            mat = matrix(GF(2), stab_to_gf2(stab))
+            computed_rank = rank(mat)
+            @test computed_rank == code_n(c) - code_k(c)
+            # A TT code is defined on n = 3*l*m*p data qubits.
+            @test code_n(c) == n == code_n(stab) == 3*l*m*p
+            @test code_k(c) == k == code_k(stab)
+            @test stab_looks_good(stab, remove_redundant_rows=true) == true
+            @test iszero(mod.(metacheck_matrix_z(c)*parity_matrix_z(c), 2))
         end
     end
 end
