f64f3fc1ad062ace7b7719efb957f277a7d169af
Feroz Ahmed Mian <feroz.ahmed.mian@gmail.com>
2026-01-01 01:33:41 -0500

    use hasproperty and hasmethod for metacheck new file for all codes

--- test/test_ecc_metachecks.jl
+++ test/test_ecc_metachecks.jl
@@ -1,6 +1,8 @@
 @testitem "ECC code metachecks" tags=[:ecc, :ecc_base] begin
+    using QECCore
+    using QuantumClifford
+    using QuantumClifford
     using QuantumClifford.ECC
-    using QuantumClifford.ECC: AbstractECC
 
     include("test_ecc_base.jl")
 
@@ -8,20 +10,26 @@
     
     function test_all_codes_metachecks(all_codes)
         for c in all_codes
+            code_mx = (hasproperty(c, :D) && c.D < 4) || # for DDimensional Toric and surface codes, Mx exist for D = 4
+                (hasproperty(c, :boundary_maps) && length(c.boundary_maps) != 4) || # for homological product, length 4 means Mx exist
+                (hasproperty(c, :t) && c.t < 4) || # for MM code, t  = 4 means Mx exist, otherwise not
+                (hasproperty(c, :polynomials) && length(c.polynomials) != 4)
+
+            code_mz = (hasproperty(c, :D) && c.D < 3) || # for DDimensional Toric and surface codes, Mz exist for D = 3
+                (hasproperty(c, :boundary_maps) && length(c.boundary_maps) != 3) || # for homological product, length 3 means Mz exist
+                (hasproperty(c, :t) && c.t < 3) || # for MM code, 3  = 4 means Mx exist, otherwise not
+                (hasproperty(c, :polynomials) && length(c.polynomials) != 4) # for TT code, Mz exist since polynomials are 3
+
             @testset "Metacheck tests for $(typeof(c))" begin
-                try
+                if hasmethod(metacheck_matrix_x, Tuple{typeof(c)}) && !code_mx
                     Hx = parity_matrix_x(c)
                     Mx = metacheck_matrix_x(c)
                     @test iszero(mod.(Mx*Hx, 2))
-                catch e
-                    # X-metacheck not defined for this code instance, e.g. 2D Toric code
                 end
-                try
+                if hasmethod(metacheck_matrix_z, Tuple{typeof(c)}) && !code_mz
                     Hz = parity_matrix_z(c)
                     Mz = metacheck_matrix_z(c)
                     @test iszero(mod.(Mz*Hz, 2))
-                catch e
-                    # Z-metacheck not defined for this code instance, e.g. 2D Toric code
                 end
             end
         end
