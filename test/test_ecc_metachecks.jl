00b0fec40ac37fe49e76732dc32e132abcec4e0d
Fe-r-oz <feroz.ahmed.mian@gmail.com>
2025-12-31 20:54:17 -0500

    try hasmethod

--- test/test_ecc_metachecks.jl
+++ test/test_ecc_metachecks.jl
@@ -9,19 +9,19 @@
     function test_all_codes_metachecks(all_codes)
         for c in all_codes
             @testset "Metacheck tests for $(typeof(c))" begin
-                try
-                    Hx = parity_matrix_x(c)
-                    Mx = metacheck_matrix_x(c)
-                    @test iszero(mod.(Mx*Hx, 2))
-                catch e
-                    # X-metacheck not defined for this code instance, e.g. 2D Toric code
+                if hasmethod(metacheck_matrix_x, Tuple{typeof(c)})
+                    if (hasproperty(c, :D) && c.D ≥ 4) || (hasproperty(c, :boundary_maps) && length(c.boundary_maps) == 4) || (!hasproperty(c, :D) && !hasproperty(c, :boundary_maps))
+                        Hx = parity_matrix_x(c)
+                        Mx = metacheck_matrix_x(c)
+                        @test iszero(mod.(Mx * Hx, 2))
+                    end
                 end
-                try
-                    Hz = parity_matrix_z(c)
-                    Mz = metacheck_matrix_z(c)
-                    @test iszero(mod.(Mz*Hz, 2))
-                catch e
-                    # Z-metacheck not defined for this code instance, e.g. 2D Toric code
+                if hasmethod(metacheck_matrix_z, Tuple{typeof(c)})
+                    if (hasproperty(c, :D) && c.D ≥ 3) || (hasproperty(c, :boundary_maps) && length(c.boundary_map) == 3) || (!hasproperty(c, :D) && !hasproperty(c, :seed_codes))
+                        Hz = parity_matrix_z(c)
+                        Mz = metacheck_matrix_z(c)
+                        @test iszero(mod.(Mz * Hz, 2))
+                    end
                 end
             end
         end
