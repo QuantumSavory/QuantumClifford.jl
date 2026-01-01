4208c4344a2985c1c909db47e6c4d68f475247f2
Fe-r-oz <feroz.ahmed.mian@gmail.com>
2026-01-01 17:24:59 -0500

    polish

--- test/test_ecc_metachecks.jl
+++ test/test_ecc_metachecks.jl
@@ -9,6 +9,7 @@
     all_codes = all_testable_code_instances()
     function test_all_codes_metachecks(all_codes)
         for c in all_codes
+            Mx, Mz = hasmetachecks(c)
             !isnothing(Mx) && @test iszero(mod.(Mx*parity_matrix_x(c), 2))
             !isnothing(Mz) && @test iszero(mod.(Mz*parity_matrix_z(c), 2))
             isnothing(Mx) && isnothing(Mz) && @info "No metacheck matrices defined for $(typeof(c))"
