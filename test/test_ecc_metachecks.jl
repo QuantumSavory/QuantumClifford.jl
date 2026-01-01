b1e3304769444338e971cfb1d932f8b61a671500
Fe-r-oz <feroz.ahmed.mian@gmail.com>
2025-12-31 20:55:47 -0500

    try has method and has property

--- test/test_ecc_metachecks.jl
+++ test/test_ecc_metachecks.jl
@@ -10,14 +10,16 @@
         for c in all_codes
             @testset "Metacheck tests for $(typeof(c))" begin
                 if hasmethod(metacheck_matrix_x, Tuple{typeof(c)})
-                    if (hasproperty(c, :D) && c.D ≥ 4) || (hasproperty(c, :boundary_maps) && length(c.boundary_maps) == 4) || (!hasproperty(c, :D) && !hasproperty(c, :boundary_maps))
+                    if (hasproperty(c, :D) && c.D ≥ 4) || (hasproperty(c, :boundary_maps) && length(c.boundary_maps) == 4) || 
+                        (hasproperty(c, :t) && c.t ≥ 4) || (!hasproperty(c, :D) && !hasproperty(c, :boundary_maps) && !hasproperty(c, :t))
                         Hx = parity_matrix_x(c)
                         Mx = metacheck_matrix_x(c)
                         @test iszero(mod.(Mx * Hx, 2))
                     end
                 end
                 if hasmethod(metacheck_matrix_z, Tuple{typeof(c)})
-                    if (hasproperty(c, :D) && c.D ≥ 3) || (hasproperty(c, :boundary_maps) && length(c.boundary_map) == 3) || (!hasproperty(c, :D) && !hasproperty(c, :seed_codes))
+                    if (hasproperty(c, :D) && c.D ≥ 3) || (hasproperty(c, :boundary_maps) && length(c.boundary_maps) == 3) || 
+                        (hasproperty(c, :t) && c.t ≥ 3) || (!hasproperty(c, :D) && !hasproperty(c, :boundary_maps) && !hasproperty(c, :t))
                         Hz = parity_matrix_z(c)
                         Mz = metacheck_matrix_z(c)
                         @test iszero(mod.(Mz * Hz, 2))
