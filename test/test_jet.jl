6115cf7c8dd8c6e658137890b6061a25ae9a7eab
Fe-r-oz <feroz.ahmed.mian@gmail.com>
2026-01-03 15:43:54 -0500

    bring back jet to 5

--- test/test_jet.jl
+++ test/test_jet.jl
@@ -6,6 +6,6 @@
 
     rep = JET.report_package(QuantumClifford, target_modules=[QuantumClifford, QuantumInterface])
     @show rep
-    @test length(JET.get_reports(rep)) <= 11
+    @test length(JET.get_reports(rep)) <= 5
     @test_broken length(JET.get_reports(rep)) == 0
 end
