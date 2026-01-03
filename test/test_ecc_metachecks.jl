3faca4f621961d34bee5a8238c11e717a050a315
Fe-r-oz <feroz.ahmed.mian@gmail.com>
2026-01-03 15:30:11 -0500

    rm isempty

--- test/test_ecc_metachecks.jl
+++ test/test_ecc_metachecks.jl
@@ -14,11 +14,11 @@
             has_mx, has_mz = hasmetachecks(c)
             if has_mz && hasmethod(metacheck_matrix_z, Tuple{typeof(c)})
                 Mz = metacheck_matrix_z(c)
-                !isempty(Mz) && @test iszero(mod.(Mz*parity_matrix_z(c), 2))
+                @test iszero(mod.(Mz*parity_matrix_z(c), 2))
             end
             if has_mx && hasmethod(metacheck_matrix_x, Tuple{typeof(c)})
                 Mx = metacheck_matrix_x(c)
-                !isempty(Mx) && @test iszero(mod.(Mx*parity_matrix_x(c), 2))
+                @test iszero(mod.(Mx*parity_matrix_x(c), 2))
             end
         end
     end
