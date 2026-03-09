8052b1b18f7d0448b6f34fa7bdf930d2bfc1f4d5
Feroz Ahmed Mian <feroz.ahmed.mian@gmail.com>
2026-03-09 05:25:22 -0400

    fix apply(genstab, paulichannel)

--- src/lowrank/PauliChannelNonClifford.jl
+++ src/lowrank/PauliChannelNonClifford.jl
@@ -446,10 +446,10 @@ function apply!(state::GeneralizedStabilizer, gate::AbstractPauliChannel; prune_
             w = gate.weights[i]
             phaseₗ, dₗ, dₗˢᵗᵃᵇ = rowdecompose(Pₗ,stab)
             phaseᵣ, dᵣ, dᵣˢᵗᵃᵇ = rowdecompose(Pᵣ,stab)
-            c = (dot(dₗˢᵗᵃᵇ,dᵢ) + dot(dᵣˢᵗᵃᵇ,dⱼ))*2
+            c = (dot(dₗˢᵗᵃᵇ,dᵢ) + dot(dᵣˢᵗᵃᵇ,dⱼ)) # no factor of two in algorithm 3 (or Eq. 29) while computing this, see https://scottaaronson.com/showcase2/report/ted-yoder.pdf
             dᵢ′ = dₗ .⊻ dᵢ
             dⱼ′ = dᵣ .⊻ dⱼ
-            χ′ = χ * w * (-tone)^c * (im)^(-phaseₗ+phaseᵣ+4)
+            χ′ = χ * w * (-tone)^c * (im)^(phaseₗ-phaseᵣ+4) # Since M Pauli operator can be decomposed as α*d_b*s_c -- here we have α_l*α† == i^(phase_l-phase_r+4) since (i^phase)† == i^(-phase)
             newdict[(dᵢ′,dⱼ′)] += χ′
         end
     end
