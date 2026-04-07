98e12fd01917b764b6236c525f4bebb84eaf9e6f
the-punisher-29 <soumenkumar9503@gmail.com>
2026-04-07 11:12:48 +0530

    doc: comply with docstring guidelines by removing slow solver

--- ext/QuantumCliffordOscarExt/zsz_code.jl
+++ ext/QuantumCliffordOscarExt/zsz_code.jl
@@ -20,13 +20,13 @@ Here is an example of the `[[80, 2, 8]]` ZSZ code from Table I of [guo2025zsz](@
 The parameters `l=5`, `m=8`, and `q=2` correspond to the semidirect product presentation ``\\langle x, y \\mid x^5=1, y^8=1, y x y^{-1} = x^2 \\rangle``.
 Note that ZSZ codes are generally asymmetric, meaning the $X$-distance and $Z$-distance differ (``d_X \\neq d_Z``). For instance, the `[[80, 2, 8]]` code below has ``d_X = 10`` and ``d_Z = 8``.
 
-```julia
-julia> using Oscar, QuantumClifford.ECC, HiGHS, JuMP
+```jldoctest
+julia> using Oscar, QuantumClifford.ECC
 
 julia> c = ZSZ(5, 8, 2, [(0,0),(4,4),(4,1)], [(0,0),(3,0),(2,7)]);
 
-julia> code_n(c), code_k(c), distance(c, DistanceMIPAlgorithm(solver=HiGHS))
-(80, 2, 8)
+julia> code_n(c), code_k(c)
+(80, 2)
 ```
 
 ### Fields
