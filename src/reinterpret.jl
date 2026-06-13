58a8a2a5027d3a8b4cfdcff1f06d4b1dc491dfc7
Arnav Kapoor <kapoorarnav43@gmail.com>
2026-06-13 22:50:24 +0530

    Update reinterpret.jl

--- src/reinterpret.jl
+++ src/reinterpret.jl
@@ -3,7 +3,7 @@ function Base.reinterpret(::Type{U}, p::PauliOperator) where {U <: Unsigned}
 	total_bytes = length(p.xz) * sizeof(old)
 	q, r = divrem(total_bytes, sizeof(U))
 	if r != 0 || isodd(q)
-		throw(ArgumentError(THROW_REINTERPRET_SIZE_MISMATCH(:pauli, old, U, total_bytes, q, r)))
+		throw(ArgumentError(THROW_REINTERPRET_SIZE_MISMATCH))
 	end
 	new_xz = reinterpret(U, p.xz)
 	return PauliOperator(p.phase, p.nqubits, new_xz)
@@ -12,22 +12,26 @@ end
 function Base.reinterpret(::Type{U}, t::Tableau) where {U <: Unsigned}
 	rows = size(t.xzs, 1)
 	old = eltype(t.xzs)
-	total_bytes = rows * sizeof(old)
-	q, r = divrem(total_bytes, sizeof(U))
+	q, r = divrem(rows * sizeof(old), sizeof(U))
 	if r != 0 || isodd(q)
-		throw(ArgumentError(THROW_REINTERPRET_SIZE_MISMATCH(:tableau, old, U, total_bytes, q, r)))
+		throw(ArgumentError(THROW_REINTERPRET_SIZE_MISMATCH))
 	end
 	new_xzs = reinterpret(U, t.xzs)
 	return Tableau(t.phases, t.nqubits, new_xzs)
 end
 
-Base.reinterpret(::Type{U}, s::Union{Stabilizer, Destabilizer}) where {U <: Unsigned} =
-	typeof(s)(reinterpret(U, tab(s)))
+Base.reinterpret(::Type{U}, s::Stabilizer) where {U <: Unsigned} =
+	Stabilizer(reinterpret(U, tab(s)))
 
-Base.reinterpret(::Type{U}, ms::Union{MixedStabilizer, MixedDestabilizer}) where {U <: Unsigned} =
-	typeof(ms)(reinterpret(U, tab(ms)), rank(ms))
+Base.reinterpret(::Type{U}, d::Destabilizer) where {U <: Unsigned} =
+	Destabilizer(reinterpret(U, tab(d)))
+
+Base.reinterpret(::Type{U}, ms::MixedStabilizer) where {U <: Unsigned} =
+	MixedStabilizer(reinterpret(U, tab(ms)), rank(ms))
+
+Base.reinterpret(::Type{U}, md::MixedDestabilizer) where {U <: Unsigned} =
+	MixedDestabilizer(reinterpret(U, tab(md)), rank(md))
 
 function Base.reinterpret(::Type{U}, f::PauliFrame) where {U <: Unsigned}
 	return PauliFrame(reinterpret(U, f.frame), f.measurements)
 end
-
