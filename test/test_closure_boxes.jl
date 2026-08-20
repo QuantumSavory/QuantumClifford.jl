34989f3695f7828788b90dce08b45a6e28b28d0b
Krastanov's agent <github.agent@krastanov.org>
2026-08-20 15:27:30 +0000

    Move closure box checks to dedicated test file

---
+++ test/test_closure_boxes.jl
@@ -0,0 +1,17 @@
+@testitem "Closure boxes" tags=[:aqua] begin
+    @static if VERSION >= v"1.14-"
+        using Hecke
+        hecke_ext = Base.get_extension(QuantumClifford, :QuantumCliffordHeckeExt)
+        @test !isnothing(hecke_ext)
+        @test isempty(Test.detect_closure_boxes(QuantumClifford, hecke_ext))
+    end
+end
+
+@testitem "Oscar extension closure boxes" tags=[:aqua, :oscar_required] begin
+    @static if VERSION >= v"1.14-"
+        using Oscar
+        oscar_ext = Base.get_extension(QuantumClifford, :QuantumCliffordOscarExt)
+        @test !isnothing(oscar_ext)
+        @test isempty(Test.detect_closure_boxes(oscar_ext))
+    end
+end
