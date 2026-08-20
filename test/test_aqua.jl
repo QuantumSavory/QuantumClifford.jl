97ed28361e21ee2301997a6a8355b7033727cdf4
Krastanov's agent <github.agent@krastanov.org>
2026-08-20 13:56:37 +0000

    Remove closure boxes from package extensions

--- test/test_aqua.jl
+++ test/test_aqua.jl
@@ -1,7 +1,22 @@
 @testitem "Aqua" tags=[:aqua] begin
     using Aqua
     Aqua.test_all(QuantumClifford)
+end
+
+@testitem "Closure boxes" tags=[:aqua] begin
+    using Hecke
     if isdefined(Test, :detect_closure_boxes)
-        @test isempty(Test.detect_closure_boxes(QuantumClifford))
+        hecke_ext = Base.get_extension(QuantumClifford, :QuantumCliffordHeckeExt)
+        @test !isnothing(hecke_ext)
+        @test isempty(Test.detect_closure_boxes(QuantumClifford, hecke_ext))
+    end
+end
+
+@testitem "Oscar extension closure boxes" tags=[:aqua, :oscar_required] begin
+    using Oscar
+    if isdefined(Test, :detect_closure_boxes)
+        oscar_ext = Base.get_extension(QuantumClifford, :QuantumCliffordOscarExt)
+        @test !isnothing(oscar_ext)
+        @test isempty(Test.detect_closure_boxes(oscar_ext))
     end
 end
