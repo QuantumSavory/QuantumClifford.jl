3a6fa93bd3a85081253a82a6bb36fa9f943294b9
Krastanov's agent <github.agent@krastanov.org>
2026-08-20 13:13:13 +0000

    Remove closure box from sumtype generation

--- test/test_aqua.jl
+++ test/test_aqua.jl
@@ -1,4 +1,7 @@
 @testitem "Aqua" tags=[:aqua] begin
     using Aqua
     Aqua.test_all(QuantumClifford)
+    if isdefined(Test, :detect_closure_boxes)
+        @test isempty(Test.detect_closure_boxes(QuantumClifford))
+    end
 end
